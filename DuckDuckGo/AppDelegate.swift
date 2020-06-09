080c0afaa781e099687ca4c2a4ac1dc0e8b1aa8c
Tomas Strba <tom@duckduckgo.com>
2020-06-09 16:24:22 +0200

    Initial Commit

---
+++ DuckDuckGo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DuckDuckGo
+//
+//  Created by Tomas Strba on 09/06/2020.
+//  Copyright © 2020 DuckDuckGo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
