c5015bb4d418feb6c57a9bf9929d0e4cb64d11cd
Chris Brind <brindy@duckduckgo.com>
2020-11-30 16:23:09 +0000

    extract context menu strings

--- DuckDuckGo/BrowserTab/View/NSMenuItem+ContextMenuItems.swift
+++ DuckDuckGo/BrowserTab/View/NSMenuItem+ContextMenuItems.swift
@@ -27,28 +27,28 @@ class URLContextMenuItem: NSMenuItem {
 // swiftlint:disable line_length
 extension NSMenuItem {
 
-    static let contextMenuBack = NSMenuItem(title: "Back", action: #selector(NavigationMenuItemSelectors.back(_:)), keyEquivalent: "")
-    static let contextMenuForward = NSMenuItem(title: "Forward", action: #selector(NavigationMenuItemSelectors.forward(_:)), keyEquivalent: "")
-    static let contextMenuReload = NSMenuItem(title: "Reload Page", action: #selector(NavigationMenuItemSelectors.reloadPage(_:)), keyEquivalent: "")
+    static let contextMenuBack = NSMenuItem(title: UserText.navigateBack, action: #selector(NavigationMenuItemSelectors.back(_:)), keyEquivalent: "")
+    static let contextMenuForward = NSMenuItem(title: UserText.navigateForward, action: #selector(NavigationMenuItemSelectors.forward(_:)), keyEquivalent: "")
+    static let contextMenuReload = NSMenuItem(title: UserText.reloadPage, action: #selector(NavigationMenuItemSelectors.reloadPage(_:)), keyEquivalent: "")
 
     static let linkContextMenuItems: [NSMenuItem] = [
 
-        URLContextMenuItem(title: "Open Link In New Tab", action: #selector(LinkMenuItemSelectors.openLinkInNewTab(_:)), keyEquivalent: ""),
-        URLContextMenuItem(title: "Open Link In New Window", action: #selector(LinkMenuItemSelectors.openLinkInNewWindow(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.openLinkInNewTab, action: #selector(LinkMenuItemSelectors.openLinkInNewTab(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.openLinkInNewWindow, action: #selector(LinkMenuItemSelectors.openLinkInNewWindow(_:)), keyEquivalent: ""),
         NSMenuItem.separator(),
-        URLContextMenuItem(title: "Download Linked File", action: #selector(LinkMenuItemSelectors.downloadLinkedFile(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.downloadLinkedFile, action: #selector(LinkMenuItemSelectors.downloadLinkedFile(_:)), keyEquivalent: ""),
         NSMenuItem.separator(),
-        URLContextMenuItem(title: "Copy Link", action: #selector(LinkMenuItemSelectors.copyLink(_:)), keyEquivalent: "")
+        URLContextMenuItem(title: UserText.copyLink, action: #selector(LinkMenuItemSelectors.copyLink(_:)), keyEquivalent: "")
 
     ]
 
     static let imageContextMenuItems: [NSMenuItem] = [
-        URLContextMenuItem(title: "Open Image in New Tab", action: #selector(ImageMenuItemSelectors.openImageInNewTab(_:)), keyEquivalent: ""),
-        URLContextMenuItem(title: "Open Image in New Window", action: #selector(ImageMenuItemSelectors.openImageInNewWindow(_:)), keyEquivalent: ""),
-        URLContextMenuItem(title: "Save Image to \"Downloads\"", action: #selector(ImageMenuItemSelectors.saveImageToDownloads(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.openImageInNewTab, action: #selector(ImageMenuItemSelectors.openImageInNewTab(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.openImageInNewWindow, action: #selector(ImageMenuItemSelectors.openImageInNewWindow(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.saveImageToDownloads, action: #selector(ImageMenuItemSelectors.saveImageToDownloads(_:)), keyEquivalent: ""),
         NSMenuItem.separator(),
-        URLContextMenuItem(title: "Copy Image Address", action: #selector(ImageMenuItemSelectors.copyImageAddress(_:)), keyEquivalent: ""),
-        URLContextMenuItem(title: "Copy Image", action: #selector(ImageMenuItemSelectors.copyImage(_:)), keyEquivalent: "")
+        URLContextMenuItem(title: UserText.copyImageAddress, action: #selector(ImageMenuItemSelectors.copyImageAddress(_:)), keyEquivalent: ""),
+        URLContextMenuItem(title: UserText.copyImage, action: #selector(ImageMenuItemSelectors.copyImage(_:)), keyEquivalent: "")
     ]
 
 }
@@ -65,7 +65,7 @@ extension NSMenuItem {
 @objc protocol LinkMenuItemSelectors {
 
     func openLinkInNewTab(_ sender: URLContextMenuItem)
-    @objc optional func openLinkInNewWindow(_ sender: URLContextMenuItem)
+    func openLinkInNewWindow(_ sender: URLContextMenuItem)
     func downloadLinkedFile(_ sender: URLContextMenuItem)
     func copyLink(_ sender: URLContextMenuItem)
 
@@ -74,9 +74,9 @@ extension NSMenuItem {
 @objc protocol ImageMenuItemSelectors {
 
     func openImageInNewTab(_ sender: URLContextMenuItem)
-    @objc optional func openImageInNewWindow(_ sender: URLContextMenuItem)
+    func openImageInNewWindow(_ sender: URLContextMenuItem)
     func saveImageToDownloads(_ sender: URLContextMenuItem)
-    @objc optional func copyImageAddress(_ sender: URLContextMenuItem)
+    func copyImageAddress(_ sender: URLContextMenuItem)
     @objc optional func copyImage(_ sender: URLContextMenuItem)
 
 }
