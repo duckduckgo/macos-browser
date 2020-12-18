cec87c6a069cde74a5ed2c100e33e8171239c08a
Sam Symons <sam@samsymons.com>
2020-12-17 20:22:13 -0800

    Add the file store.

---
+++ DuckDuckGo/FileSystem/FileStore.swift
@@ -0,0 +1,76 @@
+//
+//  FileStore.swift
+//
+//  Copyright © 2020 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+protocol FileStoring {
+    func persist(_ data: Data, fileName: String) -> Bool
+    func loadData(named fileName: String) -> Data?
+    func hasData(for fileName: String) -> Bool
+}
+
+public class FileStore: FileStoring {
+
+    private let encryptionKeyStore: EncryptionKeyStoring?
+
+    init(encryptionKeyStore: EncryptionKeyStoring? = nil) {
+        self.encryptionKeyStore = encryptionKeyStore
+    }
+
+    func persist(_ data: Data, fileName: String) -> Bool {
+        do {
+            let dataToWrite: Data
+
+            if let keyStore = self.encryptionKeyStore {
+                let key = try keyStore.readKey()
+                dataToWrite = try DataEncryption.encrypt(data: data, key: key)
+            } else {
+                dataToWrite = data
+            }
+
+            try dataToWrite.write(to: persistenceLocation(for: fileName))
+
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    func loadData(named fileName: String) -> Data? {
+        guard let data = try? Data(contentsOf: persistenceLocation(for: fileName)) else {
+            return nil
+        }
+
+        if let keyStore = self.encryptionKeyStore, let key = try? keyStore.readKey() {
+            return try? DataEncryption.decrypt(data: data, key: key)
+        } else {
+            return data
+        }
+    }
+
+    func hasData(for fileName: String) -> Bool {
+        let path = persistenceLocation(for: fileName).path
+        return FileManager.default.fileExists(atPath: path)
+    }
+
+    func persistenceLocation(for fileName: String) -> URL {
+        let applicationSupportPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first
+        return applicationSupportPath!.appendingPathComponent(fileName)
+    }
+
+}
