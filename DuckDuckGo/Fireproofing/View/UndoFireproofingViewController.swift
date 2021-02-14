5902013b0f0c66d4f9d9daa1b65e4aa2765b871c
Sam Symons <sam@samsymons.com>
2021-02-14 10:58:21 -0800

    Add the fireproofing view controllers.

---
+++ DuckDuckGo/Fireproofing/View/UndoFireproofingViewController.swift
@@ -0,0 +1,111 @@
+//
+//  UndoFireproofingViewController.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import AppKit
+
+protocol UndoFireproofingViewControllerDelegate: class {
+
+    func undoFireproofingViewController(_ viewController: UndoFireproofingViewController, requestedUndoFor domain: String)
+
+}
+
+class UndoFireproofingViewController: NSViewController {
+
+    enum Constants {
+        static let storyboardName = "Fireproofing"
+        static let identifier = "UndoFireproofingViewController"
+        static let autoDismissDuration: TimeInterval = 3.0
+    }
+
+    static func create(for domain: String) -> UndoFireproofingViewController {
+        let storyboard = NSStoryboard(name: Constants.storyboardName, bundle: nil)
+
+        return storyboard.instantiateController(identifier: Constants.identifier) { coder in
+            return UndoFireproofingViewController(coder: coder, domain: domain)
+        }
+    }
+
+    @IBOutlet weak var titleLabel: NSTextField!
+
+    weak var delegate: UndoFireproofingViewControllerDelegate?
+
+    private var timer: Timer?
+    private var domain: String
+
+    init?(coder: NSCoder, domain: String) {
+        self.domain = domain
+        super.init(coder: coder)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("You must create this view controller with a domain.")
+    }
+
+    deinit {
+        cancelAutoDismissTimer()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateTitleLabel(with: domain)
+
+        if let mouseOverView = self.view as? MouseOverView {
+            mouseOverView.delegate = self
+        }
+    }
+
+    override func viewDidAppear() {
+        super.viewDidAppear()
+        scheduleAutoDismissTimer()
+    }
+
+    @IBAction func undoAction(_ sender: Any) {
+        delegate?.undoFireproofingViewController(self, requestedUndoFor: domain)
+        self.presentingViewController?.dismiss(self)
+    }
+
+    private func cancelAutoDismissTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+
+    private func scheduleAutoDismissTimer() {
+        cancelAutoDismissTimer()
+        timer = Timer.scheduledTimer(withTimeInterval: Constants.autoDismissDuration, repeats: false) { [weak self] _ in
+            guard let self = self else { return }
+            self.presentingViewController?.dismiss(self)
+        }
+    }
+
+    private func updateTitleLabel(with domain: String) {
+        self.titleLabel.stringValue = "\(domain) is Fireproof"
+    }
+
+}
+
+extension UndoFireproofingViewController: MouseOverViewDelegate {
+
+    func mouseOverView(_ mouseOverView: MouseOverView, isMouseOver: Bool) {
+        if isMouseOver {
+            cancelAutoDismissTimer()
+        } else {
+            scheduleAutoDismissTimer()
+        }
+    }
+
+}
