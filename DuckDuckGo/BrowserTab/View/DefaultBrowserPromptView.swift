a5a9a16fd0539f664a5bdc3335457680b9307d0f
Sam Symons <sam@samsymons.com>
2021-02-23 19:34:16 -0800

    Add a basic implementation of the default browser prompt.

---
+++ DuckDuckGo/BrowserTab/View/DefaultBrowserPromptView.swift
@@ -0,0 +1,66 @@
+//
+//  DefaultBrowserPromptView.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import AppKit
+
+class DefaultBrowserPromptView: NSView, NibLoadable {
+
+    @IBOutlet var logoImageView: NSImageView! {
+        didSet {
+            logoImageView.wantsLayer = true
+            logoImageView.layer?.masksToBounds = false
+            logoImageView.layer?.shadowOpacity = 0.20
+            logoImageView.layer?.shadowColor = NSColor.black.cgColor
+            logoImageView.layer?.shadowOffset = CGSize(width: 0, height: -1)
+            logoImageView.layer?.shadowRadius = 2
+        }
+    }
+
+    @IBAction func setDefaultButtonClicked(_ sender: Any) {
+        print("Set default clicked")
+    }
+
+    @IBAction func dismissPromptButtonClicked(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+
+}
+
+protocol NibLoadable {
+
+    static var nibName: String { get }
+    static func createFromNib(in bundle: Bundle) -> Self
+
+}
+
+extension NibLoadable where Self: NSView {
+
+    static var nibName: String {
+        return String(describing: Self.self)
+    }
+
+    static func createFromNib(in bundle: Bundle = Bundle.main) -> Self {
+        var topLevelArray: NSArray?
+        bundle.loadNibNamed(NSNib.Name(nibName), owner: self, topLevelObjects: &topLevelArray)
+        let views = [Any](topLevelArray!).filter { $0 is Self }
+
+        // swiftlint:disable force_cast
+        return views.last as! Self
+        // swiftlint:enable force_cast
+    }
+}
