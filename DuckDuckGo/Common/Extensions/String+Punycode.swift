c65f40176d14b73cb8eec7cc879337b820cdaab4
Alexey Martemyanov <mallexxx@gmail.com>
2021-03-10 14:33:40 +0700

    add Punycode package, extension and tests

---
+++ DuckDuckGo/Common/Extensions/String+Punycode.swift
@@ -0,0 +1,78 @@
+//
+//  String+Punycode.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import Punycode
+
+extension String {
+
+    /// URL and URLComponents can't cope with emojis and international characters so this routine does some manual processing while trying to
+    ///  retain the input as much as possible.
+    public var punycodedUrl: URL? {
+        if let url = URL(string: self) {
+            return url
+        }
+
+        if contains(" ") {
+            return nil
+        }
+
+        var originalScheme = ""
+        var s = self
+
+        if hasPrefix(URL.NavigationalScheme.http.separated()) {
+            originalScheme = URL.NavigationalScheme.http.separated()
+        } else if hasPrefix(URL.NavigationalScheme.https.separated()) {
+            originalScheme = URL.NavigationalScheme.https.separated()
+        } else if !contains(".") {
+            // could be a local domain but user needs to use the protocol to specify that
+            return nil
+        } else {
+            s = URL.NavigationalScheme.https.separated() + s
+        }
+
+        let urlAndQuery = s.split(separator: "?")
+        guard urlAndQuery.count > 0 else {
+            return nil
+        }
+
+        let query = urlAndQuery.count > 1 ? "?" + urlAndQuery[1] : ""
+        let componentsWithoutQuery = [String](urlAndQuery[0].split(separator: "/").map { String($0) }.dropFirst())
+        guard componentsWithoutQuery.count > 0 else {
+            return nil
+        }
+
+        let host = componentsWithoutQuery[0].punycodeEncodedHostname
+        let encodedPath = componentsWithoutQuery
+            .dropFirst()
+            .map { $0.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlPathAllowed) ?? $0 }
+            .joined(separator: "/")
+
+        let hostPathSeparator = !encodedPath.isEmpty || hasSuffix("/") ? "/" : ""
+        let url = originalScheme + host + hostPathSeparator + encodedPath + query
+        return URL(string: url)
+    }
+
+    public var punycodeEncodedHostname: String {
+        return self.split(separator: ".")
+            .map { String($0) }
+            .map { $0.idnaEncoded ?? $0 }
+            .joined(separator: ".")
+    }
+    
+}
---
+++ Unit Tests/Common/String+PunycodeTests.swift
@@ -0,0 +1,45 @@
+//
+//  String+PunycodeTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2017 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+
+class StringPunycodeTests: XCTestCase {
+
+    func testWhenPunycodeUrlIsCalledOnQueryThenUrlIsNotReturned() {
+        XCTAssertNil(" ".punycodedUrl?.absoluteString)
+    }
+
+    func testWhenPunycodeUrlIsCalledOnLocalHostnameThenUrlIsNotReturned() {
+        XCTAssertNil("💩".punycodedUrl?.absoluteString)
+    }
+    
+    func testWhenPunycodeUrlIsCalledWithValidUrlsThenUrlIsReturned() {
+        XCTAssertEqual("xn--ls8h.la", "💩.la".punycodedUrl?.absoluteString)
+        XCTAssertEqual("xn--ls8h.la/", "💩.la/".punycodedUrl?.absoluteString)
+        XCTAssertEqual("82.xn--b1aew.xn--p1ai", "82.мвд.рф".punycodedUrl?.absoluteString)
+        XCTAssertEqual("http://xn--ls8h.la:8080", "http://💩.la:8080".punycodedUrl?.absoluteString)
+        XCTAssertEqual("http://xn--ls8h.la", "http://💩.la".punycodedUrl?.absoluteString)
+        XCTAssertEqual("https://xn--ls8h.la", "https://💩.la".punycodedUrl?.absoluteString)
+        XCTAssertEqual("https://xn--ls8h.la/", "https://💩.la/".punycodedUrl?.absoluteString)
+        XCTAssertEqual("https://xn--ls8h.la/path/to/resource", "https://💩.la/path/to/resource".punycodedUrl?.absoluteString)
+        XCTAssertEqual("https://xn--ls8h.la/path/to/resource?query=true", "https://💩.la/path/to/resource?query=true".punycodedUrl?.absoluteString)
+        XCTAssertEqual("https://xn--ls8h.la/%F0%9F%92%A9", "https://💩.la/💩".punycodedUrl?.absoluteString)
+    }
+
+}
