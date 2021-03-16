768c18af8ef9e3aa59bbf54da0e4ee16a15ec2fa
Sam Symons <sam@samsymons.com>
2021-03-15 18:53:34 -0700

    Add back a test that fails locally.

--- Unit Tests/BrowserTab/ViewModel/TabViewModelTests.swift
+++ Unit Tests/BrowserTab/ViewModel/TabViewModelTests.swift
@@ -135,6 +135,7 @@ class TabViewModelTests: XCTestCase {
 
         tabViewModel.$favicon.debounce(for: 0.1, scheduler: RunLoop.main).sink { favicon in
             XCTAssertNotNil(favicon)
+            XCTAssertNotEqual(favicon, TabViewModel.Favicon.home)
             if favicon != TabViewModel.Favicon.defaultFavicon {
                 faviconExpectation.fulfill()
             }
