822dcd00aeeffc4e981cc1bd843fb2b2257403cf
Alexey Martemyanov <mallexxx@gmail.com>
2021-04-13 16:18:50 +0700

    copyright

--- DuckDuckGo/ContentBlocker/contentblocker.js
+++ DuckDuckGo/ContentBlocker/contentblocker.js
@@ -2,7 +2,7 @@
 //  contentblocker.js
 //  DuckDuckGo
 //
-//  Copyright © 2017 DuckDuckGo. All rights reserved.
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
