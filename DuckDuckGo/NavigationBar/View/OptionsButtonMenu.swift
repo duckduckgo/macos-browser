6995fe7b35593f34e91054e5b11b23999dd14fa7
Alexey Martemyanov <mallexxx@gmail.com>
2021-04-28 11:10:37 +0700

    fix build (rollback this!)

--- DuckDuckGo/NavigationBar/View/OptionsButtonMenu.swift
+++ DuckDuckGo/NavigationBar/View/OptionsButtonMenu.swift
@@ -297,20 +297,21 @@ final class EmailOptionsButtonSubMenu: NSMenu {
     }
     
     @objc func createAddressAction(_ sender: NSMenuItem) {
-        guard let url = emailManager.generateTokenPageURL else {
-            assertionFailure("Could not get token page URL, token not available")
-            return
-        }
-        let tab = Tab()
-        tab.url = url
-        tabCollectionViewModel.append(tab: tab)
-
-        (supermenu as? OptionsButtonMenu)?.result = .emailProtectionCreateAddress
+        fatalError()
+//        guard let url = emailManager.generateTokenPageURL else {
+//            assertionFailure("Could not get token page URL, token not available")
+//            return
+//        }
+//        let tab = Tab()
+//        tab.url = url
+//        tabCollectionViewModel.append(tab: tab)
+//
+//        (supermenu as? OptionsButtonMenu)?.result = .emailProtectionCreateAddress
     }
     
     @objc func viewDashboardAction(_ sender: NSMenuItem) {
         let tab = Tab()
-        tab.url = EmailUrls().emailDashboardPage
+//        tab.url = EmailUrls().emailDashboardPage
         tabCollectionViewModel.append(tab: tab)
 
         (supermenu as? OptionsButtonMenu)?.result = .emailProtectionDashboard
@@ -324,7 +325,7 @@ final class EmailOptionsButtonSubMenu: NSMenu {
     
     @objc func turnOnEmailAction(_ sender: NSMenuItem) {
         let tab = Tab()
-        tab.url = EmailUrls().emailLandingPage
+//        tab.url = EmailUrls().emailLandingPage
         tabCollectionViewModel.append(tab: tab)
 
         (supermenu as? OptionsButtonMenu)?.result = .emailProtection
