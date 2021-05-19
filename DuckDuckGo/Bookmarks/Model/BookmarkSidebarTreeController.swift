85105dbcf8f4e783d3fbefc7b0a8be42395c7765
Sam Symons <sam@samsymons.com>
2021-05-18 21:12:43 -0700

    Bookmarks management part 2 (#115)
    * Add NSOutlineView extensions.
    
    * Add PasteboardFolder and PasteboardBookmark.
    
    * Update the NSOutlineView extension.
    
    * Add the bookmark sidebar tree controller.
    
    * Add the BookmarksOutlineView subclass.
    
    * Rename a test file.
    
    * Add the bookmarks outline view data source and views.
    
    * Clean up a few comments.
    
    * Fix another misnamed header.
    
    * Fix failed tests.
    
    * Fix PR comments.

---
+++ DuckDuckGo/Bookmarks/Model/BookmarkSidebarTreeController.swift
@@ -0,0 +1,106 @@
+//
+//  BookmarkSidebarTreeController.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+final class BookmarkSidebarTreeController: TreeControllerDataSource {
+
+    func treeController(treeController: TreeController, childNodesFor node: BookmarkNode) -> [BookmarkNode] {
+        return node.isRoot ? childNodesForRootNode(node) : childNodes(for: node)
+    }
+
+    private let bookmarkManager: BookmarkManager
+
+    init(bookmarkManager: BookmarkManager = LocalBookmarkManager.shared) {
+        self.bookmarkManager = bookmarkManager
+    }
+
+    // MARK: - Private
+
+    private func childNodesForRootNode(_ node: BookmarkNode) -> [BookmarkNode] {
+        let favorites = PseudoFolder.favorites
+        let favoritesNode = BookmarkNode(representedObject: favorites, parent: node)
+        favoritesNode.canHaveChildNodes = false
+
+        let blankSpacer = SpacerNode.blank
+        let spacerNode = BookmarkNode(representedObject: blankSpacer, parent: node)
+        spacerNode.canHaveChildNodes = false
+
+        let bookmarks = PseudoFolder.bookmarks
+        let bookmarksNode = BookmarkNode(representedObject: bookmarks, parent: node)
+        bookmarksNode.canHaveChildNodes = true
+
+        return [favoritesNode, spacerNode, bookmarksNode]
+    }
+
+    private func childNodes(for parentNode: BookmarkNode) -> [BookmarkNode] {
+        if let pseudoFolder = parentNode.representedObject as? PseudoFolder, pseudoFolder == PseudoFolder.bookmarks {
+            return childNodesForBookmarksPseudoFolder(parentNode)
+        }
+
+        if let folder = parentNode.representedObject as? BookmarkFolder {
+            return childNodes(for: folder, parentNode: parentNode)
+        }
+
+        return []
+    }
+
+    private func createNode(with folder: BookmarkFolder, parent: BookmarkNode) -> BookmarkNode {
+        let node = BookmarkNode(representedObject: folder, parent: parent)
+        node.canHaveChildNodes = !folder.childFolders.isEmpty
+
+        return node
+    }
+
+    private func childNodesForBookmarksPseudoFolder(_ parent: BookmarkNode) -> [BookmarkNode] {
+        let nodes = bookmarkManager.topLevelItems?.compactMap { (possibleFolder) -> BookmarkNode? in
+            guard let folder = possibleFolder as? BookmarkFolder else { return nil }
+
+            let folderNode = parent.findOrCreateChildNode(with: folder)
+            folderNode.canHaveChildNodes = !folder.childFolders.isEmpty
+
+            return folderNode
+        } ?? []
+
+        return nodes.bookmarksSortedAlphabetically()
+    }
+
+    private func childNodes(for folder: BookmarkFolder, parentNode: BookmarkNode) -> [BookmarkNode] {
+        var children = [BookmarkFolder]()
+        var updatedChildNodes = [BookmarkNode]()
+
+        for folder in folder.childFolders {
+            children.append(folder)
+        }
+
+        children.forEach { folder in
+            if let existingNode = parentNode.childNodeRepresenting(object: folder) {
+                if !updatedChildNodes.contains(existingNode) {
+                    updatedChildNodes += [existingNode]
+                    return
+                }
+            }
+
+            let newNode = self.createNode(with: folder, parent: parentNode)
+            updatedChildNodes += [newNode]
+        }
+
+        return updatedChildNodes.bookmarksSortedAlphabetically()
+    }
+
+}
---
+++ DuckDuckGo/Bookmarks/View/BookmarkTableCellView.swift
@@ -0,0 +1,268 @@
+//
+//  BookmarkTableCellView.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+@objc protocol BookmarkTableCellViewDelegate: AnyObject {
+
+    func bookmarkTableCellViewRequestedMenu(_ sender: NSButton, cell: BookmarkTableCellView)
+    func bookmarkTableCellViewToggledFavorite(cell: BookmarkTableCellView)
+
+}
+
+final class BookmarkTableCellView: NSTableCellView, NibLoadable {
+
+    static private var defaultBookmarkFavicon = NSImage(named: "Web")
+    static private var folderAccessoryViewImage = NSImage(named: "Forward")
+    static private var favoriteAccessoryViewImage = NSImage(named: "Favorite")
+    static private var favoriteFilledAccessoryViewImage = NSImage(named: "FavoriteFilledBorder")
+    static private var ellipsisAccessoryViewImage = NSImage(named: "Settings")
+
+    @IBOutlet var faviconImageView: NSImageView! {
+        didSet {
+            faviconImageView.wantsLayer = true
+            faviconImageView.layer?.cornerRadius = 2.0
+        }
+    }
+
+    @IBOutlet var titleLabel: NSTextField!
+    @IBOutlet var bookmarkURLLabel: NSTextField!
+    @IBOutlet var favoriteButton: NSButton!
+    @IBOutlet var accessoryImageView: NSImageView!
+    @IBOutlet var shadowView: NSView! {
+        didSet {
+            shadowView.isHidden = true
+            shadowView.wantsLayer = true
+            shadowView.layer?.backgroundColor = .white
+            shadowView.layer?.cornerRadius = 6
+
+            let shadow = NSShadow()
+            shadow.shadowOffset = NSSize(width: 0, height: -1)
+            shadow.shadowColor = NSColor.black.withAlphaComponent(0.2)
+            shadow.shadowBlurRadius = 2.0
+
+            shadowView.shadow = shadow
+        }
+    }
+
+    @IBOutlet var menuButton: NSButton! {
+        didSet {
+            menuButton.isHidden = true
+        }
+    }
+
+    // Shadow view constraints:
+
+    @IBOutlet var shadowViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet var shadowViewBottomConstraint: NSLayoutConstraint!
+
+    // Container view constraints:
+
+    @IBOutlet var titleLabelTopConstraint: NSLayoutConstraint!
+    @IBOutlet var titleLabelBottomConstraint: NSLayoutConstraint!
+
+    @IBAction func cellMenuButtonClicked(_ sender: NSButton) {
+        delegate?.bookmarkTableCellViewRequestedMenu(sender, cell: self)
+    }
+
+    @IBAction func favoriteButtonClicked(_ sender: NSButton) {
+        guard entity is Bookmark else {
+            assertionFailure("\(#file): Tried to favorite non-Bookmark object")
+            return
+        }
+
+        delegate?.bookmarkTableCellViewToggledFavorite(cell: self)
+    }
+
+    @IBOutlet weak var delegate: BookmarkTableCellViewDelegate?
+
+    var isEditing: Bool = false {
+        didSet {
+            if isEditing {
+                enterEditingMode()
+            } else {
+                exitEditingMode()
+            }
+        }
+    }
+
+    private var entity: BaseBookmarkEntity?
+    private var trackingArea: NSTrackingArea?
+    private var mouseInside: Bool = false {
+        didSet {
+            guard self.entity is Bookmark else {
+                menuButton.isHidden = true
+                return
+            }
+
+            accessoryImageView.isHidden = mouseInside || isEditing
+            menuButton.isHidden = !mouseInside || isEditing
+
+            if !mouseInside || isEditing {
+                resetAppearanceFromBookmark()
+            }
+
+            updateTitleLabelValue()
+        }
+    }
+
+    override var backgroundStyle: NSView.BackgroundStyle {
+        didSet {
+            faviconImageView.contentTintColor = .black
+            updateTitleLabelValue()
+        }
+    }
+
+    override func updateTrackingAreas() {
+        super.updateTrackingAreas()
+        ensureTrackingArea()
+        if !trackingAreas.contains(trackingArea!) {
+            addTrackingArea(trackingArea!)
+        }
+    }
+
+    override func mouseEntered(with event: NSEvent) {
+        mouseInside = true
+    }
+
+    override func mouseExited(with event: NSEvent) {
+        mouseInside = false
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        resetCellState()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        resetCellState()
+    }
+
+    func update(from bookmark: Bookmark) {
+        self.entity = bookmark
+
+        faviconImageView.image = bookmark.favicon ?? Self.defaultBookmarkFavicon
+        accessoryImageView.image = bookmark.isFavorite ? Self.favoriteAccessoryViewImage : nil
+        favoriteButton.image = bookmark.isFavorite ? Self.favoriteFilledAccessoryViewImage : Self.favoriteAccessoryViewImage
+        primaryTitleLabelValue = bookmark.title
+        tertiaryTitleLabelValue = bookmark.url.absoluteString
+        bookmarkURLLabel.stringValue = bookmark.url.absoluteString
+    }
+
+    func update(from folder: BookmarkFolder) {
+        self.entity = folder
+
+        faviconImageView.image = NSImage(named: "Folder")
+        accessoryImageView.image = Self.folderAccessoryViewImage
+        primaryTitleLabelValue = folder.title
+        tertiaryTitleLabelValue = nil
+    }
+
+    private func resetCellState() {
+        bookmarkURLLabel.isHidden = true
+        favoriteButton.isHidden = true
+        titleLabelBottomConstraint.priority = .required
+    }
+
+    private func enterEditingMode() {
+        titleLabel.isEditable = true
+        bookmarkURLLabel.isEditable = true
+
+        shadowViewTopConstraint.constant = 10
+        shadowViewBottomConstraint.constant = 10
+        titleLabelTopConstraint.constant = 12
+        shadowView.isHidden = false
+        faviconImageView.isHidden = true
+
+        bookmarkURLLabel.isHidden = false
+        favoriteButton.isHidden = false
+        titleLabelBottomConstraint.priority = .defaultLow
+
+        titleLabel.becomeFirstResponder()
+    }
+
+    private func exitEditingMode() {
+        titleLabel.resignFirstResponder()
+
+        titleLabel.isEditable = false
+        bookmarkURLLabel.isEditable = false
+
+        titleLabelTopConstraint.constant = 6
+        shadowViewTopConstraint.constant = 3
+        shadowViewBottomConstraint.constant = 3
+        shadowView.isHidden = true
+        faviconImageView.isHidden = false
+
+        bookmarkURLLabel.isHidden = true
+        favoriteButton.isHidden = true
+        titleLabelBottomConstraint.priority = .required
+    }
+
+    private func ensureTrackingArea() {
+        if trackingArea == nil {
+            trackingArea = NSTrackingArea(rect: .zero, options: [.inVisibleRect, .activeAlways, .mouseEnteredAndExited], owner: self, userInfo: nil)
+        }
+    }
+
+    /// Provides the primary value displayed in the title label. This value will be rendered with a black text color.
+    private var primaryTitleLabelValue = "" {
+        didSet {
+            updateTitleLabelValue()
+        }
+    }
+
+    /// Provides the tertiary value displayed in the title label when the cell is selected. This value will be rendered with a lighter text color to the main text.
+    private var tertiaryTitleLabelValue: String? {
+        didSet {
+            updateTitleLabelValue()
+        }
+    }
+
+    private func resetAppearanceFromBookmark() {
+        if let bookmark = self.entity as? Bookmark {
+            update(from: bookmark)
+        } else if let folder = self.entity as? BookmarkFolder {
+            update(from: folder)
+        } else {
+            assertionFailure("\(#file): Failed to update cell from bookmark entity")
+        }
+    }
+
+    private func updateTitleLabelValue() {
+        if let tertiaryValue = tertiaryTitleLabelValue, mouseInside, !isEditing {
+            titleLabel.stringValue = ""
+            titleLabel.attributedStringValue = buildTitleAttributedString(tertiaryValue: tertiaryValue)
+        } else {
+            titleLabel.attributedStringValue = NSAttributedString()
+            titleLabel.stringValue = primaryTitleLabelValue
+        }
+    }
+
+    private func buildTitleAttributedString(tertiaryValue: String) -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.foregroundColor: NSColor.labelColor]
+        let titleString = NSMutableAttributedString(string: primaryTitleLabelValue, attributes: titleAttributes)
+
+        let urlAttributes = [NSAttributedString.Key.foregroundColor: NSColor.tertiaryLabelColor]
+        let urlString = NSAttributedString(string: " – \(tertiaryValue)", attributes: urlAttributes)
+
+        titleString.append(urlString)
+
+        return titleString
+    }
+}
