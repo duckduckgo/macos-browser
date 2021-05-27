cf7d9151d42eb20efe2e12b504138308ce8fa668
Alexey Martemyanov <mallexxx@gmail.com>
2021-05-27 21:32:49 +0700

    Move Data.init?(dataHref:mimeType: to FileDownload/Model

--- DuckDuckGo/Common/Extensions/DataExtension.swift
+++ DuckDuckGo/Common/Extensions/DataExtension.swift
@@ -17,68 +17,11 @@
 //
 
 import Foundation
-import os.log
 
 extension Data {
 
     func utf8String() -> String? {
         return String(data: self, encoding: .utf8)
-    }
-
-    init?(dataHref: String, mimeType: inout String) {
-        var scanner = Scanner(string: dataHref)
-        guard scanner.scanString("data:") != nil,
-              // data:text/plain;charset=utf-8, plaindata
-              // data:application/pdf;base64,
-              var header = scanner.scanUpToString(",")?.trimmingWhitespaces() ?? .some(""),
-              scanner.scanString(",") != nil
-        else {
-            os_log("HTML5DownloadUserScript: unexpected data format: %s", type: .error, dataHref)
-            return nil
-        }
-        _=scanner.scanString(" ")
-        let body = dataHref[scanner.currentIndex...]
-
-        if header.isEmpty {
-            header = "text/plain;charset=US-ASCII"
-        }
-        scanner = Scanner(string: header)
-        // text/plain;charset=utf-8,
-        // application/pdf;base64,
-        // base64,
-        let mime = (header == "base64" ? nil : scanner.scanUpToString(";"))
-            ?? "text/plain"
-        _=scanner.scanString(";")
-        _=scanner.scanString(" ")
-
-        if scanner.scanString("base64") != nil {
-            guard let data = Data(base64Encoded: String(body)) else {
-                os_log("HTML5DownloadUserScript: could not decode base64-encoded data: %s", type: .error, dataHref)
-                return nil
-            }
-            self = data
-            mimeType = mime
-
-        } else {
-            let encoding: String.Encoding
-            if scanner.scanString("charset=") != nil,
-               let rawEncoding = scanner.scanUpToString(";")
-                .map({ $0 as CFString })
-                .map(CFStringConvertIANACharSetNameToEncoding)
-                .map(CFStringConvertEncodingToNSStringEncoding),
-               rawEncoding != UInt32.max {
-
-                encoding = String.Encoding(rawValue: rawEncoding)
-            } else {
-                encoding = .ascii
-            }
-            guard let data = body.data(using: encoding) else {
-                os_log("HTML5DownloadUserScript: could not decode data: %s", type: .error, dataHref)
-                return nil
-            }
-            self = data
-            mimeType = mime
-        }
     }
 
 }
