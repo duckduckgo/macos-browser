0ead6903bba726d1b2d3a5f61955f0f52bc76f0b
Chris Brind <brindy@duckduckgo.com>
2021-05-27 15:58:01 +0100

    prevent copying scripts as the generated source may not share state correctly

--- DuckDuckGo/BrowserTab/Model/UserScripts.swift
+++ DuckDuckGo/BrowserTab/Model/UserScripts.swift
@@ -35,9 +35,15 @@ final class UserScripts {
     init() {
     }
 
+// Copy means that the instance which generated the source is not the same instance that handles the message.
+//  This is require for the autofill script which uses a secret token to make sure the page isn't
+//   sending messages on old versions of WebKit.
+//
+// See AutofillUserScript.swift:157
+//
     init(copy other: UserScripts) {
         // copy compiled scripts to avoid repeated loading from disk
-        self.scripts = other.scripts
+//        self.scripts = other.scripts
     }
 
     lazy var userScripts: [UserScript] = [
