d1220ec6dde340be126a31c6c997534ab7604869
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2021-07-23 12:39:04 +0200

    Link to landing url from new tab page
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- DuckDuckGo/Common/Extensions/URLExtension.swift
+++ DuckDuckGo/Common/Extensions/URLExtension.swift
@@ -245,7 +245,7 @@ extension URL {
         return URL(string: "https://duckduckgo.com/about")!
     }
 
-    static var duckDuckGoEmail = URL(string: "https://duckduckgo.com/email/login")!
+    static var duckDuckGoEmail = URL(string: "https://duckduckgo.com/email-protection")!
 
     static var duckDuckGoMorePrivacyInfo = URL(string: "https://help.duckduckgo.com/duckduckgo-help-pages/privacy/atb/")!
 
