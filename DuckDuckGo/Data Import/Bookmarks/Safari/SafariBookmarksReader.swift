8c5ca77bde2d716c68e380ba1ed0fccac6207198
Sam Symons <sam@samsymons.com>
2021-09-08 20:57:51 -0700

    Remove a print statement.

--- DuckDuckGo/Data Import/Bookmarks/Safari/SafariBookmarksReader.swift
+++ DuckDuckGo/Data Import/Bookmarks/Safari/SafariBookmarksReader.swift
@@ -100,7 +100,6 @@ final class SafariBookmarksReader {
             return ImportedBookmarks.BookmarkOrFolder(name: title, type: "bookmark", urlString: url, children: nil)
         }
 
-        print("RETURNING NIL")
         return nil
     }
 
