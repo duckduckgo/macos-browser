c654ec155630926df97d08665d5c19186f156ba7
bstandaert-ddg <90369497+bstandaert-ddg@users.noreply.github.com>
2021-09-15 21:18:50 -0500

    add UI test for opening tabs

---
+++ UI Tests/TabBarTests.swift
@@ -0,0 +1,45 @@
+//
+//  TabBarTests.swift
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+
+class TabBarTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testOpeningTabs() throws {
+        let app = XCUIApplication()
+        
+        let tabbarviewitemElementsQuery = app.windows.collectionViews.otherElements.containing(.group, identifier: "TabBarViewItem")
+        // click on add tab button twice
+        tabbarviewitemElementsQuery.children(matching: .group).element(boundBy: 1).children(matching: .button).element.click()
+        tabbarviewitemElementsQuery.children(matching: .group).element(boundBy: 2).children(matching: .button).element.click()
+        
+        let tabs = app.windows.collectionViews.otherElements.containing(.group, identifier: "TabBarViewItem").children(matching: .group)
+            .matching(identifier: "TabBarViewItem")
+        
+        XCTAssertEqual(tabs.count, 3)
+    }
+
+}
