c0f33e663d068a6d733ff97014891a050fae92b1
Tomas Strba <tom@duckduckgo.com>
2021-09-29 15:06:21 +0200

    Typo fixed

--- Unit Tests/History/Model/HistoryCoordinatorTests.swift
+++ Unit Tests/History/Model/HistoryCoordinatorTests.swift
@@ -130,10 +130,9 @@ class HistoryCoordinatorTests: XCTestCase {
 
         let fireproofDomains = FireproofDomains()
         fireproofDomains.addToAllowed(domain: fireproofDomain)
-
         historyCoordinator.burnHistory(except: fireproofDomains) {
-            XCTAssert(historyStoringMock.cleanAndReloadHistoryExteptions.count == 1)
-            XCTAssert(historyStoringMock.cleanAndReloadHistoryExteptions.first!.url.host!.hasPrefix(fireproofDomain))
+            XCTAssert(historyStoringMock.cleanAndReloadHistoryExceptions.count == 1)
+            XCTAssert(historyStoringMock.cleanAndReloadHistoryExceptions.first!.url.host!.hasPrefix(fireproofDomain))
         }
     }
 
