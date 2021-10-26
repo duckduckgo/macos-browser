f1c117f22fd6cc545fd3ab0cadfab4f514da32d5
Tomas Strba <tom@duckduckgo.com>
2021-10-26 10:38:12 +0200

    Array extension moved to Common

--- DuckDuckGo/History/Services/HistoryStore.swift
+++ DuckDuckGo/History/Services/HistoryStore.swift
@@ -244,12 +244,3 @@ fileprivate extension HistoryEntryManagedObject {
     }
 
 }
-
-//TODO common
-fileprivate extension Array {
-    func chunked(into size: Int) -> [[Element]] {
-        return stride(from: 0, to: count, by: size).map {
-            Array(self[$0 ..< Swift.min($0 + size, count)])
-        }
-    }
-}
