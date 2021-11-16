ccbc060b5aba00f7a822a4da587400d3d44f40b3
Tomas Strba <tom@duckduckgo.com>
2021-11-16 19:14:19 +0100

    Unit tests fixed

--- Unit Tests/BrowserTab/ViewModel/TabViewModelTests.swift
+++ Unit Tests/BrowserTab/ViewModel/TabViewModelTests.swift
@@ -165,10 +165,10 @@ final class TabViewModelTests: XCTestCase {
 
         let faviconExpectation = expectation(description: "Favicon")
 
-        tabViewModel.$favicon.debounce(for: 0.3, scheduler: RunLoop.main).sink { favicon in
+        tabViewModel.$favicon.debounce(for: 1, scheduler: RunLoop.main).sink { favicon in
             XCTAssertNotNil(favicon)
             XCTAssertNotEqual(favicon, TabViewModel.Favicon.home)
-            if favicon != TabViewModel.Favicon.defaultFavicon {
+            if favicon != nil {
                 faviconExpectation.fulfill()
             }
         } .store(in: &cancellables)
