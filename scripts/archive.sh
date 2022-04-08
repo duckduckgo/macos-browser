0c1b39fe3966d908eaa01bcd7d82d376b5869bde
Dominik Kapusta <dkapusta@duckduckgo.com>
2022-04-08 14:25:32 +0200

    Add logging, make xcpretty optional and handle network errors

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
 CWD="$(dirname $0)"
 WORKDIR="${PWD}/release"
@@ -11,59 +11,84 @@ NOTARIZATION_INFO_PLIST="${WORKDIR}/notarization-info.plist"
 NOTARIZATION_STATUS_INFO_PLIST="${WORKDIR}/notarization-status-info.plist"
 
 SCHEME="Product Review Release"
+XCPRETTY="xcpretty"
 
-get_developer_credentials () {
+get_developer_credentials() {
     DEVELOPER_APPLE_ID="${XCODE_DEVELOPER_APPLE_ID}"
     DEVELOPER_PASSWORD="${XCODE_DEVELOPER_PASSWORD}"
     if [[ -z "${DEVELOPER_APPLE_ID}" ]]; then
-        read -p 'Apple ID: ' DEVELOPER_APPLE_ID
+        echo "Please enter Apple ID that will be used for requesting notarization"
+        echo "Set it in XCODE_DEVELOPER_APPLE_ID environment variable to not be asked again."
+        read -p "Apple ID: " DEVELOPER_APPLE_ID
+    else
+        echo "Using ${DEVELOPER_APPLE_ID} Apple ID"
     fi
-    if [[ -z "${DEVELOPER_PASSWORD}" ]]; then
-        read -sp 'Password: ' DEVELOPER_PASSWORD
+    while [[ -z "${DEVELOPER_PASSWORD}" ]]; do
+        echo "Set password in XCODE_DEVELOPER_PASSWORD environment variable to not be asked for password."
+        read -sp "Password for ${DEVELOPER_APPLE_ID}: " DEVELOPER_PASSWORD
         echo
-    fi
+    done
 }
 
-clean_working_directory () {
+clean_working_directory() {
     rm -rf "${WORKDIR}"
     mkdir -p "${WORKDIR}"
 }
 
-archive_and_export () {
+check_xcpretty() {
+    if ! command -v xcpretty &> /dev/null; then
+        echo
+        echo 'xcpretty not found - not prettifying Xcode logs. You can install it using `gem install xcpretty`.'
+        echo
+        XCPRETTY='tee'
+    fi
+}
+
+archive_and_export() {
+    echo
+    echo "Building and archiving the app ..."
+    echo
+
     xcrun xcodebuild archive \
         -scheme "${SCHEME}" \
         -archivePath "${WORKDIR}/DuckDuckGo" \
-        | xcpretty
+        | ${XCPRETTY}
+
+    echo
+    echo "Exporting archive ..."
+    echo
 
     xcrun xcodebuild -exportArchive \
         -archivePath "${ARCHIVE}.xcarchive" \
         -exportPath "${WORKDIR}" \
         -exportOptionsPlist "${CWD}/ExportOptions.plist" \
         -configuration Release \
-        | xcpretty
+        | ${XCPRETTY}
+}
+
+altool_upload() {
+    xcrun altool --notarize-app \
+        --primary-bundle-id "com.duckduckgo.macos.browser.review" \
+        -u "${DEVELOPER_APPLE_ID}" \
+        -p "${DEVELOPER_PASSWORD}" \
+        -f "${ZIP_PATH}" \
+        --output-format xml \
+        2>/dev/null \
+        > "${NOTARIZATION_INFO_PLIST}"
 }
 
 upload_for_notarization() {
     ditto -c -k --keepParent "${APP_PATH}" "${ZIP_PATH}"
 
     retries=3
-    while true
-    do
+    while true; do
+        echo
         printf '%s' "Uploading app for notarization ... "
 
-        xcrun altool --notarize-app \
-            --primary-bundle-id "com.duckduckgo.macos.browser.review" \
-            -u "${DEVELOPER_APPLE_ID}" \
-            -p "${DEVELOPER_PASSWORD}" \
-            -f "${ZIP_PATH}" \
-            --output-format xml \
-            2>/dev/null \
-            > "${NOTARIZATION_INFO_PLIST}"
-
-        if [[ $? -eq 0 ]]; then
+        if altool_upload; then
             echo "Done"
             break
-        elif [[ $? -ne 0 ]]; then
+        else
             echo "Failed to upload, retrying ..."
             retries=$((retries-1))
         fi
@@ -73,35 +98,40 @@ upload_for_notarization() {
             exit 1
         fi
     done
+    echo
 }
 
-get_notarization_info () {
+get_notarization_info() {
     echo $(/usr/libexec/PlistBuddy -c "Print :notarization-upload:RequestUUID" "${NOTARIZATION_INFO_PLIST}")
 }
 
-get_notarization_status () {
+get_notarization_status() {
     echo $(/usr/libexec/PlistBuddy -c "Print :notarization-info:Status" "${NOTARIZATION_STATUS_INFO_PLIST}")
+}
+
+altool_check_notarization_status () {
+    xcrun altool --notarization-info "$(get_notarization_info)" \
+        -u "${DEVELOPER_APPLE_ID}" \
+        -p "${DEVELOPER_PASSWORD}" \
+        --output-format xml \
+        2>/dev/null \
+        > "${NOTARIZATION_STATUS_INFO_PLIST}"
 }
 
 wait_for_notarization() {
     echo "Checking notarization status ..."
-    while true
-    do
-        xcrun altool --notarization-info "$(get_notarization_info)" \
-            -u "${DEVELOPER_APPLE_ID}" \
-            -p "${DEVELOPER_PASSWORD}" \
-            --output-format xml \
-            2>/dev/null \
-            > "${NOTARIZATION_STATUS_INFO_PLIST}"
-        if [[ $? -ne 0 ]]; then
+    while true; do
+        if altool_check_notarization_status; then
+            if [[ "$(get_notarization_status)" != "in progress" ]]; then
+                echo "Notarization complete"
+                break
+            else
+                echo "Still in progress, rechecking in 60 seconds ..."
+                sleep 60
+            fi
+        else
             echo "Failed to fetch notarization info, rechecking in 10 seconds ..."
             sleep 10
-        elif [[ "$(get_notarization_status)" != "in progress" ]]; then
-            echo "Notarization complete"
-            break
-        else
-            echo "Still in progress, rechecking in 60 seconds ..."
-            sleep 60
         fi
     done
 }
@@ -110,18 +140,20 @@ staple_notarized_app() {
     xcrun stapler staple "${APP_PATH}"
 }
 
-main () {
+main() {
     get_developer_credentials
     clean_working_directory
+    check_xcpretty
     archive_and_export
     upload_for_notarization
     wait_for_notarization
     staple_notarized_app
 
+    echo
     echo "Notarized app ready at ${APP_PATH}"
 
     if [[ -z $CI ]]; then
-        open $(dirname ${APP_PATH})
+        open "${WORKDIR}"
     fi
 }
 
