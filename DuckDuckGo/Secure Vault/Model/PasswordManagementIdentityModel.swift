4b4972eee0be18b0300463646102346034311ecc
Sam Symons <sam@samsymons.com>
2022-04-11 07:56:45 -0700

    Prevent the Autofill Cancel button from resetting item selection (#520)
    Task/Issue URL: https://app.asana.com/0/1177771139624306/1202040507157531/f
    Tech Design URL:
    CC: @ayoy
    
    Description:
    
    This PR removes the onCancelled block from the Autofill data models.
    
    This block was getting called after going into an editing state and then simply cancelling, triggering the list model to reselect the first item in the list.
    
    This block was introduced in the onboarding PR, but removing it appears to fix this issue with no negative side effects.

--- DuckDuckGo/Secure Vault/Model/PasswordManagementIdentityModel.swift
+++ DuckDuckGo/Secure Vault/Model/PasswordManagementIdentityModel.swift
@@ -33,7 +33,6 @@ final class PasswordManagementIdentityModel: ObservableObject, PasswordManagemen
     var onDirtyChanged: (Bool) -> Void
     var onSaveRequested: (SecureVaultModels.Identity) -> Void
     var onDeleteRequested: (SecureVaultModels.Identity) -> Void
-    var onCancelled: () -> Void
 
     var isEditingPublisher: Published<Bool>.Publisher {
         return $isEditing
@@ -167,12 +166,10 @@ final class PasswordManagementIdentityModel: ObservableObject, PasswordManagemen
 
     init(onDirtyChanged: @escaping (Bool) -> Void,
          onSaveRequested: @escaping (SecureVaultModels.Identity) -> Void,
-         onDeleteRequested: @escaping (SecureVaultModels.Identity) -> Void,
-         onCancelled: @escaping () -> Void) {
+         onDeleteRequested: @escaping (SecureVaultModels.Identity) -> Void) {
         self.onDirtyChanged = onDirtyChanged
         self.onSaveRequested = onSaveRequested
         self.onDeleteRequested = onDeleteRequested
-        self.onCancelled = onCancelled
     }
 
     func copy(_ value: String) {
@@ -192,8 +189,6 @@ final class PasswordManagementIdentityModel: ObservableObject, PasswordManagemen
             identity = nil
             isNew = false
         }
-
-        onCancelled()
     }
 
     func save() {
