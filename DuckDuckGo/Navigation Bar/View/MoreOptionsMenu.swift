e6c3db013b3c0f250fb428f0f8bb173b4d72fd67
Sam Symons <sam@samsymons.com>
2022-04-12 15:09:11 -0700

    ‘Rename user-facing “Preferences” strings to “Settings”.

--- DuckDuckGo/Navigation Bar/View/MoreOptionsMenu.swift
+++ DuckDuckGo/Navigation Bar/View/MoreOptionsMenu.swift
@@ -81,7 +81,7 @@ final class MoreOptionsMenu: NSMenu {
 
         addPageItems()
 
-        let preferencesItem = NSMenuItem(title: UserText.preferences, action: #selector(openPreferences(_:)), keyEquivalent: "")
+        let preferencesItem = NSMenuItem(title: UserText.settings, action: #selector(openPreferences(_:)), keyEquivalent: "")
             .targetting(self)
             .withImage(NSImage(named: "Preferences"))
             .firingPixel(Pixel.Event.MoreResult.preferences)
