ef146ecf94765610b51206fbddefd2cc1a287e7b
Maxim <maks.tsoy@gmail.com>
2022-04-22 18:49:43 +0200

    Add some debug logs to autoconsent background

--- DuckDuckGo/Autoconsent/background.js
+++ DuckDuckGo/Autoconsent/background.js
@@ -12,21 +12,28 @@ async function loadRules () {
     rules.autoconsent.forEach((rule) => {
         consent.addCMP(rule)
     })
+    console.log('rules loaded', consent.rules.length);
 }
 
 loadRules()
 
 browser.webNavigation.onCommitted.addListener((details) => {
     if (details.frameId === 0) {
+        console.log('Received onCommitted, removing tab', details.tabId)
         consent.removeTab(details.tabId)
     }
 }, {
     url: [{ schemes: ['http', 'https'] }]
 })
 
-browser.webNavigation.onCompleted.addListener(consent.onFrame.bind(consent), {
-    url: [{ schemes: ['http', 'https'] }]
-})
+browser.webNavigation.onCompleted.addListener(
+    (args) => {
+        console.log('Received onCompleted, running onFrame()', args);
+        return consent.onFrame(args);
+    }, {
+        url: [{ schemes: ['http', 'https'] }]
+    }
+)
 
 window.autoconsent = consent
 
@@ -43,9 +50,11 @@ window.callAction = (messageId, tabId, action) => {
     }
 
     if (action === 'detectCMP') {
+        console.log(`detecting cmp for tab ${tabId}`);
         consent.checkTab(tabId).then(async (cmp) => {
             try {
                 await cmp.checked
+                console.log('cmp detection finished', cmp.getCMPName());
                 respond({
                     ruleName: cmp.getCMPName(),
                     result: cmp.getCMPName() !== null
