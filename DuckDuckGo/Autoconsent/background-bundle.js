9662d5a94e143fe1685a91b10de74b04c3d80830
Maxim <maks.tsoy@gmail.com>
2022-04-29 11:40:03 +0300

    Commit the background bundle

--- DuckDuckGo/Autoconsent/background-bundle.js
+++ DuckDuckGo/Autoconsent/background-bundle.js
@@ -3904,21 +3904,28 @@
         autoconsent.forEach((rule) => {
             consent.addCMP(rule);
         });
+        console.log('rules loaded', consent.rules.length);
     }
 
     loadRules();
 
     browser.webNavigation.onCommitted.addListener((details) => {
         if (details.frameId === 0) {
+            console.log('Received onCommitted, removing tab', details.tabId);
             consent.removeTab(details.tabId);
         }
     }, {
         url: [{ schemes: ['http', 'https'] }]
     });
 
-    browser.webNavigation.onCompleted.addListener(consent.onFrame.bind(consent), {
-        url: [{ schemes: ['http', 'https'] }]
-    });
+    browser.webNavigation.onCompleted.addListener(
+        (args) => {
+            console.log('Received onCompleted, running onFrame()', args);
+            return consent.onFrame(args);
+        }, {
+            url: [{ schemes: ['http', 'https'] }]
+        }
+    );
 
     window.autoconsent = consent;
 
@@ -3935,9 +3942,11 @@
         };
 
         if (action === 'detectCMP') {
+            console.log(`detecting cmp for tab ${tabId}`);
             consent.checkTab(tabId).then(async (cmp) => {
                 try {
                     await cmp.checked;
+                    console.log('cmp detection finished', cmp.getCMPName());
                     respond({
                         ruleName: cmp.getCMPName(),
                         result: cmp.getCMPName() !== null
@@ -3957,7 +3966,8 @@
             const successResponse = (result) => respond({ ruleName: cmp.getCMPName(), result });
             switch (action) {
             case 'detectPopup':
-                cmp.isPopupOpen(20, 100).then(successResponse, errorResponse);
+                // give up after (20 * 200) ms
+                cmp.isPopupOpen(20, 200).then(successResponse, errorResponse);
                 break
             case 'doOptOut':
                 cmp.doOptOut().then(successResponse, errorResponse);
