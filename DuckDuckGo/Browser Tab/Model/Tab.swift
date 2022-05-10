e1a66f6b01a5676c66b06b0c12aa731ae67b634f
Dominik Kapusta <dkapusta@duckduckgo.com>
2022-05-10 14:28:14 +0200

    Do not store HTTP redirects in history (#569)
    Task/Issue URL: https://app.asana.com/0/1177771139624306/1202220970350463/f
    
    Description:
    Add Tab.isBeingRedirected that defaults to false, but is set to true when a server redirect occurs
    (decided based on a respective WKNavigationDelegate callback), and then cleared when the actual
    navigation starts, or when an error happens.
    Tab.addVisit is now called:
    * on webView didFinishNavigation callback
    * on URL change, but only when webView is not currently loading (filters out server redirects)

--- DuckDuckGo/Browser Tab/Model/Tab.swift
+++ DuckDuckGo/Browser Tab/Model/Tab.swift
@@ -211,6 +211,8 @@ final class Tab: NSObject, Identifiable {
 
     var isLazyLoadingInProgress = false
 
+    private var isBeingRedirected: Bool = false
+
     @Published private(set) var content: TabContent {
         didSet {
             handleFavicon(oldContent: oldValue)
@@ -889,6 +891,14 @@ extension Tab: WKNavigationDelegate {
         }
     }
 
+    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+        isBeingRedirected = true
+    }
+
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        isBeingRedirected = false
+    }
+
     struct Constants {
         static let webkitMiddleClick = 4
     }
@@ -1109,6 +1119,11 @@ extension Tab: WKNavigationDelegate {
 
     @MainActor
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if !isBeingRedirected, let url = webView.url {
+            addVisit(of: url)
+        }
+
+        isBeingRedirected = false
         invalidateSessionStateData()
         webViewDidFinishNavigationPublisher.send()
         if isAMPProtectionExtracting { isAMPProtectionExtracting = false }
@@ -1119,6 +1134,7 @@ extension Tab: WKNavigationDelegate {
         // https://app.asana.com/0/1199230911884351/1200381133504356/f
         //        hasError = true
 
+        isBeingRedirected = false
         webViewDidFailNavigationPublisher.send()
         invalidateSessionStateData()
     }
@@ -1133,6 +1149,7 @@ extension Tab: WKNavigationDelegate {
         }
 
         self.error = error
+        isBeingRedirected = false
         webViewDidFailNavigationPublisher.send()
     }
 
