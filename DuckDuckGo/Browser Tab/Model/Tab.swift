47b611db5c92ab590e77b2770cf8d80bda869559
Alexey Martemyanov <mallexxx@gmail.com>
2022-05-13 13:02:40 +0700

    Fix CBR waiting threading issue

--- DuckDuckGo/Browser Tab/Model/Tab.swift
+++ DuckDuckGo/Browser Tab/Model/Tab.swift
@@ -1046,6 +1046,7 @@ extension Tab: WKNavigationDelegate {
         setConnectionUpgradedTo(upgradedURL, navigationAction: navigationAction)
     }
 
+    @MainActor
     private func prepareForContentBlocking() async {
         // Ensure Content Blocking Assets (WKContentRuleList&UserScripts) are installed
         if !userContentController.contentBlockingAssetsInstalled {
