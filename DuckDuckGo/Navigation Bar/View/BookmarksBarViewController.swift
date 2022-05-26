38693f8827ad218370ba949caf65f786aff77703
Sam Symons <sam@samsymons.com>
2022-05-25 18:12:50 -0700

    Replace the collection view with a custom view.

--- DuckDuckGo/Navigation Bar/View/BookmarksBarViewController.swift
+++ DuckDuckGo/Navigation Bar/View/BookmarksBarViewController.swift
@@ -18,79 +18,158 @@
 
 import Foundation
 import AppKit
+import CryptoKit
 
 final class BookmarksBarViewController: NSViewController {
- 
-    @IBOutlet var bookmarksCollectionView: NSCollectionView!
     
-    let totalDataSourceCount: Int = 10
-    var currentMaximumCells: Int = 10 {
-        didSet {
-            bookmarksCollectionView.reloadData()
-        }
+    private enum Constants {
+        static let buttonSpacing: CGFloat = 12
+        static let buttonHeight: CGFloat = 28
+        static let maximumButtonWidth: CGFloat = 120
     }
     
+    private var buttons: [NSButton] = []
+    private var clippedButtons: [NSButton] = []
+    private var hasClippedButtons: Bool {
+        !clippedButtons.isEmpty
+    }
+    
+    // MARK: - Layout Calculation
+    
+    private var cumulativeButtonWidth: CGFloat = 0
+    private var cumulativeSpacingWidth: CGFloat = 0
+    private var totalButtonListWidth: CGFloat = 0
+    
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.postsFrameChangedNotifications = true
-        NotificationCenter.default.addObserver(self, selector: #selector(frameChanged), name: NSView.frameDidChangeNotification, object: self.view)
-
-        let nib = NSNib(nibNamed: BookmarksBarCollectionViewItem.identifier.rawValue, bundle: nil)
-        bookmarksCollectionView.register(nib, forItemWithIdentifier: BookmarksBarCollectionViewItem.identifier)
-        bookmarksCollectionView.delegate = self
-        bookmarksCollectionView.dataSource = self
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(frameChanged),
+                                               name: NSView.frameDidChangeNotification,
+                                               object: self.view)
         
-        (bookmarksCollectionView.collectionViewLayout as? NSCollectionViewGridLayout)?.minimumInteritemSpacing = 10.0
-        (bookmarksCollectionView.collectionViewLayout as? NSCollectionViewGridLayout)?.margins = NSEdgeInsets(top: 0, left: 0, bottom: 0, right: 10.0)
+        self.buttons = generateFakeButtons()
+        positionButtonsForInitialLayout()
+        layoutButtons()
     }
     
     @objc
     private func frameChanged() {
-        print("Frame changed \(self.bookmarksCollectionView.visibleItems().count)")
-        
-        calculateMaximumVisibleItems(viewWidth: view.bounds.width)
+        if view.frame.size.width <= (totalButtonListWidth + (Constants.buttonSpacing * 2)) {
+            removeLastButton()
+        } else {
+            tryToRestoreClippedButton()
+        }
     }
     
-    private func calculateMaximumVisibleItems(viewWidth: CGFloat) {
-        let cellWidth = 100
-        let spacing = 10
-        let totalCellWidth = CGFloat(cellWidth + spacing)
-        
-        let maximumVisibleCells = floor(viewWidth / totalCellWidth)
-        
-        print(maximumVisibleCells)
-        
-        currentMaximumCells = Int(maximumVisibleCells)
+    override func viewWillLayout() {
+        super.viewWillLayout()
+        layoutButtons()
     }
     
-}
+    override func viewDidLayout() {
+        super.viewDidLayout()
+    }
+    
+    private func positionButtonsForInitialLayout() {
+        for button in buttons {
+            view.addSubview(button)
+        }
+        
+        calculateFixedButtonSizingValues()
+    }
+    
+    private func tryToRestoreClippedButton() {
+        guard let firstClippedButton = clippedButtons.first else {
+            return
+        }
 
-extension BookmarksBarViewController: NSCollectionViewDataSource {
-    
-    func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-        return min(currentMaximumCells, totalDataSourceCount)
+        // Check if the next clipped button to restore can fit, and add it if so:
+        
+        let clippedButtonWidth = firstClippedButton.bounds.width
+        
+        // Button spacing * 3: Once for the padding between the last button and the new one,
+        // and two to account for the spacing at the beginning and end of the list.
+        if totalButtonListWidth + (Constants.buttonSpacing * 3) + clippedButtonWidth < view.bounds.width {
+            let buttonToRestore = clippedButtons.removeFirst()
+            buttons.append(buttonToRestore)
+            view.addSubview(buttonToRestore)
+            
+            calculateFixedButtonSizingValues()
+            layoutButtons()
+        }
     }
     
-    func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
-        let item = collectionView.makeItem(withIdentifier: BookmarksBarCollectionViewItem.identifier, for: indexPath)
-        guard let firePopoverItem = item as? BookmarksBarCollectionViewItem else { return item }
+    private func removeLastButton() {
+        guard let lastButton = buttons.popLast() else {
+            return
+        }
+
+        lastButton.removeFromSuperview()
+        clippedButtons.insert(lastButton, at: 0)
         
-        print("Made item at \(indexPath.item)")
-        
-        return firePopoverItem
+        calculateFixedButtonSizingValues()
+        layoutButtons()
     }
     
-}
+    private func calculateFixedButtonSizingValues() {
+        self.cumulativeButtonWidth = buttons.map(\.bounds.size.width).reduce(0, +)
+        self.cumulativeSpacingWidth = Constants.buttonSpacing * CGFloat(max(0, buttons.count - 1))
+        self.totalButtonListWidth = cumulativeButtonWidth + cumulativeSpacingWidth
+    }
 
-extension BookmarksBarViewController: NSCollectionViewDelegate {
-    
-}
+    private func layoutButtons() {
+        var previousMaximumXValue: CGFloat
+        
+        // If there are any clipped buttons, the button list should always be leading-aligned.
+        if hasClippedButtons {
+            previousMaximumXValue = Constants.buttonSpacing
+        } else {
+            previousMaximumXValue = max(Constants.buttonSpacing, (view.bounds.midX) - (self.totalButtonListWidth / 2))
+        }
 
-extension BookmarksBarViewController: NSCollectionViewDelegateFlowLayout {
+        for button in buttons {
+            var updatedButtonFrame = button.frame
+            updatedButtonFrame.origin = CGPoint(x: previousMaximumXValue, y: view.frame.midY - (button.frame.height / 2) + 3)
+            button.frame = updatedButtonFrame
+            
+            previousMaximumXValue = updatedButtonFrame.maxX + Constants.buttonSpacing
+        }
+    }
     
-    func collectionView(_ collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> NSSize {
-        return NSSize(width: 100, height: 20)
+    private func generateFakeButtons() -> [NSButton] {
+        return [
+            bookmarkButton(titled: "Testing Testing Testing 1"),
+            bookmarkButton(titled: "Test 2"),
+            bookmarkButton(titled: "Test 3"),
+            bookmarkButton(titled: "Test 4?!"),
+            bookmarkButton(titled: "Test 5"),
+            bookmarkButton(titled: "Test Test 6"),
+            bookmarkButton(titled: "Testing 7: Still Testing"),
+            bookmarkButton(titled: "Test Test 8"),
+            bookmarkButton(titled: "Test Test 9"),
+            bookmarkButton(titled: "Test Test 10"),
+            bookmarkButton(titled: "Test Test 11"),
+            bookmarkButton(titled: "Test Test 12")
+        ]
+    }
+    
+    private func bookmarkButton(titled title: String) -> NSButton {
+        let button = NSButton(frame: .zero)
+        button.isBordered = false
+        button.title = title
+        button.sizeToFit()
+        
+        var buttonFrame = button.frame
+        buttonFrame.size.width = min(Constants.maximumButtonWidth, buttonFrame.size.width)
+        button.frame = buttonFrame
+        
+        button.lineBreakMode = .byTruncatingTail
+
+        return button
     }
     
 }
