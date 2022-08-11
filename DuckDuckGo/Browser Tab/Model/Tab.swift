26b865bc90c12bb578c8eecf4e0166250a74994b
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2022-08-11 10:32:05 +0200

    Update BSK to latest version + noop updates (#669)

--- DuckDuckGo/Browser Tab/Model/Tab.swift
+++ DuckDuckGo/Browser Tab/Model/Tab.swift
@@ -930,6 +930,7 @@ extension Tab: SecureVaultManagerDelegate {
     func secureVaultManager(_: SecureVaultManager,
                             promptUserToAutofillCredentialsForDomain domain: String,
                             withAccounts accounts: [SecureVaultModels.WebsiteAccount],
+                            withTrigger trigger: AutofillUserScript.GetTriggerType,
                             completionHandler: @escaping (SecureVaultModels.WebsiteAccount?) -> Void) {
         // no-op on macOS
     }
