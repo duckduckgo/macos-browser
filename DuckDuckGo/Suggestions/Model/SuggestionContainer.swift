e3474692857cc5a64897aae8e14ca8731fd40af0
Michal Smaga <miasma13@gmail.com>
2022-09-09 10:58:25 +0200

    Use new API method for adding multiple parameters

--- DuckDuckGo/Suggestions/Model/SuggestionContainer.swift
+++ DuckDuckGo/Suggestions/Model/SuggestionContainer.swift
@@ -97,11 +97,7 @@ extension SuggestionContainer: SuggestionLoadingDataSource {
                            suggestionDataFromUrl url: URL,
                            withParameters parameters: [String: String],
                            completion: @escaping (Data?, Error?) -> Void) {
-        var url = url
-        parameters.forEach {
-            url = url.appendingParameter(name: $0.key, value: $0.value)
-        }
-        
+        var url = url.appendingParameters(parameters)
         var request = URLRequest.defaultRequest(with: url)
         request.timeoutInterval = 1
 
