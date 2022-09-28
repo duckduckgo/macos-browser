d6059f82283a12f0ab44f6498a130693b5f249b9
Chris Brind <brindy@duckduckgo.com>
2022-09-28 12:18:20 +0100

    call refresh now on startup

--- DuckDuckGo/Configuration/ConfigurationManager.swift
+++ DuckDuckGo/Configuration/ConfigurationManager.swift
@@ -70,6 +70,7 @@ final class ConfigurationManager {
                 self.lastRefreshCheckTime = Date()
                 self.refreshIfNeeded()
             })
+        refreshNow()
     }
 
     func log() {
