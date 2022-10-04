0a765efe816f3722ee7f8896ba9e218dad72b412
Dominik Kapusta <dkapusta@duckduckgo.com>
2022-10-04 20:30:12 +0200

    Remove unused files

--- DuckDuckGo/Youtube Player/Resources/private-player-inject.js
+++
@@ -1,5 +0,0 @@
-alert("Private Player Content Script was loaded.");
-
-window.addEventListener('message', (e) => {
-   alert('Private Player Content Script received a message from the Private Player HTML-page');
-});
