cacf3834d9e7cb216fdf066d8d533366df31f9a5
Shane Osbourne <shane.osbourne8@gmail.com>
2022-10-06 10:17:29 +0100

    updated copy

--- js/youtube-player/src/text.js
+++ js/youtube-player/src/text.js
@@ -9,7 +9,7 @@ const text = {
         "title": "Tired of targeted YouTube ads and recommendations?"
     },
     "videoOverlaySubtitle": {
-        "title": "<b>Duck Player</b> protects your viewing activity from advertiser profiling and from influencing your YouTube recommendations."
+        "title": "<b>Duck Player</b> provides a clean viewing experience without personalized ads and prevents viewing activity from influencing your YouTube recommendations."
     },
     "videoButtonOpen": {
         "title": "Watch in Duck Player"
