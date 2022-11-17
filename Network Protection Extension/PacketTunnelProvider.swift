21650590260f7d9d47b02eb685ef121555413037
Sam Symons <sam@samsymons.com>
2022-11-16 20:19:36 -0800

    Add a template packet tunnel provider (#2)
    Task/Issue URL: https://app.asana.com/0/0/1203340357678926/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR adds an empty packet tunnel provider to the macOS browser. It uses Network Protection Extension as the target name, but uses DuckDuckGo Network Protection as the product name, since that is what will be visible to users who are looking in Activity Monitor etc.

---
+++ Network Protection Extension/PacketTunnelProvider.swift
@@ -0,0 +1,47 @@
+//
+//  PacketTunnelProvider.swift
+//
+//  Copyright © 2022 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import NetworkExtension
+
+final class PacketTunnelProvider: NEPacketTunnelProvider {
+
+    override func startTunnel(options: [String: NSObject]?, completionHandler: @escaping (Error?) -> Void) {
+        // Add code here to start the process of connecting the tunnel.
+    }
+    
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // Add code here to start the process of stopping the tunnel.
+        completionHandler()
+    }
+    
+    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
+        // Add code here to handle the message.
+        if let handler = completionHandler {
+            handler(messageData)
+        }
+    }
+    
+    override func sleep(completionHandler: @escaping () -> Void) {
+        // Add code here to get ready to sleep.
+        completionHandler()
+    }
+    
+    override func wake() {
+        // Add code here to wake up.
+    }
+}
