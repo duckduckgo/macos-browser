5ff802e36c2c2f14a148b72f0749f6d475d0d2ca
Alexey Martemyanov <mallexxx@gmail.com>
2022-11-17 16:35:26 +0600

    Dependency-injection based WKWebViewConfiguration

--- DuckDuckGo/Common/Extensions/WKWebViewConfigurationExtensions.swift
+++ DuckDuckGo/Common/Extensions/WKWebViewConfigurationExtensions.swift
@@ -22,39 +22,85 @@ import BrowserServicesKit
 
 extension WKWebViewConfiguration {
 
-    func applyStandardConfiguration() {
-#if DEBUG
-        guard !AppDelegate.isRunningTests else { return }
-#endif
+    struct Dependencies {
+        @Injected(.testable) static var privatePlayerSchemeHandler: WKURLSchemeHandler = PrivatePlayerSchemeHandler()
+        @Injected(.testable) static var preferences: [WKWebViewConfiguration.Preference] = [
+            \WKWebViewConfiguration.allowsAirPlayForMediaPlayback: true,
+            \WKWebViewConfiguration.preferences.javaScriptCanOpenWindowsAutomatically: true,
+            \WKWebViewConfiguration.preferences.isFraudulentWebsiteWarningEnabled: false,
+            "preferences.fullScreenEnabled": true,
+            "preferences.allowsPictureInPictureMediaPlayback": true,
+            "preferences.developerExtrasEnabled": true,
+            "preferences.backspaceKeyNavigationEnabled": false
+        ]
 
-        allowsAirPlayForMediaPlayback = true
-        preferences.setValue(true, forKey: "fullScreenEnabled")
-        preferences.setValue(true, forKey: "allowsPictureInPictureMediaPlayback")
-        preferences.setValue(true, forKey: "developerExtrasEnabled")
-        preferences.setValue(false, forKey: "backspaceKeyNavigationEnabled")
-        if WKNavigationAction.supportsIsUserInitiated {
-            preferences.javaScriptCanOpenWindowsAutomatically = true
-        } else {
-            assertionFailure("WKNavigationAction does not respond to _isUserInitiated")
-            preferences.javaScriptCanOpenWindowsAutomatically = false
+        @Injected(forTests: PassthroughSubject().eraseToAnyPublisher()) static var userContentBlockingAssets: AnyPublisher =
+            ContentBlocking.shared.userContentUpdating.userContentBlockingAssets
+
+        private static var privacyConfigurationManagerMock: PrivacyConfigurationManaging {
+            ((NSClassFromString("MockPrivacyConfigurationManager") as? NSObject.Type)!.init() as? PrivacyConfigurationManaging)!
         }
-        preferences.isFraudulentWebsiteWarningEnabled = false
-        
+        @Injected(forTests: privacyConfigurationManagerMock) static var privacyConfigurationManager: PrivacyConfigurationManaging =
+            ContentBlocking.shared.privacyConfigurationManager
+        @Injected(.testable) static var geolocationProviderFactory: (WKProcessPool) -> GeolocationProviderProtocol? = {
+            GeolocationProvider(processPool: $0, geolocationService: GeolocationService.shared)
+        }
+    }
+
+    func applyStandardConfiguration(with delegate: UserContentControllerDelegate) {
+        self.apply(Dependencies.preferences)
+
         if urlSchemeHandler(forURLScheme: PrivatePlayer.privatePlayerScheme) == nil {
-            setURLSchemeHandler(PrivatePlayerSchemeHandler(), forURLScheme: PrivatePlayer.privatePlayerScheme)
+            setURLSchemeHandler(Dependencies.privatePlayerSchemeHandler, forURLScheme: PrivatePlayer.privatePlayerScheme)
         }
 
-        self.userContentController = UserContentController()
-        self.processPool.geolocationProvider = GeolocationProvider(processPool: self.processPool)
+        let userContentController = UserContentController(assetsPublisher: Dependencies.userContentBlockingAssets,
+                                                          privacyConfigurationManager: Dependencies.privacyConfigurationManager)
+        userContentController.delegate = delegate
+        self.userContentController = userContentController
+
+        self.processPool.geolocationProvider = Dependencies.geolocationProviderFactory(self.processPool)
      }
 
 }
 
-extension UserContentController {
+extension WKWebViewConfiguration {
+    public struct Preference {
+        let kvcKeyPathOrString: KVCKeyPathOrString
+        let value: Any
 
-    convenience init(privacyConfigurationManager: PrivacyConfigurationManager = ContentBlocking.shared.privacyConfigurationManager) {
-        self.init(assetsPublisher: ContentBlocking.shared.userContentUpdating.userContentBlockingAssets,
-                  privacyConfigurationManager: privacyConfigurationManager)
+        static func apply(to configuration: WKWebViewConfiguration) -> (Preference) -> Void {
+            { preference in
+                guard let key = preference.kvcKeyPathOrString.keyPath else {
+                    assertionFailure("\(preference.kvcKeyPathOrString)._kvcKeyPathString not available")
+                    return
+                }
+
+                NSException.try {
+                    configuration.setValue(preference.value, forKeyPath: key)
+                }.map {
+                    assertionFailure("\($0)")
+                }
+            }
+        }
     }
 
+    func apply(_ preferences: [Preference]) {
+        preferences.forEach(Preference.apply(to: self))
+    }
+}
+
+public protocol KVCKeyPathOrString {
+    var keyPath: String? { get }
+}
+extension ReferenceWritableKeyPath: KVCKeyPathOrString where Root == WKWebViewConfiguration {
+    public var keyPath: String? { _kvcKeyPathString }
+}
+extension String: KVCKeyPathOrString {
+    public var keyPath: String? { self }
+}
+extension [WKWebViewConfiguration.Preference]: ExpressibleByDictionaryLiteral {
+    public init(dictionaryLiteral elements: (KVCKeyPathOrString, Any)...) {
+        self = elements.compactMap(WKWebViewConfiguration.Preference.init)
+    }
 }
