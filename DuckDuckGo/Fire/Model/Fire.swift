e234339ebd310117fe9ef42a0450d4f720f11db4
Sam Symons <sam@samsymons.com>
2022-11-28 12:13:02 -0800

    Fix SwiftLint warnings.

--- DuckDuckGo/Fire/Model/Fire.swift
+++ DuckDuckGo/Fire/Model/Fire.swift
@@ -137,6 +137,7 @@ final class Fire {
         }
     }
 
+    // swiftlint:disable:next function_body_length
     func burnDomains(_ domains: Set<String>,
                      includingHistory: Bool = true,
                      completion: (() -> Void)? = nil) {
