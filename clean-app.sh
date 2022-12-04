198ea5ece5ca3bcdf88c60f5365588c5e3f90396
Dominik Kapusta <dkapusta@duckduckgo.com>
2022-12-04 13:47:54 +0100

    Add support for sandbox apps to clean-app.sh

--- clean-app.sh
+++ clean-app.sh
@@ -1,13 +1,23 @@
 
 if [ "$1" == "debug" ]; then
   echo "removing com.duckduckgo.macos.browser.$1"
+  defaults delete com.duckduckgo.macos.browser.$1
+  rm -rf ~/Library/Containers/com.duckduckgo.macos.browser.$1
 elif [ "$1" == "review" ]; then
   echo "removing com.duckduckgo.macos.browser.$1"
+  defaults delete com.duckduckgo.macos.browser.$1
+  rm -rf ~/Library/Containers/com.duckduckgo.macos.browser.$1
+elif [ "$1" == "debug-sandbox" ]; then
+  echo "removing com.duckduckgo.mobile.ios.debug"
+  defaults delete com.duckduckgo.mobile.ios.debug
+  rm -rf ~/Library/Containers/com.duckduckgo.mobile.ios.debug
+elif [ "$1" == "review-sandbox" ]; then
+  echo "removing com.duckduckgo.mobile.ios.review"
+  defaults delete com.duckduckgo.mobile.ios.review
+  rm -rf ~/Library/Containers/com.duckduckgo.mobile.ios.review
 else 
   echo "usage: clean-app debug|review"
   exit 1
 fi 
 
-defaults delete com.duckduckgo.macos.browser.$1
-rm -rf ~/Library/Containers/com.duckduckgo.macos.browser.$1
 
