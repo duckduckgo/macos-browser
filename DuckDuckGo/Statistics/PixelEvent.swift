7d71bd51db49ce829f32207ade13ae32a6033153
Elle Sullivan <esullivan@duckduckgo.com>
2022-12-05 18:03:27 +0000

    Remove unnecessary references to identifier_name

--- DuckDuckGo/Statistics/PixelEvent.swift
+++ DuckDuckGo/Statistics/PixelEvent.swift
@@ -19,7 +19,6 @@
 import Foundation
 import BrowserServicesKit
 
-// swiftlint:disable identifier_name
 extension Pixel {
 
     enum Event {
@@ -195,7 +194,6 @@ extension Pixel {
 
     }
 }
-// swiftlint:enable identifier_name
 
 extension Pixel.Event {
 
