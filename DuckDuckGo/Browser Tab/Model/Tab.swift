77eebb8c845d7fa7c149db21bb2b3ad06e24a055
Alexey Martemyanov <mallexxx@gmail.com>
2022-12-12 12:08:31 +0600

    minor cleanup

--- DuckDuckGo/Browser Tab/Model/Tab.swift
+++ DuckDuckGo/Browser Tab/Model/Tab.swift
@@ -1049,7 +1049,6 @@ extension Tab: ContentBlockerRulesUserScriptDelegate {
         guard let url = webView.url else { return }
         
         privacyInfo?.trackerInfo.addDetectedTracker(tracker, onPageWithURL: url)
-        self.adClickAttribution?.logic.onRequestDetected(request: tracker)
         historyCoordinating.addDetectedTracker(tracker, onURL: url)
     }
 
