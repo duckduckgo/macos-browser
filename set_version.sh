e17dc7a75aff47ea1d3d1b5505d1a63dd29170f7
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-02-02 22:56:36 +0100

    Update set_version.sh to increment build number every time it's called

--- set_version.sh
+++ set_version.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 version_number="$1"
-build_number="${2:-0}"
 
 if [[ -z "${version_number}" ]]; then
-   echo 'Usage: ./set_version.sh VERSION_NUMBER [BUILD_NUMBER=0]'
-   echo 'Example: ./set_version.sh 0.28.6 0'
+   echo 'Usage: ./set_version.sh VERSION_NUMBER'
+   echo 'Example: ./set_version.sh 0.28.6'
    echo "Current version: $(cut -d ' ' -f 3 <Configuration/Version.xcconfig)"
    exit 1
 fi
 
-printf 'APP_VERSION = %s' "${version_number}" > Configuration/Version.xcconfig
-printf 'CURRENT_PROJECT_VERSION = %s' "${build_number}" > Configuration/AppStoreBuildNumber.xcconfig
+current_build_number=$(cut -d ' ' -f 3 <Configuration/AppStoreBuildNumber.xcconfig)
+next_build_number=$(( current_build_number + 1 ))
+
+printf 'APP_VERSION = %s\n' "${version_number}" | tee Configuration/Version.xcconfig
+printf 'CURRENT_PROJECT_VERSION = %s\n' "${next_build_number}" | tee Configuration/AppStoreBuildNumber.xcconfig
