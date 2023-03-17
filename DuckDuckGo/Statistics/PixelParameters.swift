7029f82ba5c4ed3ee55edaf7f5a4f08f02ddec6c
Sam Symons <sam@samsymons.com>
2023-03-16 20:30:37 -0700

    Send Keychain error information with the encryption store failure pixels (#1030)
    Task/Issue URL: https://app.asana.com/0/1199230911884351/1204181429340400/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR adds a new pixel parameter to debug pixels. It does this by introducing a new ErrorWithParameters protocol which the Pixel file checks for. We could use NSError here instead perhaps, but it was done this way to limit the scope of changes and ensure that the error pixel name/format doesn't change – it simply adds one new parameter. I'm open to changing this though.

--- DuckDuckGo/Statistics/PixelParameters.swift
+++ DuckDuckGo/Statistics/PixelParameters.swift
@@ -30,6 +30,7 @@ extension Pixel {
         static let underlyingErrorDesc = "ud"
         static let underlyingErrorSQLiteCode = "sqlrc"
         static let underlyingErrorSQLiteExtendedCode = "sqlerc"
+        static let keychainErrorCode = "keychain_error_code"
 
         static let emailCohort = "cohort"
         static let emailLastUsed = "duck_address_last_used"
@@ -52,6 +53,10 @@ extension Pixel.Event {
         case .debug(event: let debugEvent, error: let error):
             var params = [String: String]()
 
+            if let errorWithUserInfo = error as? ErrorWithParameters {
+                params = errorWithUserInfo.errorParameters
+            }
+
             if case let .assertionFailure(message, file, line) = debugEvent {
                 params[Pixel.Parameters.assertionMessage] = message
                 params[Pixel.Parameters.assertionFile] = String(file)
@@ -63,13 +68,16 @@ extension Pixel.Event {
 
                 params[Pixel.Parameters.errorCode] = "\(nsError.code)"
                 params[Pixel.Parameters.errorDesc] = nsError.domain
+
                 if let underlyingError = nsError.userInfo["NSUnderlyingError"] as? NSError {
                     params[Pixel.Parameters.underlyingErrorCode] = "\(underlyingError.code)"
                     params[Pixel.Parameters.underlyingErrorDesc] = underlyingError.domain
                 }
+
                 if let sqlErrorCode = nsError.userInfo["SQLiteResultCode"] as? NSNumber {
                     params[Pixel.Parameters.underlyingErrorSQLiteCode] = "\(sqlErrorCode.intValue)"
                 }
+
                 if let sqlExtendedErrorCode = nsError.userInfo["SQLiteExtendedResultCode"] as? NSNumber {
                     params[Pixel.Parameters.underlyingErrorSQLiteExtendedCode] = "\(sqlExtendedErrorCode.intValue)"
                 }
