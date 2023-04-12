5b97f5d0841cbef1b35b0bd27c3849eebdba9c20
Daniel Bernal <dbernal@duckduckgo.com>
2023-04-12 17:01:16 +0200

    Avoid removing Favicons from saved logins

--- DuckDuckGo/Fire/Model/Fire.swift
+++ DuckDuckGo/Fire/Model/Fire.swift
@@ -94,13 +94,14 @@ final class Fire {
     let stateRestorationManager: AppStateRestorationManager?
     let recentlyClosedCoordinator: RecentlyClosedCoordinating?
     let pinnedTabsManager: PinnedTabsManager
-
     let tabsCleaner = TabDataCleaner()
+    let secureVaultFactory: SecureVaultFactory
 
     enum BurningData {
         case specificDomains(_ domains: Set<String>)
         case all
     }
+
     @Published private(set) var burningData: BurningData?
 
     init(cacheManager: WebCacheManager = WebCacheManager.shared,
@@ -112,7 +113,8 @@ final class Fire {
          autoconsentManagement: AutoconsentManagement? = nil,
          stateRestorationManager: AppStateRestorationManager? = nil,
          recentlyClosedCoordinator: RecentlyClosedCoordinating? = RecentlyClosedCoordinator.shared,
-         pinnedTabsManager: PinnedTabsManager = WindowControllersManager.shared.pinnedTabsManager
+         pinnedTabsManager: PinnedTabsManager = WindowControllersManager.shared.pinnedTabsManager,
+         secureVaultFactory: SecureVaultFactory = SecureVaultFactory.default
     ) {
         self.webCacheManager = cacheManager
         self.historyCoordinating = historyCoordinating
@@ -122,6 +124,7 @@ final class Fire {
         self.faviconManagement = faviconManagement
         self.recentlyClosedCoordinator = recentlyClosedCoordinator
         self.pinnedTabsManager = pinnedTabsManager
+        self.secureVaultFactory = secureVaultFactory
 
         if #available(macOS 11, *), autoconsentManagement == nil {
             self.autoconsentManagement = AutoconsentManagement.shared
@@ -343,15 +346,27 @@ final class Fire {
 
     // MARK: - Favicons
 
+    private func autofillDomains() -> Set<String> {
+        guard let vault = try? secureVaultFactory.makeVault(errorReporter: SecureVaultErrorReporter.shared),
+              let accounts = try? vault.accounts() else {
+            return []
+        }
+        return Set(accounts.map { $0.domain })
+    }
+
     private func burnFavicons(completion: @escaping () -> Void) {
+        let autofillDomains = autofillDomains()
         self.faviconManagement.burnExcept(fireproofDomains: FireproofDomains.shared,
                                           bookmarkManager: LocalBookmarkManager.shared,
+                                          savedLogins: autofillDomains,
                                           completion: completion)
     }
 
     private func burnFavicons(for domains: Set<String>, completion: @escaping () -> Void) {
+        let autofillDomains = autofillDomains()
         self.faviconManagement.burnDomains(domains,
-                                           except: LocalBookmarkManager.shared,
+                                           exceptBookmarks: LocalBookmarkManager.shared,
+                                           exceptSavedLogins: autofillDomains,
                                            completion: completion)
     }
 
