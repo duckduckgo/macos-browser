205477e50d3dd83414704322884361574f93e248
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-04-21 16:24:39 +0200

    Drop unwanted titles from Imported Logins (#1145)
    Task/Issue URL: https://app.asana.com/0/1204099484721401/1204318806095418/f
    
    Description:
    When importing logins, remove the title when it matches the URL or the Title matches a specific pattern.

--- UnitTests/DataImport/CSVImporterTests.swift
+++ UnitTests/DataImport/CSVImporterTests.swift
@@ -68,6 +68,26 @@ class CSVImporterTests: XCTestCase {
         XCTAssertEqual(logins, [ImportedLoginCredential(title: "Some Title", url: "duck.com", username: "username", password: "p4ssw0rd")])
     }
 
+    func testWhenImportingLoginsWhichTitlePatternsMatchTheURL_ThenRemoveTheTitle() {
+        let csvFileContents = """
+        ,duck.com,username,p4ssw0rd
+        duck.com,duck.com,username,p4ssw0rd
+        duck.com (test@duck.com),duck.com,username,p4ssw0rd
+        signin.duck.com (test@duck.com.co),signin.duck.com,username,p4ssw0rd
+        http://signin.duck.com (test@duck.com.co),signin.duck.com,username,p4ssw0rd
+        https://signin.duck.com (test@duck.com.co),signin.duck.com,username,p4ssw0rd
+        http://signin.duck.com,signin.duck.com,username,p4ssw0rd
+        https://signin.duck.com,signin.duck.com,username,p4ssw0rd
+        https://signin.duck.com/page.php?test=variable1&b=variable2,signin.duck.com,username,p4ssw0rd
+        https://signin.duck.com/section/page.php?test=variable1&b=variable2,signin.duck.com,username,p4ssw0rd
+        """
+
+        let logins = CSVImporter.extractLogins(from: csvFileContents)
+        for login in logins {
+            XCTAssertEqual(login.title, nil)
+        }
+    }
+
     func testWhenImportingCSVDataFromTheFileSystem_AndNoTitleIsIncluded_ThenLoginCredentialsAreImported() {
         let mockLoginImporter = MockLoginImporter()
         let file = "https://example.com/,username,password"
