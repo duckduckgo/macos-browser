053425cc3dfc3db550a327d54e5dcb2a1a0e9c1d
Tomas Strba <tom@duckduckgo.com>
2023-04-23 20:31:38 +0200

    Burner Window homepage refactored based on the latest design

--- DuckDuckGo/HomePage/View/DisposableHomePageView.swift
+++ DuckDuckGo/HomePage/View/DisposableHomePageView.swift
@@ -23,6 +23,12 @@ extension HomePage.Views {
 
     struct DisposableHomePageView: View {
 
+        @Environment(\.colorScheme) var colorScheme
+
+        private var backgroundColor: Color {
+            return colorScheme == .dark ? Color.black.opacity(0.15) : Color.white
+        }
+
         var body: some View {
             ZStack {
                 Image("BurnerWindowBackground")
@@ -30,12 +36,13 @@ extension HomePage.Views {
                     .scaledToFill()
                     .edgesIgnoringSafeArea(.all)
 
-                VStack {
+                VStack(alignment: .leading, spacing: 15) {
                     HStack {
                         Image("BurnerWindowPopoverImage")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 64, height: 48)
+                            .padding(.leading, -15)
 
                         Text("Burner Window")
                             .font(.system(size: 22, weight: .bold))
@@ -50,53 +57,51 @@ extension HomePage.Views {
                     FeaturesBox()
                         .padding(.top, 10)
                 }
-                .frame(width: 600, height: 600, alignment: .center)
-                .padding(.top, -50)
+                .frame(width: 480, height: 260, alignment: .center)
+                .padding()
+                .padding(10)
+                .background(RoundedRectangle(cornerRadius: 8).fill(backgroundColor))
+                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.3), lineWidth: 1))
+                .shadow(color: Color.black.opacity(0.05), radius: 2, x: 0, y: 2)
             }
         }
     }
 
     struct Description: View {
         var body: some View {
-            Text("Unlike other browsers, ")
-                .font(.system(size: 15))
-                .foregroundColor(Color.primary)
-            + Text("all")
-                .font(.system(size: 15, weight: .bold))
-                .foregroundColor(Color.primary)
-            + Text(" DuckDuckGo windows are private.")
-                .font(.system(size: 15))
-                .foregroundColor(Color.primary)
+            VStack(alignment: .leading, spacing: 15) {
+                Text("All" + " ")
+                    .font(.system(size: 15, weight: .bold))
+                    .foregroundColor(Color.primary)
+                + Text("DuckDuckGo windows block most hidden trackers so websites can't spy on you.")
+                    .font(.system(size: 15))
+                    .foregroundColor(Color.primary)
+
+                Text("Burner windows" + " ")
+                    .font(.system(size: 15, weight: .bold))
+                    .foregroundColor(Color.primary)
+                + Text("are temporary: they don't access existing cookies or remember websites you visit after you close the window.")
+                    .font(.system(size: 15))
+                    .foregroundColor(Color.primary)
+            }
         }
     }
 
     struct FeaturesBox: View {
 
-            @Environment(\.colorScheme) var colorScheme
-
-            private var backgroundColor: Color {
-                return colorScheme == .dark ? Color.black.opacity(0.15) : Color.white
-            }
-
             var body: some View {
-                VStack(alignment: .leading, spacing: 15) {
+                VStack(alignment: .leading, spacing: 8) {
                     Text("Burner Windows make it easier to:")
                         .font(.system(size: 13, weight: .bold))
                         .foregroundColor(Color.primary)
-                        .padding(.top, -10)
 
                     HStack {
                         Image("BurnerWindowPopoverIcon1")
                             .resizable()
                             .frame(width: 16, height: 16)
-                        VStack(alignment: .leading) {
-                            Text("Sign into a site with a different account.")
-                                .font(.system(size: 13))
-                                .foregroundColor(Color.primary)
-                            Text("For example a work or personal account.")
-                                .font(.system(size: 13))
-                                .foregroundColor(Color.secondary)
-                        }
+                        Text("Sign into a site with a different account")
+                            .font(.system(size: 13))
+                            .foregroundColor(Color.primary)
 
                     }
 
@@ -105,37 +110,11 @@ extension HomePage.Views {
                             .resizable()
                             .frame(width: 16, height: 16)
                             .foregroundColor(Color.primary)
-                        VStack(alignment: .leading) {
-                            Text("Quickly visit a site in a signed-out state.")
-                                .font(.system(size: 13))
-                                .foregroundColor(Color.primary)
-                            Text("For example to avoid seeing recommendations.")
-                                .font(.system(size: 13))
-                                .foregroundColor(Color.secondary)
-                        }
-                    }
-
-                    Divider()
-                        .frame(width: 316)
-
-                    Text("Burner Windows automatically burn data on close.\nHistory and cookies are always isolated just to the\nBurner Window.")
-                        .font(.system(size: 13))
-                        .foregroundColor(Color.primary)
-
-                    HStack {
-                        Text("You can always burn data using the Fire Button.")
+                        Text("View websites without signing in")
                             .font(.system(size: 13))
                             .foregroundColor(Color.primary)
-                        Image("BurnerWindowHomepageFireIcon")
                     }
-
                 }
-                .padding()
-                .padding(10)
-                .background(RoundedRectangle(cornerRadius: 8).fill(backgroundColor))
-                .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray.opacity(0.3), lineWidth: 1))
-                .shadow(color: Color.black.opacity(0.05), radius: 2, x: 0, y: 2)
             }
         }
-
 }
