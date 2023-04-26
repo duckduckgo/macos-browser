2c0c9dfbc4eb3948220b1601a457571114e370ca
Christopher Brind <brindy@duckduckgo.com>
2023-04-26 20:56:46 +0100

    sync remove device (#1160)
    Task/Issue URL: https://app.asana.com/0/392891325557410/1204383167189823
    Tech Design URL:
    CC:
    
    **Description**:
    
    Adds remove device functionality.  
    
    **Steps to test this PR**:
    1. Drop in corresponding BSK branch
    https://github.com/duckduckgo/BrowserServicesKit/pull/335
    2. Connect multiple devices
    3. Hover over another device in the UI
    4. Click Remove...
    5. Click Remove Device
    6. Go to other device and open sync, it will detect it has been logged
    out and return the 'off' state
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    **When ready for review, remember to post the PR in MM**

--- DuckDuckGo/Preferences/Model/SyncPreferences.swift
+++ DuckDuckGo/Preferences/Model/SyncPreferences.swift
@@ -83,6 +83,11 @@ final class SyncPreferences: ObservableObject, SyncUI.ManagementViewModel {
         presentDialog(for: .deviceDetails(device))
     }
 
+    @MainActor
+    func presentRemoveDevice(_ device: SyncDevice) {
+        presentDialog(for: .removeDevice(device))
+    }
+
     func turnOffSync() {
         Task { @MainActor in
             do {
@@ -313,6 +318,19 @@ extension SyncPreferences: ManagementDialogModelDelegate {
             try data.writeFileWithProgress(to: location)
         }
 
+    }
+
+    @MainActor
+    func removeDevice(_ device: SyncDevice) {
+        Task { @MainActor in
+            do {
+                try await syncService.disconnect(deviceId: device.id)
+                managementDialogModel.endFlow()
+                refreshDevices()
+            } catch {
+                managementDialogModel.errorMessage = String(describing: error)
+            }
+        }
     }
 
 }
