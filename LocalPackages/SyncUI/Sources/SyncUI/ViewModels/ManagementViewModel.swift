975ba966a1eb3056886f404d95c7661747620295
Christopher Brind <brindy@duckduckgo.com>
2023-04-26 22:04:54 +0100

    sync delete account (#1161)
    Task/Issue URL: https://app.asana.com/0/0/1204383167189822/f
    Tech Design URL:
    CC:
    
    **Description**:
    
    Add turn off and delete data 
    
    **Steps to test this PR**:
    1. Turn on sync and add multiple devices
    2. Then choose Turn off and Delete Data
    3. All devices should end up turned off
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    **When ready for review, remember to post the PR in MM**

--- LocalPackages/SyncUI/Sources/SyncUI/ViewModels/ManagementViewModel.swift
+++ LocalPackages/SyncUI/Sources/SyncUI/ViewModels/ManagementViewModel.swift
@@ -31,6 +31,7 @@ public protocol ManagementViewModel: ObservableObject {
     func presentEnableSyncDialog()
     func presentRecoverSyncAccountDialog()
     func presentTurnOffSyncConfirmDialog()
+    func presentDeleteAccount()
     func presentShowOrEnterCodeDialog()
     func presentDeviceDetails(_ device: SyncDevice)
     func presentRemoveDevice(_ device: SyncDevice)
