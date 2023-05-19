00a8b384f55cf368f5c7ed9167932ded31301c3d
Sabrina Tardio <44158575+SabrinaTardio@users.noreply.github.com>
2023-05-19 11:11:55 +0100

    Add activation points pixels (#1206)
    Task/Issue URL:
    https://app.asana.com/0/1199230911884351/1204614030645089/f
    
    **Description**: Add Temporary initial pixels for 5 activation points:
    Enable email Protection, Watch in Duck Player, Enable cookie pop up
    management, Make default browser, Import bookmarks or passwords.
    
    **Steps to test this PR**:
    1. Enable email protection and check that the pixel
    "m_mac.enable-email-protection.initial” is fired.
    2. Disable and enable email protection again and check that the pixel is
    not fired
    3. Watch a video in Duck Player and check that the pixel
    "m_mac.watch-in-duckplayer.initial” is fired.
    4. Watch another video in Duck Player and check that the pixel is not
    fired
    5. Enable cookie pop up management and check that the pixel
    "m_mac.cookie-management-enabled.initial” is fired.
    6. Disable and enable cookie pop up management again and check that the
    pixel is not fired
    7. Make default browser and check that the pixel
    "m_mac.set-as-default.initial” is fired.
    8. Make another browser default and make DDG the default browser again
    and check that the pixel is not fired
    9. Import bookmarks and/or passwords and check that the pixel
    “m_mac.import-data.initial” is fired
    10. Import data again and check that the pixel is not fired
    11. Do all the 5 actions again after restarting the app and check no
    pixel is fired.
    
    
    <!—
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    —>
    
    —
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- DuckDuckGo/Statistics/Pixel.swift
+++ DuckDuckGo/Statistics/Pixel.swift
@@ -59,7 +59,6 @@ final class Pixel {
         guard !dryRun else {
             let params = params?.filter { key, _ in !["appVersion", "test"].contains(key) } ?? [:]
             os_log(.debug, log: .pixel, "%@ %@", pixelName.replacingOccurrences(of: "_", with: "."), params)
-
             // simulate server response time for Dry Run mode
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 onComplete(nil)
