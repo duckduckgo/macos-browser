b1dd298cadc171513217301a77d64ff8e4993cd6
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-05-24 10:01:37 +0200

    Base implementation

--- LocalPackages/BuildToolPlugins/Plugins/InputFilesChecker/InputFilesChecker.swift
+++ LocalPackages/BuildToolPlugins/Plugins/InputFilesChecker/InputFilesChecker.swift
@@ -38,7 +38,9 @@ let extraInputFiles: [TargetName: Set<InputFile>] = [
         .init("UpdateController.swift", .source),
         .init("LegacyWebKitDownloadDelegate.swift", .source),
         .init("WKProcessPool+DownloadDelegate.swift", .source),
-        .init("_WKDownload+WebKitDownload.swift", .source)
+        .init("_WKDownload+WebKitDownload.swift", .source),
+        .init("PFMoveApplication.m", .source),
+        .init("MoveApplication.strings", .resource)
     ],
 
     "Unit Tests": [
