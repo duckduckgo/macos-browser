368c5a159771e0a42149c7552ec825578c6f9753
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-06-16 17:14:29 -0300

    Add C-S-S scan operation integration (#1264)
    ## Asana tasks
    Task/Issue URL:
    https://app.asana.com/0/1204006570077678/1204539891101973/f
    Tech Design URL:
    https://app.asana.com/0/481882893211075/1204625986406930/f
    
    ## Description
    Adds the content scope integration for Data Broker Protection. It also
    adds integration with the scan operation. This is a short description of
    the classes and their responsibility:

---
+++ LocalPackages/DataBrokerProtection/Tests/DataBrokerProtectionTests/DataBrokerProtectionActionsHandlerTests.swift
@@ -0,0 +1,55 @@
+//
+//  DataBrokerProtectionActionsHandlerTests.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+@testable import DataBrokerProtection
+
+final class DataBrokerProtectionActionsHandlerTests: XCTestCase {
+
+    func testWhenStepHasNoActions_thenNilIsReturned() {
+        let step = Step(type: .scan, actions: [Action]())
+        let sut = DataBrokerProtectionActionsHandler(step: step)
+
+        XCTAssertNil(sut.nextAction())
+    }
+
+    func testWhenNextStepDoesNotFindAnyMoreActions_thenNilIsReturned() {
+        let firstAction = NavigateAction(id: "id", actionType: .navigate, url: "url", ageRange: [String]())
+        let secondAction = NavigateAction(id: "id2", actionType: .navigate, url: "url", ageRange: [String]())
+        let step = Step(type: .scan, actions: [firstAction, secondAction])
+        let sut = DataBrokerProtectionActionsHandler(step: step)
+
+        _ = sut.nextAction() // Returns the first action
+        _ = sut.nextAction() // Returns the second action
+
+        XCTAssertNil(sut.nextAction())
+    }
+
+    func testWhenNextStepFindsAnAction_thenThatNextActionIsReturned() {
+        let firstAction = NavigateAction(id: "id", actionType: .navigate, url: "url", ageRange: [String]())
+        let secondAction = NavigateAction(id: "id2", actionType: .navigate, url: "url", ageRange: [String]())
+        let step = Step(type: .scan, actions: [firstAction, secondAction])
+        let sut = DataBrokerProtectionActionsHandler(step: step)
+
+        _ = sut.nextAction() // Returns the first action
+        let action = sut.nextAction() // Returns the second and last action
+
+        XCTAssertEqual(action?.id, secondAction.id)
+    }
+
+}
