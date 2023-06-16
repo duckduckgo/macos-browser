a22c8582b5b58a071903bbc4bfbe3b1355af5df7
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-06-16 17:14:29 -0300

    Add C-S-S scan operation integration (#1264)
    ## Asana tasks
    Task/Issue URL:
    https://app.asana.com/0/1204006570077678/1204539891101973/f
    Tech Design URL:
    https://app.asana.com/0/481882893211075/1204625986406930/f
    
    ## Description
    Adds the content scope integration for Data Broker Protection. It also
    adds integration with the scan operation. This is a short description of
    the classes and their responsibility:

---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/C-S-S/DataBrokerProtectionResponseModels.swift
@@ -0,0 +1,95 @@
+//
+//  DataBrokerProtectionResponseModels.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+struct NavigateResponse: Decodable {
+    let url: String
+}
+
+enum CSSSuccessData {
+    case navigate(NavigateResponse)
+    case extract([ExtractedProfile])
+}
+
+struct CSSSuccessResponse: Decodable {
+    let actionID: String
+    let actionType: ActionType
+    let response: CSSSuccessData?
+
+    enum CodingKeys: CodingKey {
+        case actionID
+        case actionType
+        case response
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.actionID = try container.decode(String.self, forKey: .actionID)
+        self.actionType = try container.decode(ActionType.self, forKey: .actionType)
+
+        switch actionType {
+        case .navigate:
+            self.response = .navigate(try container.decode(NavigateResponse.self, forKey: .response))
+        case .extract:
+            self.response = .extract(try container.decode([ExtractedProfile].self, forKey: .response))
+        default:
+            self.response = nil
+        }
+    }
+}
+
+struct CSSErrorResponse: Decodable {
+    let actionID: String
+    let message: String
+}
+
+struct CSSSuccess: Decodable {
+    let success: CSSSuccessResponse
+}
+
+struct CSSErrorWrapper: Decodable {
+    let error: CSSErrorResponse
+}
+
+enum CSSResultResponse {
+    case success(CSSSuccessResponse)
+    case error(CSSErrorResponse)
+}
+
+struct CSSResult: Decodable {
+    let result: CSSResultResponse
+
+    enum CodingKeys: CodingKey {
+        case result
+        case success
+        case error
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        if let success = try? container.decode(CSSSuccess.self, forKey: .result) {
+            result = .success(success.success)
+        } else if let error = try? container.decode(CSSErrorWrapper.self, forKey: .result) {
+            result = .error(error.error)
+        } else {
+            throw DataBrokerProtectionError.parsingErrorObjectFailed
+        }
+    }
+}
