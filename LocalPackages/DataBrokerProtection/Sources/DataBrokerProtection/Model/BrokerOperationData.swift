d773daf78061f17687ec89fbee5e218c880d808f
Fernando Bunn <bunn@idevzilla.com>
2023-06-17 09:43:34 +0100

    DBP Scheduler initial setup (#1270)
    Task/Issue URL:
    https://app.asana.com/0/72649045549333/1203980870103835/f
    
    **Description**:
    This is just the setup for the operations where the Scheduler will base
    itself on

---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Model/BrokerOperationData.swift
@@ -0,0 +1,86 @@
+//
+//  BrokerOperationData.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+protocol BrokerOperationData {
+    var id: UUID { get set }
+    var brokerProfileQueryID: UUID { get set }
+    var preferredRunDate: Date? { get set }
+    var historyEvents: [HistoryEvent] { get set }
+    var lastRunDate: Date? { get set }
+
+    mutating func addHistoryEvent(_ historyEvent: HistoryEvent)
+}
+
+extension BrokerOperationData {
+    mutating func addHistoryEvent(_ historyEvent: HistoryEvent) {
+        self.historyEvents.append(historyEvent)
+    }
+
+    var lastRunDate: Date? {
+        return historyEvents.last?.date
+    }
+}
+
+class ScanOperationData: BrokerOperationData {
+    var id: UUID
+    var brokerProfileQueryID: UUID
+    var preferredRunDate: Date?
+    var historyEvents: [HistoryEvent]
+    var lastRunDate: Date?
+
+    internal init(id: UUID = UUID(),
+                  brokerProfileQueryID: UUID,
+                  preferredRunDate: Date?,
+                  historyEvents: [HistoryEvent],
+                  lastRunDate: Date? = nil) {
+
+        self.id = id
+        self.brokerProfileQueryID = brokerProfileQueryID
+        self.preferredRunDate = preferredRunDate
+        self.historyEvents = historyEvents
+        self.lastRunDate = lastRunDate
+    }
+
+}
+
+class OptOutOperationData: BrokerOperationData {
+    var id: UUID
+    var brokerProfileQueryID: UUID
+    var preferredRunDate: Date?
+    var historyEvents: [HistoryEvent]
+    var lastRunDate: Date?
+
+    var extractedProfile: ExtractedProfile
+
+    internal init(id: UUID = UUID(),
+                  brokerProfileQueryID: UUID,
+                  preferredRunDate: Date?,
+                  historyEvents: [HistoryEvent],
+                  lastRunDate: Date? = nil,
+                  extractedProfile: ExtractedProfile) {
+
+        self.id = id
+        self.brokerProfileQueryID = brokerProfileQueryID
+        self.preferredRunDate = preferredRunDate
+        self.historyEvents = historyEvents
+        self.lastRunDate = lastRunDate
+        self.extractedProfile = extractedProfile
+    }
+}
---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Operations/OperationRunner.swift
@@ -0,0 +1,53 @@
+//
+//  OperationRunner.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+protocol OperationRunner {
+
+    func scan(_ profileQuery: BrokerProfileQueryData) async throws -> [ExtractedProfile]
+    func optOut(_ extractedProfile: ExtractedProfile) async throws
+}
+
+//TODO: Remove this
+import WebKit
+
+class TestOperationRunner: OperationRunner {
+
+    let webView: WKWebView
+
+    internal init() {
+        self.webView = WKWebView(frame: .init(x: 0, y: 0, width: 100, height: 100))
+    }
+
+
+    func scan(_ profileQuery: BrokerProfileQueryData) async throws -> [ExtractedProfile] {
+        let extractedProfile = ExtractedProfile(name: "John")
+        return [extractedProfile]
+
+    }
+
+    func optOut(_ extractedProfile: ExtractedProfile) async throws {
+        print("Fake opt out")
+    }
+
+    deinit {
+        print("DEINIT")
+    }
+
+}
