1c4e477b9fdb526251830926eadf07a625f166f0
Brad Slayter <slayterdevelopment@gmail.com>
2023-06-20 13:20:28 -0500

    Update BSK

--- LocalPackages/NetworkProtection/Package.swift
+++ LocalPackages/NetworkProtection/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // If you are updating the BSK dependency in the main app, this version will need to be updated to match.
         // There is work underway to move this package into BSK itself, after which this will not be required.
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "61.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", revision: "465598956a148508a01ab700f3990e7f1f5827b3"),
         .package(url: "https://github.com/duckduckgo/wireguard-apple", exact: "1.0.0")
     ],
     targets: [
