b7c8b04f863ca927bf2ca304c9c6524d487fb2a9
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-06-26 17:19:10 -0300

    Add click, expectation and fill form actions (#1289)
    ## Task
    
    https://app.asana.com/0/1204006570077678/1204862976049537/f
    
    ## Description
    
    This pull request has two goals:
    - Add the click, expectation and fillForm actions
    - Refactor the operation classes to be more isolated.
    
    **Operation refactor**
    Now we have a `ScanOperation` and `OptOutOperation` that need to be
    initialized with a `BrokerProfileQueryData`. Then we just run it (using
    the `run` method). We do not need to pass any more data to it.
    
    ## Steps to test
    
    Follow the same process as #1264. Then instead of creating a scan step.
    We should create a optOut step the code below:
    
    It is important to notice that this will return an error, because the
    expectation will not be found.
    
    ```swift
    let privacySettings = PrivacySecurityPreferences.shared
    let sessionKey = UUID().uuidString
    let prefs = ContentScopeProperties.init(gpcEnabled: privacySettings.gpcEnabled,
                                            sessionKey: sessionKey,
                                            featureToggles: features)
    
    let profileData = ProfileQuery(firstName: "Ben", lastName: "Smith", city: "Miami", state: "FL", age: 43, profileUrl: "https://verecor.com/ng/view/Ben-Smith-DVAXHtB", email: "juan@duck")
    let navigateAction = NavigateAction(
        id: "navigateID",
        actionType: .navigate,
        url: "https://verecor.com/ng/control/privacy",
        ageRange: [String]()
    )
    let fillForm = FillFormAction(
        id: "fillFormID",
        actionType: .fillForm,
        selector: ".ahm",
        elements: [
            .init(type: "fullName", selector: "#user_name"),
            .init(type: "email", selector: "#user_email"),
            .init(type: "profileUrl", selector: "#url")
        ]
    )
    let expectationAction = ExpectationAction(
        id: "expectation",
        actionType: .expectation,
        expectations: [
            .init(type: .text, expect: "Your removal request has been received", selector: "body")
        ]
    )
    
    let click = ClickAction(id: "clickID", actionType: .click, elements: [.init(type: "button", selector: ".btn-sbmt")])
    
    let extractedProfile = ExtractedProfile(
        name: "//div[@class='col-sm-24 col-md-19 col-text']",
        alternativeNamesList: ".name",
        addressCityStateList: ".location",
        profileUrl: "a",
        age: ".age"
    )
    let extractAction = ExtractAction(id: "extractID", actionType: .extract, selector: ".search-item", profile: extractedProfile)
    let step = Step(type: .optOut, actions: [navigateAction, fillForm, click, expectationAction])
    let dataBrokerData = DataBroker(name: "verecor", steps: [step])
    let query = BrokerProfileQueryData(id: UUID(), profileQuery: profileData, dataBroker: dataBrokerData)
    let dbp = OptOutOperation(privacyConfig: privacyConfigurationManager, prefs: prefs, query: query)
    
    Task {
        do {
            try await dbp.run()
            print("Success!")
        } catch {
            print(error)
        }
    }
    ```

---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Operations/OptOutOperation.swift
@@ -0,0 +1,76 @@
+//
+//  OptOutOperation.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import WebKit
+import BrowserServicesKit
+import UserScript
+
+public final class OptOutOperation: DataBrokerOperation {
+    public typealias ReturnValue = Void
+
+    public var privacyConfig: PrivacyConfigurationManaging
+    public var prefs: ContentScopeProperties
+    public var query: BrokerProfileQueryData
+    public var webViewHandler: DataBrokerProtectionWebViewHandler?
+    public var actionsHandler: DataBrokerProtectionActionsHandler?
+    public var continuation: CheckedContinuation<Void, Error>?
+
+    public init(privacyConfig: PrivacyConfigurationManaging,
+                prefs: ContentScopeProperties,
+                query: BrokerProfileQueryData) {
+        self.privacyConfig = privacyConfig
+        self.prefs = prefs
+        self.query = query
+    }
+
+    public func run() async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            self.continuation = continuation
+
+            Task {
+                await initialize()
+
+                do {
+                    if let optOutStep = try query.dataBroker.optOutStep() {
+                        actionsHandler = DataBrokerProtectionActionsHandler(step: optOutStep)
+                        await executeNextStep()
+                    } else {
+                        // Children sites could not have an optout step
+                        complete(())
+                    }
+                } catch {
+                    failed(with: DataBrokerProtectionError.unknown(error.localizedDescription))
+                }
+            }
+        }
+    }
+
+    public func extractedProfiles(profiles: [ExtractedProfile]) {
+        // No - op
+    }
+
+    public func executeNextStep() async {
+        if let action = actionsHandler?.nextAction() {
+            await webViewHandler?.execute(action: action, profileData: query.profileQuery)
+        } else {
+            complete(())
+            await webViewHandler?.finish() // If we executed all steps we release the web view
+        }
+    }
+}
