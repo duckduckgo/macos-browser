3717e9bc35208eb19324cca2d3f09e98f0128546
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-06-29 11:58:53 +0200

    Fixes the NetP sysex alert look

--- LocalPackages/NetworkProtectionUI/Sources/NetworkProtectionUI/NetworkProtectionStatusView.swift
+++ LocalPackages/NetworkProtectionUI/Sources/NetworkProtectionUI/NetworkProtectionStatusView.swift
@@ -228,7 +228,7 @@ public struct NetworkProtectionStatusView: View {
     private func connectionHealthWarningView(message: String) -> some View {
         VStack(spacing: 0) {
             HStack(alignment: .top, spacing: 12) {
-                Image("WarningColored")
+                Image("WarningColored", bundle: Bundle.module)
 
                 /// Text elements in SwiftUI don't expand horizontally more than needed, so we're adding an "optional" spacer at the end so that
                 /// the alert bubble won't shrink if there's not enough text.
