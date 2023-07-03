7e550291efd95c1e99539e30f59437607dae9322
Fernando Bunn <bunn@idevzilla.com>
2023-07-03 14:54:08 +0100

    Add DuckDuckGo DBP Target (#1316)
    Task/Issue URL:
    https://app.asana.com/0/1204167627774280/1204884122679453/f
    
    **Description**:
    Add DuckDuckGo DBP Target

--- DuckDuckGo/Tab/ViewModel/TabViewModel.swift
+++ DuckDuckGo/Tab/ViewModel/TabViewModel.swift
@@ -27,6 +27,9 @@ final class TabViewModel {
         static let burnerHome = NSImage(named: "BurnerTabFavicon")!
         static let preferences = NSImage(named: "Preferences")!
         static let bookmarks = NSImage(named: "Bookmarks")!
+#if DBP
+        static let dataBrokerProtection = NSImage(named: "BurnerWindowPopoverIcon1")! // PLACEHOLDER: Change it once we have the final icon
+#endif
     }
 
     private(set) var tab: Tab
@@ -248,6 +251,10 @@ final class TabViewModel {
         }
 
         switch tab.content {
+#if DBP
+        case .dataBrokerProtection:
+            title = UserText.tabDataBrokerProtectionTitle
+#endif
         case .preferences:
             title = UserText.tabPreferencesTitle
         case .bookmarks:
@@ -279,6 +286,11 @@ final class TabViewModel {
         }
 
         switch tab.content {
+#if DBP
+        case .dataBrokerProtection:
+            favicon = Favicon.dataBrokerProtection
+            return
+#endif
         case .homePage:
             if tab.burnerMode.isBurner {
                 favicon = Favicon.burnerHome
