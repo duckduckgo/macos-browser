duckduckgo/macos-browser

commit 90f1e4b4b7a0efe4dc0ad3633b263ce520761adc

Improve NetP logging (#1259)
Task/Issue URL: https://app.asana.com/0/1199230911884351/1204830533003048/f
Tech Design URL:
CC: @diegoreymendez

Description:

This PR enables NetP extension logging by default. It was disabled before the PR was merged, but I'm turning it back on for now as it doesn't get used unless NetP is actually running, and because of our disclosure terms we're able to perform such logging. In production, we wouldn't do this.

This PR also sets the subsystem of the NetP extension logs to com.duckduckgo.macos.browser.network-protection. Our current use of subsystem across the app isn't super useful - today, we just set it to the bundle ID, but it would be better to filter by subsystem in cases like this where we want to see all NetP logs at once.

DuckDuckGo/Common/Utilities/UserDefaultsWrapper.swift
@@ -134,7 +134,7 @@ public struct UserDefaultsWrapper<T> {
     }
 
     static var sharedDefaults: UserDefaults {
-#if DEBUG
+#if DEBUG && !NETP_SYSTEM_EXTENSION
         if case .normal = NSApp.runType {
             return .standard
         } else {

DuckDuckGo/AppDelegate/AppDelegate.swift
@@ -327,12 +327,12 @@ final class AppDelegate: NSObject, NSApplicationDelegate, FileDownloadManagerDel
 
         // should‘ve been run at least once with NetP enabled
         guard let lastVersionRun = versionStore.lastVersionRun else {
-            os_log(.error, log: .networkProtection, "🔴 running netp for the first time: update not needed")
+            os_log(.info, log: .networkProtection, "No last version found for the NetP login items, skipping update")
             return
         }
 
         if lastVersionRun != currentVersion {
-            os_log(.error, log: .networkProtection, "🟡 App updated from %{public}s to %{public}s: updating", lastVersionRun, currentVersion)
+            os_log(.info, log: .networkProtection, "App updated from %{public}s to %{public}s: updating login items", lastVersionRun, currentVersion)
             updateNetworkProtectionTunnelAndMenu()
         } else {
             // If login items failed to launch (e.g. because of the App bundle rename), launch using NSWorkspace

DuckDuckGo/Common/Logging/Logging.swift
@@ -26,7 +26,6 @@ extension OSLog {
         case atb = "ATB"
         case config = "Configuration Downloading"
         case fire = "Fire"
-        case passwordManager = "Password Manager"
         case history = "History"
         case dataImportExport = "Data Import/Export"
         case pixel = "Pixel"
@@ -42,7 +41,9 @@ extension OSLog {
         case navigation = "Navigation"
         case duckPlayer = "Duck Player"
         case sync = "Sync"
+        case networkProtection = "Network Protection"
     }
+
     enum AllCategories {
         static var allCases: [String] {
             Categories.allCases.map(\.rawValue) + AppCategories.allCases.map(\.rawValue)
@@ -67,6 +68,7 @@ extension OSLog {
     @OSLogWrapper(.navigation) static var navigation
     @OSLogWrapper(.duckPlayer) static var duckPlayer
     @OSLogWrapper(.sync) static var sync
+    @OSLogWrapper(.networkProtection) static var networkProtection
 
     // Debug->Logging categories will only be enabled for one day
     @UserDefaultsWrapper(key: .loggingEnabledDate, defaultValue: .distantPast)

DuckDuckGo/NetworkProtection/AppTargets/BothAppTargets/NetworkProtectionTunnelController.swift
@@ -254,19 +254,21 @@ final class NetworkProtectionTunnelController: NetworkProtection.TunnelControlle
         Task {
             try await Task.sleep(interval: interval)
 
-            os_log(.error, log: .networkProtection, "🟢 checking login agents")
+            os_log(.info, log: .networkProtection, "Checking whether login agents are enabled and running")
+
             for item in Self.loginItems {
                 guard !item.isRunning && (condition.shouldIgnoreItemStatus || item.status.isEnabled) else {
-                    os_log(.error, log: .networkProtection, "🟢 %{public}s: ok", item.debugDescription)
+                    os_log(.info, log: .networkProtection, "Login item with ID '%{public}s': ok", item.debugDescription)
                     continue
                 }
-                os_log(.error, log: .networkProtection, "🔴 %{public}s is not running, launching manually", item.debugDescription)
+
+                os_log(.error, log: .networkProtection, "%{public}s is not running, launching manually", item.debugDescription)
 
                 do {
                     try await item.launch()
-                    os_log("🔴 launched %{public}s", item.debugDescription)
+                    os_log(.info, log: .networkProtection, "Launched login item with ID '%{public}s'", item.debugDescription)
                 } catch {
-                    os_log(.error, log: .networkProtection, "🔴 %{public}s could not be launched. %{public}s", item.debugDescription, "\(error)")
+                    os_log(.error, log: .networkProtection, "Login item with ID '%{public}s' could not be launched. Error: %{public}s", item.debugDescription, "\(error)")
                 }
             }
         }

DuckDuckGo/NetworkProtection/NetworkExtensionTargets/NetworkExtensionTargets/PacketTunnelProvider.swift
@@ -60,7 +60,7 @@ final class PacketTunnelProvider: NEPacketTunnelProvider {
             if logLevel == .error {
                 os_log("🔵 Received error from adapter: %{public}@", log: .networkProtection, type: .error, message)
             } else {
-                os_log("🔵 Received message from adapter: %{public}@", log: .networkProtection, type: .info, message)
+                os_log("🔵 Received message from adapter: %{public}@", log: .networkProtection, message)
             }
         }
     }()
@@ -179,7 +179,7 @@ final class PacketTunnelProvider: NEPacketTunnelProvider {
     }
 
     private func resetRegistrationKey() {
-        os_log("Resetting the current registration key", log: .networkProtectionKeyManagement, type: .info)
+        os_log("Resetting the current registration key", log: .networkProtectionKeyManagement)
         keyStore.resetCurrentKeyPair()
     }
 
@@ -196,7 +196,7 @@ final class PacketTunnelProvider: NEPacketTunnelProvider {
     }
 
     private func rekey() async {
-        os_log("Rekeying...", log: .networkProtectionKeyManagement, type: .info)
+        os_log("Rekeying...", log: .networkProtectionKeyManagement)
 
         Pixel.fire(.networkProtectionActiveUser, frequency: .dailyOnly, includeAppVersionParameter: true)
         Pixel.fire(.networkProtectionRekeyCompleted, frequency: .dailyAndContinuous, includeAppVersionParameter: true)
@@ -471,7 +471,7 @@ final class PacketTunnelProvider: NEPacketTunnelProvider {
         tunnelHealth.isHavingConnectivityIssues = false
         controllerErrorStore.lastErrorMessage = nil
 
-        os_log("🔵 Will load options\n%{public}@", log: .networkProtection, type: .info, String(describing: options))
+        os_log("🔵 Will load options\n%{public}@", log: .networkProtection, String(describing: options))
 
         if options?["tunnelFailureSimulation"] as? String == "true" {
             internalCompletionHandler(TunnelError.simulateTunnelFailureError)

LocalPackages/NetworkProtection/Sources/NetworkProtection/KeyManagement/NetworkProtectionKeyStore.swift
@@ -78,7 +78,6 @@ public final class NetworkProtectionKeychainKeyStore: NetworkProtectionKeyStore 
     public func currentKeyPair() -> KeyPair {
         os_log("Querying the current key pair (publicKey: %{public}@, expirationDate: %{public}@)",
                log: .networkProtectionKeyManagement,
-               type: .info,
                String(describing: currentPublicKey),
                String(describing: currentExpirationDate))
 
@@ -86,7 +85,6 @@ public final class NetworkProtectionKeychainKeyStore: NetworkProtectionKeyStore 
             let keyPair = newCurrentKeyPair()
             os_log("Returning a new key pair as there's no current private key (newPublicKey: %{public}@)",
                    log: .networkProtectionKeyManagement,
-                   type: .info,
                    String(describing: keyPair.publicKey.base64Key))
             return keyPair
         }
@@ -97,15 +95,11 @@ public final class NetworkProtectionKeychainKeyStore: NetworkProtectionKeyStore 
             let keyPair = newCurrentKeyPair()
             os_log("Returning a new key pair as the expirationDate date is missing, or we're past it (now: %{public}@, expirationDate: %{public}@)",
                    log: .networkProtectionKeyManagement,
-                   type: .info,
                    String(describing: Date()),
                    String(describing: currentExpirationDate))
             return keyPair
         }
 
-        os_log("Returning an existing key pair.",
-               log: .networkProtectionKeyManagement,
-               type: .info)
         return KeyPair(privateKey: currentPrivateKey, expirationDate: currentExpirationDate)
     }
 

LocalPackages/NetworkProtection/Sources/NetworkProtection/Logging/Logging.swift
@@ -64,35 +64,36 @@ extension OSLog {
 
 struct Logging {
 
-    static let subsystem = Bundle.main.bundleIdentifier ?? "DuckDuckGo"
+    static let subsystem = "com.duckduckgo.macos.browser.network-protection"
 
-    fileprivate static let networkProtectionLoggingEnabled = false
+    fileprivate static let networkProtectionLoggingEnabled = true
     fileprivate static let networkProtection: OSLog = OSLog(subsystem: subsystem, category: "Network Protection")
 
-    fileprivate static let networkProtectionBandwidthAnalysisLoggingEnabled = false
+    fileprivate static let networkProtectionBandwidthAnalysisLoggingEnabled = true
     fileprivate static let networkProtectionBandwidthAnalysis: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Bandwidth Analysis")
 
-    fileprivate static let networkProtectionConnectionTesterLoggingEnabled = false
+    fileprivate static let networkProtectionConnectionTesterLoggingEnabled = true
     fileprivate static let networkProtectionConnectionTesterLog: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Connection Tester")
 
-    fileprivate static let networkProtectionDistributedNotificationsLoggingEnabled = false
+    fileprivate static let networkProtectionDistributedNotificationsLoggingEnabled = true
     fileprivate static let networkProtectionDistributedNotificationsLog: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Distributed Notifications")
 
-    fileprivate static let networkProtectionIPCLoggingEnabled = false
+    fileprivate static let networkProtectionIPCLoggingEnabled = true
     fileprivate static let networkProtectionIPCLog: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: IPC")
 
-    fileprivate static let networkProtectionKeyManagementLoggingEnabled = false
+    fileprivate static let networkProtectionKeyManagementLoggingEnabled = true
     fileprivate static let networkProtectionKeyManagement: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Key Management")
 
-    fileprivate static let networkProtectionMemoryLoggingEnabled = false
+    fileprivate static let networkProtectionMemoryLoggingEnabled = true
     fileprivate static let networkProtectionMemoryLog: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Memory")
 
-    fileprivate static let networkProtectionPixelLoggingEnabled = false
+    fileprivate static let networkProtectionPixelLoggingEnabled = true
     fileprivate static let networkProtectionPixel: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Pixel")
 
-    fileprivate static let networkProtectionStatusReporterLoggingEnabled = false
+    fileprivate static let networkProtectionStatusReporterLoggingEnabled = true
     fileprivate static let networkProtectionStatusReporterLog: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Status Reporter")
 
-    fileprivate static let networkProtectionSleepLoggingEnabled = false
+    fileprivate static let networkProtectionSleepLoggingEnabled = true
     fileprivate static let networkProtectionSleepLog: OSLog = OSLog(subsystem: subsystem, category: "Network Protection: Sleep and Wake")
+
 }

LocalPackages/NetworkProtection/Sources/NetworkProtection/WireGuardKit/IPAddressRange.swift
@@ -7,11 +7,6 @@ import Network
 public struct IPAddressRange {
     public let address: IPAddress
     public let networkPrefixLength: UInt8
-
-    init(address: IPAddress, networkPrefixLength: UInt8) {
-        self.address = address
-        self.networkPrefixLength = networkPrefixLength
-    }
 }
 
 extension IPAddressRange: Equatable {

commit 96d6a0a53221ad6027ca0afb1656863c5e0d0267

Update the check used to determine which user defaults wrapper is used (#1332)
Task/Issue URL: https://app.asana.com/0/1199230911884351/1204964526109657/f
Tech Design URL:
CC: @ayoy

Description:

This PR fixes a test failure introduced recently in PR #1259.

This PR was using NETP_SYSTEM_EXTENSION as its check, but this value is actually set on the Developer ID target also. So, to fix this, we need to check for NETP_SYSTEM_EXTENSION and also NETWORK_EXTENSION, to ensure we're only targeting the system extension itself.

DuckDuckGo/Common/Utilities/UserDefaultsWrapper.swift
@@ -134,7 +134,7 @@ public struct UserDefaultsWrapper<T> {
     }
 
     static var sharedDefaults: UserDefaults {
-#if DEBUG && !NETP_SYSTEM_EXTENSION
+#if DEBUG && !(NETP_SYSTEM_EXTENSION && NETWORK_EXTENSION) // Avoid looking up special user defaults when running inside the system extension
         if case .normal = NSApp.runType {
             return .standard
         } else {