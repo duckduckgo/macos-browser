9045f7514e79f8f6ff36196e3acad63bf88ab022
Fernando Bunn <bunn@idevzilla.com>
2023-07-10 13:45:00 +0100

    Add error handler for sending pixels (#1338)
    Task/Issue URL:
    https://app.asana.com/0/1204167627774280/1204946671793052/f
    
    **Description**:
    Add error handler for sending pixels

--- DuckDuckGo/Statistics/PixelEvent.swift
+++ DuckDuckGo/Statistics/PixelEvent.swift
@@ -277,7 +277,9 @@ extension Pixel {
             case invalidPayload(Configuration)
 
             case burnerTabMisplaced
-
+#if DBP
+            case dataBrokerProtectionError
+#endif
         }
 
     }
@@ -626,6 +628,9 @@ extension Pixel.Event.Debug {
 
         case .burnerTabMisplaced: return "burner_tab_misplaced"
 
+#if DBP
+        case .dataBrokerProtectionError: return "data_broker_error"
+#endif
         }
     }
 }
