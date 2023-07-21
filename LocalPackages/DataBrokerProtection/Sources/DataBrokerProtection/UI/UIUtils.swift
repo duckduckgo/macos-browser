5f936af1aa920e94a4d886acbd00c425bcac219a
Fernando Bunn <bunn@idevzilla.com>
2023-07-21 16:12:37 +0100

    Add GettingStartedView (#1361)
    Task/Issue URL: https://app.asana.com/0/0/1205073892368591/f and
    https://app.asana.com/0/1204167627774280/1205083232800704/f
    
    **Description**:
    Add GettingStartedView and HeaderView.
    DebugUI moved to it's own ViewController

---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/UI/UIUtils.swift
@@ -0,0 +1,29 @@
+//
+//  CTAButtonStyle.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+
+struct CTAButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .foregroundColor(configuration.isPressed ? Color.primary : Color.white)
+            .background(configuration.isPressed ? Color.secondary : Color.accentColor)
+            .cornerRadius(6.0)
+            .padding()
+    }
+}
