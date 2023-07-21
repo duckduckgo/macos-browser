c70a3dcbb696edd1a90e8968ec7644a0981dd311
Fernando Bunn <bunn@idevzilla.com>
2023-07-21 16:12:37 +0100

    Add GettingStartedView (#1361)
    Task/Issue URL: https://app.asana.com/0/0/1205073892368591/f and
    https://app.asana.com/0/1204167627774280/1205083232800704/f
    
    **Description**:
    Add GettingStartedView and HeaderView.
    DebugUI moved to it's own ViewController

---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/UI/DashboardHeaderView.swift
@@ -0,0 +1,101 @@
+//
+//  DashboardHeaderView.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+
+private enum Constants {
+    static let heroBottomPadding: CGFloat = 16
+    static let titleSubtitlePadding: CGFloat = 5
+}
+
+@available(macOS 11.0, *)
+struct DashboardHeaderView: View {
+    @ObservedObject var viewModel: DashboardHeaderViewModel
+
+    var body: some View {
+        ZStack {
+            Image("header-background", bundle: .module).resizable()
+
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    CTAHeaderView()
+                }
+                HeaderTitleView(viewModel: viewModel)
+                Spacer()
+            }
+        }
+    }
+}
+
+@available(macOS 11.0, *)
+private struct HeaderTitleView: View {
+    @ObservedObject var viewModel: DashboardHeaderViewModel
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Image("header-hero", bundle: .module)
+                .padding(.bottom, Constants.heroBottomPadding)
+            VStack (spacing: Constants.titleSubtitlePadding) {
+                Text("Data Broker Protection")
+                    .font(.title)
+                    .bold()
+
+                Text(viewModel.statusText)
+                    .font(.body)
+            }
+        }
+    }
+}
+
+@available(macOS 11.0, *)
+private struct CTAHeaderView: View {
+    var body: some View {
+        HStack {
+            Button {
+                print("FAQ")
+            } label: {
+                Text("FAQs")
+            }
+            .buttonStyle(.borderless)
+            .foregroundColor(.primary)
+
+            Button {
+                print("Edit Profile")
+            } label: {
+                HStack {
+                    Image(systemName: "person")
+                    Text("Edit Profile")
+                }
+                .frame(maxWidth: 110, maxHeight: 26)
+            }.buttonStyle(CTAButtonStyle())
+        }
+    }
+}
+
+@available(macOS 11.0, *)
+struct DashboardHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        let viewModel = DashboardHeaderViewModel(statusText: "Scanning...",
+                                                 faqButtonClicked: {},
+                                                 editProfileClicked: {})
+
+        DashboardHeaderView(viewModel: viewModel)
+            .frame(width: 1000, height: 300)
+    }
+}
---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/UI/GettingStartedView.swift
@@ -0,0 +1,174 @@
+//
+//  GettingStartedView.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+struct GettingStartedView: View {
+
+    var body: some View {
+        VStack(alignment: .center, spacing: Constants.ContainerView.verticalSpacing) {
+            TitleView(title: "Getting Started",
+                      subtitle: "Data brokers and people-search sites publish personal info online.\nDiscover where you’re exposed and automatically remove your data.")
+            DividerView()
+
+            BodyView(rowItems: [
+                RowItem(imageName: "person.badge.plus",
+                        title: "Personal Information",
+                        subtitle: "Your info is required for us to find matching profiles within data broker sites."),
+
+                RowItem(imageName: "externaldrive.badge.person.crop",
+                        title: "We Don’t Store Your Data",
+                        subtitle: "All your information is stored on-device."),
+
+                RowItem(imageName: "globe.americas",
+                        title: "United States Only",
+                        subtitle: "Data brokers we cover target people with US addresses.")
+            ])
+
+            CTAButton(title: "Get Started")
+        }
+        .padding(Constants.ContainerView.padding)
+        .background(Color("modal-background-color", bundle: .module))
+        .cornerRadius(Constants.ContainerView.cornerRadius)
+        .shadow(color: .black.opacity(0.08), radius: 4, x: 0, y: 4)
+        .shadow(color: .black.opacity(0.1), radius: 2, x: 0, y: 1)
+        .padding(.all)
+    }
+}
+
+private struct CTAButton: View {
+    let title: String
+    var body: some View {
+        Button(action: {
+        }) {
+            Text(title)
+                .bold()
+                .frame(width: Constants.CTA.width, height: Constants.CTA.height)
+        }
+        .buttonStyle(CTAButtonStyle())
+    }
+}
+
+private struct TitleView: View {
+    let title: String
+    let subtitle: String
+    var body: some View {
+        VStack(alignment: .center, spacing: Constants.titleViewSpacing) {
+            Text(title)
+                .font(.title)
+                .bold()
+
+            Text(subtitle)
+                .frame(minHeight: 32)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+private struct DividerView: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.clear)
+            .frame(width: Constants.Divider.width, height: Constants.Divider.height)
+            .background(
+                LinearGradient(
+                    stops: [
+                        Gradient.Stop(color: .primary.opacity(0.16), location: 0.00),
+                        Gradient.Stop(color: .primary.opacity(0.7), location: 0.50),
+                        Gradient.Stop(color: .primary.opacity(0.16), location: 1.00)
+                    ],
+                    startPoint: UnitPoint(x: 1, y: 0),
+                    endPoint: UnitPoint(x: 0, y: 0)
+                )
+            )
+    }
+}
+
+private struct RowItem: Identifiable {
+    let id = UUID()
+    let imageName: String
+    let title: String
+    let subtitle: String
+}
+
+@available(macOS 11.0, *)
+private struct BodyView: View {
+    let rowItems: [RowItem]
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: Constants.bodyViewSpacing) {
+            ForEach(rowItems) { item in
+                RowItemView(item: item)
+            }
+        }
+    }
+}
+
+@available(macOS 11.0, *)
+private struct RowItemView: View {
+    let item: RowItem
+
+    var body: some View {
+        HStack(alignment: .top, spacing: Constants.RowItem.horizontalSpacing) {
+            Image(systemName: item.imageName)
+                .resizable()
+                .frame(width: Constants.RowItem.imageSize.width, height: Constants.RowItem.imageSize.height)
+            VStack(alignment: .leading, spacing: Constants.RowItem.textVerticalSpacing) {
+                Text(item.title)
+                    .font(.title2)
+                Text(item.subtitle)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+private enum Constants {
+    enum CTA {
+        static let width: CGFloat = 440
+        static let height: CGFloat = 44
+    }
+
+    enum Divider {
+        static let width: CGFloat = 440
+        static let height: CGFloat = 2
+    }
+
+    enum RowItem {
+        static let horizontalSpacing: CGFloat = 16
+        static let textVerticalSpacing: CGFloat = 2
+        static let imageSize = CGSize(width: 24, height: 24)
+    }
+
+    enum ContainerView {
+        static let padding: CGFloat = 48
+        static let cornerRadius: CGFloat = 16
+        static let verticalSpacing: CGFloat = 24
+    }
+
+    static let bodyViewSpacing: CGFloat = 16
+    static let titleViewSpacing: CGFloat = 16
+}
+
+@available(macOS 11.0, *)
+struct GettingStartedView_Previews: PreviewProvider {
+    static var previews: some View {
+        GettingStartedView().frame(width: 600, height: 400)
+    }
+}
