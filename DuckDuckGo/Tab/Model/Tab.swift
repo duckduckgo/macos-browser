c63ae503b9494187c1c766f60c6fe6901120542f
Christopher Brind <brindy@duckduckgo.com>
2023-07-24 23:51:53 +0100

    if there's no icon, but the domain is the same, just keep the old icon

--- DuckDuckGo/Tab/Model/Tab.swift
+++ DuckDuckGo/Tab/Model/Tab.swift
@@ -498,7 +498,7 @@ protocol NewWindowPolicyDecisionMaker {
                 webView.stopMediaCapture()
                 webView.stopAllMediaPlayback()
             }
-            handleFavicon()
+            handleFavicon(oldValue: oldValue)
             invalidateInteractionStateData()
             error = nil
         }
@@ -934,7 +934,7 @@ protocol NewWindowPolicyDecisionMaker {
     @Published var favicon: NSImage?
     let faviconManagement: FaviconManagement
 
-    private func handleFavicon() {
+    private func handleFavicon(oldValue: TabContent? = nil) {
         guard content.isUrl, let url = content.urlForWebView else {
             favicon = nil
             return
@@ -951,7 +951,8 @@ protocol NewWindowPolicyDecisionMaker {
             if cachedFavicon != favicon {
                 favicon = cachedFavicon
             }
-        } else {
+        } else if oldValue?.url?.host != url.host {
+            // If the domain matches the previous value, just keep the same favicon
             favicon = nil
         }
     }
