c3f93f3e2d03c6355f76f3e5469ff608a524bd08
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-07-25 13:04:41 -0300

    Add fake broker setup with UI (#1384)

--- LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Scheduler/DataBrokerProtectionScheduler.swift
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Scheduler/DataBrokerProtectionScheduler.swift
@@ -68,14 +68,14 @@ public final class DataBrokerProtectionScheduler {
         self.notificationCenter = notificationCenter
     }
 
-    public func start(debug: Bool = false) {
+    public func start(useFakeBrokers: Bool, debug: Bool = true) {
         os_log("Starting scheduler...", log: .dataBrokerProtection)
         if debug {
-            self.dataBrokerProcessor.runQueuedOperations()
+            self.dataBrokerProcessor.runQueuedOperations(useFakeBroker: useFakeBrokers)
         } else {
             activity.schedule { completion in
                 os_log("Scheduler runnning...", log: .dataBrokerProtection)
-                self.dataBrokerProcessor.runQueuedOperations {
+                self.dataBrokerProcessor.runQueuedOperations(useFakeBroker: useFakeBrokers) {
                     completion(.finished)
                 }
             }
@@ -87,9 +87,9 @@ public final class DataBrokerProtectionScheduler {
         activity.invalidate()
     }
 
-    public func scanAllBrokers() {
+    public func scanAllBrokers(useFakeBrokers: Bool) {
         os_log("Scanning all brokers...", log: .dataBrokerProtection)
-        self.dataBrokerProcessor.runScanOnAllDataBrokers()
+        self.dataBrokerProcessor.runScanOnAllDataBrokers(useFakeBroker: useFakeBrokers)
     }
 
 }
