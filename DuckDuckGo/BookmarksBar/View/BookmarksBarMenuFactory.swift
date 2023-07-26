f7afc6caf4a1cd83ae684f68aa617a4838c5fcaf
Christopher Brind <brindy@duckduckgo.com>
2023-07-26 22:57:19 +0100

    move to common

--- DuckDuckGo/BookmarksBar/View/BookmarksBarMenuFactory.swift
+++ DuckDuckGo/BookmarksBar/View/BookmarksBarMenuFactory.swift
@@ -59,18 +59,3 @@ struct BookmarksBarMenuFactory {
     }
 
 }
-
-final class BlockMenuItem: NSMenuItem {
-    var block: (() -> Void)?
-
-    convenience init(title: String, keyEquivalent: String = "", isChecked: Bool, block: (() -> Void)?) {
-        self.init(title: title, action: #selector(performBlockAction(_:)), keyEquivalent: keyEquivalent)
-        self.block = block
-        self.target = self
-        self.state = isChecked ? .on : .off
-    }
-
-    @objc func performBlockAction(_ sender: NSMenuItem) {
-        block?()
-    }
-}
