197faad8dfcfbec8b2ff3843b61fdf3e6674b2a9
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-08-02 16:39:19 -0300

    Add secure storage setup (#1416)

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "68.1.1")
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "71.0.0")
     ],
     targets: [
         .target(
