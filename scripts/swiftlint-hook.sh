9c5193b99ded88a1f8aad1d97925a8f86cc3e956
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-08-04 13:44:38 +0200

    Update to download a local copy first

--- scripts/swiftlint-hook.sh
+++ scripts/swiftlint-hook.sh
@@ -2,6 +2,7 @@
 
 # URL where the installation/uninstallation script is hosted
 SCRIPT_URL="https://raw.githubusercontent.com/duckduckgo/BrowserServicesKit/daniel/swiftlint-hook/scripts/swiftlint-hook.sh"
+LOCAL_SCRIPT="local-install-pre-commit-hook.sh"
 
 # Check the argument for install or uninstall
 if [ "$1" != "--install" ] && [ "$1" != "--uninstall" ]; then
@@ -9,14 +10,23 @@ if [ "$1" != "--install" ] && [ "$1" != "--uninstall" ]; then
   exit 1
 fi
 
-# Download and execute the script with the given option
+# Download the script
 if command -v curl >/dev/null; then
-  curl -s "$SCRIPT_URL" | bash -s "$1"
+  curl -s -o "$LOCAL_SCRIPT" "$SCRIPT_URL"
 elif command -v wget >/dev/null; then
-  wget -O - "$SCRIPT_URL" | bash -s "$1"
+  wget -O "$LOCAL_SCRIPT" "$SCRIPT_URL"
 else
   echo "error: Neither curl nor wget are available on your system. Please install one of them to proceed."
   exit 1
 fi
+
+# Make the script executable
+chmod +x "$LOCAL_SCRIPT"
+
+# Execute the script with the given option
+./"$LOCAL_SCRIPT" "$1"
+
+# Optionally, remove the downloaded script
+rm "$LOCAL_SCRIPT"
 
 echo "Operation completed!"
