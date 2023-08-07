6fef202453a0dbc2683027862020f8ea4367384b
Tomas Strba <tom@duckduckgo.com>
2023-08-07 16:46:21 +0200

    SwiftLint

--- scripts/appcast_manager/appcastManager.swift
+++ scripts/appcast_manager/appcastManager.swift
@@ -1,5 +1,8 @@
 #!/usr/bin/swift
 
+// swiftlint:disable line_length
+// swiftlint:disable file_length
+
 import Foundation
 
 let appcastURLString = "https://staticcdn.duckduckgo.com/macos-desktop-browser/appcast2.xml"
@@ -23,7 +26,7 @@ enum Action: String {
 struct Arguments {
     let action: Action
     let parameters: [String: String]
-    
+
     init(args: [String]) throws {
         guard args.count > 1 else {
             throw NSError(domain: "ArgumentsError", code: 1000, userInfo: ["message": "Missing action argument."])
@@ -32,15 +35,15 @@ struct Arguments {
             throw NSError(domain: "ArgumentsError", code: 1001, userInfo: ["message": "Unknown action."])
         }
         self.action = action
-        
+
         let remainingArgs = Array(args.dropFirst(2))
         guard remainingArgs.count % 2 == 0 else {
             throw NSError(domain: "ArgumentsError", code: 1002, userInfo: ["message": "Each argument must have a corresponding value."])
         }
-        
+
         var parameters: [String: String] = [:]
-        for i in stride(from: 0, to: remainingArgs.count, by: 2) {
-            parameters[remainingArgs[i]] = remainingArgs[i+1]
+        for index in stride(from: 0, to: remainingArgs.count, by: 2) {
+            parameters[remainingArgs[index]] = remainingArgs[index+1]
         }
         self.parameters = parameters
     }
@@ -59,7 +62,7 @@ do {
 
 // Use the arguments
 switch arguments.action {
-    
+
 case .help:
     print("""
 NAME
@@ -93,61 +96,59 @@ DESCRIPTION
         Displays this help message.
 
 """)
-    
+
     exit(0)
-    
-    
-    
+
 case .releaseToInternalChannel, .releaseHotfixToPublicChannel:
     guard let dmgPath = arguments.parameters["--dmg"], let releaseNotesPath = arguments.parameters["--release-notes"] else {
         print("Missing required parameters for action '--add-to-internal-channel'")
         exit(1)
     }
-    
+
     print("Action: Add to internal channel")
     print("DMG Path: \(dmgPath)")
     print("Release Notes Path: \(releaseNotesPath)")
-    
+
     // Download appcast and update files
     AppcastDownloader().download()
-    
+
     // Handle dmg file
     let updatesDirectoryPath = NSString(string: "~/Developer/sparkle-updates").expandingTildeInPath
     let updatesDirectoryURL = URL(fileURLWithPath: updatesDirectoryPath)
     guard let dmgURL = handleDMGFile(dmgPath: dmgPath, updatesDirectoryURL: updatesDirectoryURL) else {
         exit(1)
     }
-    
+
     // Handle release notes file
     handleReleaseNotesFile(path: releaseNotesPath, updatesDirectoryURL: updatesDirectoryURL, dmgURL: dmgURL)
-    
+
     // Extract version number from DMG file name
     let versions = getVersionFromDMGFileName(dmgURL: dmgURL)
-    
+
     // Differentiate between the two actions
     if arguments.action == .releaseToInternalChannel {
         runGenerateAppcast(withVersions: versions, channel: "internal-channel")
     } else {
         runGenerateAppcast(withVersions: versions)
     }
-    
+
 case .releaseToPublicChannel:
     guard let version = arguments.parameters["--version"] else {
         print("Missing required version parameter for action '--release-to-public-channel'")
         exit(1)
     }
-    
+
     print("Action: Release to public channel")
     print("Version: \(version)")
-    
+
     // Download appcast and update files
     AppcastDownloader().download()
-    
+
     // Verify version
     if !verifyVersion(version: version, atDirectory: specificDir) {
         exit(1)
     }
-    
+
     // Handle release notes if provided
     if let releaseNotesPath = arguments.parameters["--release-notes"] {
         print("Release Notes Path: \(releaseNotesPath)")
@@ -156,31 +157,31 @@ case .releaseToPublicChannel:
     } else {
         print("No new release notes provided. Keeping existing release notes.")
     }
-    
+
     // Process appcast content
     if !processAppcastRemovingVersion(version: version, appcastFilePath: appcastFilePath) {
         exit(1)
     }
-    
+
     runGenerateAppcast(withVersions: version, rolloutInterval: "43200")
 }
 
-// MARK: - Generating of new appcast and delta file
+// MARK: - Downloading of Appcast and Files
 
 class AppcastDownloader {
-    
+
     private let dispatchGroup = DispatchGroup()
-    
+
     func download() {
         prepareDirectories()
         downloadAppcast()
         dispatchGroup.wait()
         parseAndDownloadFilesFromAppcast()
         dispatchGroup.wait()
-        
+
         print("All builds downloaded.")
     }
-    
+
     private func prepareDirectories() {
         // Delete directory if it already exists
         if FileManager.default.fileExists(atPath: specificDir.path) {
@@ -192,7 +193,7 @@ class AppcastDownloader {
                 exit(1)
             }
         }
-        
+
         // Create new directory
         do {
             try FileManager.default.createDirectory(at: specificDir, withIntermediateDirectories: true, attributes: nil)
@@ -202,15 +203,15 @@ class AppcastDownloader {
             exit(1)
         }
     }
-    
+
     private func downloadFile(_ url: URL, _ destinationURL: URL, completion: @escaping (Error?) -> Void) {
         print("Downloading file from: \(url.absoluteString)")
-        let task = URLSession.shared.downloadTask(with: url) { (location, response, error) in
+        let task = URLSession.shared.downloadTask(with: url) { (location, _, error) in
             guard let location = location else {
                 completion(error)
                 return
             }
-            
+
             do {
                 try FileManager.default.moveItem(at: location, to: destinationURL)
                 completion(nil)
@@ -220,7 +221,7 @@ class AppcastDownloader {
         }
         task.resume()
     }
-    
+
     private func downloadAppcast() {
         dispatchGroup.enter()
         downloadFile(appcastURL, appcastFilePath) { [self] error in
@@ -232,7 +233,7 @@ class AppcastDownloader {
             self.dispatchGroup.leave()
         }
     }
-    
+
     private func parseAndDownloadFilesFromAppcast() {
         let parser = XMLParser(contentsOf: specificDir.appendingPathComponent("appcast2.xml"))
         let delegate = AppcastXMLParserDelegate(downloadFile: downloadFile, dispatchGroup: dispatchGroup)
@@ -244,22 +245,22 @@ class AppcastDownloader {
             }
         }
     }
-    
+
     class AppcastXMLParserDelegate: NSObject, XMLParserDelegate {
         var currentElement: String = ""
         var enclosureURL: String = ""
-        var releaseNotesHTML: String? = nil
-        var currentVersion: String? = nil
-        
+        var releaseNotesHTML: String?
+        var currentVersion: String?
+
         private let downloadFile: (URL, URL, @escaping (Error?) -> Void) -> Void
         private let dispatchGroup: DispatchGroup
-        
+
         init(downloadFile: @escaping (URL, URL, @escaping (Error?) -> Void) -> Void, dispatchGroup: DispatchGroup) {
             self.downloadFile = downloadFile
             self.dispatchGroup = dispatchGroup
         }
-        
-        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+
+        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String] = [:]) {
             currentElement = elementName
             if elementName == "enclosure" {
                 enclosureURL = attributeDict["url"] ?? ""
@@ -281,7 +282,7 @@ class AppcastDownloader {
                 releaseNotesHTML = nil
             }
         }
-        
+
         func parser(_ parser: XMLParser, foundCharacters string: String) {
             if currentElement == "description" {
                 releaseNotesHTML = (releaseNotesHTML ?? "") + string.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -289,7 +290,7 @@ class AppcastDownloader {
                 currentVersion = ((currentVersion ?? "") + string.trimmingCharacters(in: .whitespacesAndNewlines))
             }
         }
-        
+
         func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
             if elementName == "item" {
                 if let releaseNotesHTML = releaseNotesHTML, let version = currentVersion {
@@ -304,7 +305,7 @@ class AppcastDownloader {
                 }
             }
         }
-        
+
         func parser(_ parser: XMLParser, parseErrorOccurred parseError: Error) {
             print("XML Parse Error: \(parseError)")
             exit(1)
@@ -312,22 +313,22 @@ class AppcastDownloader {
     }
 }
 
-// MARK: -
+// MARK: - Handling of Release Notes
 
-func handleReleaseNotesFile(path: String, updatesDirectoryURL: URL, dmgURL: URL) -> Void {
+func handleReleaseNotesFile(path: String, updatesDirectoryURL: URL, dmgURL: URL) {
     // Copy release notes file and rename it to match the dmg filename
     let releaseNotesURL = URL(fileURLWithPath: path)
     let destinationReleaseNotesURL = updatesDirectoryURL.appendingPathComponent(dmgURL.deletingPathExtension().lastPathComponent + ".html")
-    
+
     do {
         if FileManager.default.fileExists(atPath: destinationReleaseNotesURL.path) {
             try FileManager.default.removeItem(at: destinationReleaseNotesURL)
             print("Old release notes file removed.")
         }
-        
+
         // Load the release notes from the txt file
         let releaseNotes = try String(contentsOf: releaseNotesURL, encoding: .utf8)
-        
+
         // Convert the release notes to HTML
         let releaseNotesLines = releaseNotes.split(separator: "\n")
         let releaseNotesListItems = releaseNotesLines.map { "<li>\($0)</li>" }.joined(separator: "\n")
@@ -337,11 +338,11 @@ func handleReleaseNotesFile(path: String, updatesDirectoryURL: URL, dmgURL: URL)
         \(releaseNotesListItems)
         </ul>
         """
-        
+
         // Save the converted release notes to the destination file
         try releaseNotesHTML.write(to: destinationReleaseNotesURL, atomically: true, encoding: .utf8)
         print("New release notes file copied to the updates directory and converted to HTML.")
-        
+
     } catch {
         print("Failed to copy and convert release notes file: \(error).")
         exit(1)
@@ -362,8 +363,8 @@ func getVersionFromDMGFileName(dmgURL: URL) -> String {
     return versions
 }
 
+// MARK: - Handling of DMG Files
 
-// Use the arguments
 func handleDMGFile(dmgPath: String, updatesDirectoryURL: URL) -> URL? {
     let dmgURL = URL(fileURLWithPath: dmgPath)
     let destinationDMGURL = updatesDirectoryURL.appendingPathComponent(dmgURL.lastPathComponent)
@@ -393,6 +394,8 @@ func verifyVersion(version: String, atDirectory dir: URL) -> Bool {
     }
 }
 
+// MARK: - Processing of Appcast
+
 func processAppcastRemovingVersion(version: String, appcastFilePath: URL) -> Bool {
     guard let appcastContent = readAppcastContent(from: appcastFilePath) else {
         print("Failed to read the appcast file.")
@@ -413,7 +416,7 @@ func readAppcastContent(from filePath: URL) -> String? {
 
 func removeVersionFromAppcast(_ version: String, appcastContent: String) -> String? {
     let pattern = "(<item>\\s*<title>\\s*\(version)\\s*</title>.*?</item>)"
-    
+
     guard let regex = try? NSRegularExpression(pattern: pattern, options: .dotMatchesLineSeparators),
           let startMatch = regex.firstMatch(in: appcastContent, range: NSRange(appcastContent.startIndex..., in: appcastContent)),
           let endRange = appcastContent.range(of: "</item>", options: [], range: Range(startMatch.range, in: appcastContent)!)
@@ -421,7 +424,7 @@ func removeVersionFromAppcast(_ version: String, appcastContent: String) -> Stri
         print("Failed to match version \(version) in the appcast content.")
         return nil
     }
-    
+
     var modifiedAppcastContent = appcastContent
     modifiedAppcastContent.removeSubrange(Range(startMatch.range, in: appcastContent)!.lowerBound..<endRange.upperBound)
     return modifiedAppcastContent
@@ -436,6 +439,8 @@ func writeAppcastContent(_ content: String, to filePath: URL) {
     }
 }
 
+// MARK: - Generating of New Appcast
+
 func runGenerateAppcast(withVersions versions: String, channel: String? = nil, rolloutInterval: String? = nil) {
     // Check if backup file already exists and remove it
     if FileManager.default.fileExists(atPath: backupFileURL.path) {
@@ -446,7 +451,7 @@ func runGenerateAppcast(withVersions versions: String, channel: String? = nil, r
             exit(1)
         }
     }
-    
+
     // Create a new backup of the appcast file
     do {
         try FileManager.default.copyItem(at: appcastFilePath, to: backupFileURL)
@@ -454,44 +459,44 @@ func runGenerateAppcast(withVersions versions: String, channel: String? = nil, r
         print("Error backing up appcast2.xml: \(error)")
         exit(1)
     }
-    
+
     let maximumVersions = "2"
     let maximumDeltas = "2"
-    
+
     var commandComponents: [String] = []
     commandComponents.append("generate_appcast")
     commandComponents.append("--versions \(versions)")
     commandComponents.append("--maximum-versions \(maximumVersions)")
     commandComponents.append("--maximum-deltas \(maximumDeltas)")
-    
+
     if let channel = channel {
         commandComponents.append("--channel \(channel)")
     }
-    
+
     if let rolloutInterval = rolloutInterval {
         commandComponents.append("--phased-rollout-interval \(rolloutInterval)")
     }
-    
+
     commandComponents.append("--embed-release-notes")
     commandComponents.append("--auto-prune-update-files \(specificDir.path)")
-    
+
     let command = commandComponents.joined(separator: " ")
-    
+
     // Execute the command
     let task = Process()
     task.launchPath = "/usr/bin/env"
     task.arguments = ["bash", "-c", command]
-    
+
     task.launch()
     task.waitUntilExit()
-    
+
     if task.terminationStatus != 0 {
         print("generate_appcast command failed with exit code \(task.terminationStatus).")
         exit(1)
     }
-    
+
     print("generate_appcast command executed successfully.")
-    
+
     // Get and save the diff
     let diffResult = shell("diff", backupAppcastFilePath, appcastFilePath.path)
     let diffFilePath = specificDir.appendingPathComponent("appcast_diff.txt").path
@@ -501,7 +506,7 @@ func runGenerateAppcast(withVersions versions: String, channel: String? = nil, r
     } catch {
         print("Error writing diff to file: \(error)")
     }
-    
+
     // Open specific directory in Finder
     shell("open", specificDir.path)
 }
@@ -510,12 +515,14 @@ func runGenerateAppcast(withVersions versions: String, channel: String? = nil, r
     let task = Process()
     task.launchPath = "/usr/bin/env"
     task.arguments = [command] + arguments
-    
+
     let pipe = Pipe()
     task.standardOutput = pipe
     task.launch()
     task.waitUntilExit()
-    
+
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     return String(data: data, encoding: .utf8) ?? ""
 }
+
+// swiftlint:enable line_length
