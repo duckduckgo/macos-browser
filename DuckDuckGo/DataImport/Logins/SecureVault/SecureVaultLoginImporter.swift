0739bfa369bb40c92bdf0f530b085ce953061843
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-08-11 17:52:00 +0200

    Add support for syncing Credentials (#1368)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1204029872658872/f
    Tech Design URL: https://app.asana.com/0/481882893211075/1204906306298609/f
    
    Description:
    Update Autofill UI data models and related code to account's domain and username being optional.
    Move Bookmarks Database Cleaner to SyncBookmarksAdapter.
    Add SyncCredentialsAdapter (and it also contains CredentialsDatabaseCleaner).
    Added sync triggers to Autofill UI and fixed UI to preserve Logins list selection when it's reloaded,
    e.g. by sync.

--- DuckDuckGo/DataImport/Logins/SecureVault/SecureVaultLoginImporter.swift
+++ DuckDuckGo/DataImport/Logins/SecureVault/SecureVaultLoginImporter.swift
@@ -35,26 +35,31 @@ final class SecureVaultLoginImporter: LoginImporter {
         var duplicates: [String] = []
         var failed: [String] = []
 
-        for login in logins {
-            let title = login.title
-            let account = SecureVaultModels.WebsiteAccount(title: title, username: login.username, domain: login.url)
-            let credentials = SecureVaultModels.WebsiteCredentials(account: account, password: login.password.data(using: .utf8)!)
-            let importSummaryValue: String
+        let encryptionKey = try vault.getEncryptionKey()
+        let hashingSalt = try vault.getHashingSalt()
 
-            if let title = account.title {
-                importSummaryValue = "\(title): \(credentials.account.domain) (\(credentials.account.username))"
-            } else {
-                importSummaryValue = "\(credentials.account.domain) (\(credentials.account.username))"
-            }
+        try vault.inDatabaseTransaction { database in
+            for login in logins {
+                let title = login.title
+                let account = SecureVaultModels.WebsiteAccount(title: title, username: login.username, domain: login.url)
+                let credentials = SecureVaultModels.WebsiteCredentials(account: account, password: login.password.data(using: .utf8)!)
+                let importSummaryValue: String
 
-            do {
-                _ = try vault.storeWebsiteCredentials(credentials)
-                successful.append(importSummaryValue)
-            } catch {
-                if case .duplicateRecord = error as? SecureStorageError {
-                    duplicates.append(importSummaryValue)
+                if let title = account.title {
+                    importSummaryValue = "\(title): \(credentials.account.domain ?? "") (\(credentials.account.username ?? ""))"
                 } else {
-                    failed.append(importSummaryValue)
+                    importSummaryValue = "\(credentials.account.domain ?? "") (\(credentials.account.username ?? ""))"
+                }
+
+                do {
+                    _ = try vault.storeWebsiteCredentials(credentials, in: database, encryptedUsing: encryptionKey, hashedUsing: hashingSalt)
+                    successful.append(importSummaryValue)
+                } catch {
+                    if case .duplicateRecord = error as? SecureStorageError {
+                        duplicates.append(importSummaryValue)
+                    } else {
+                        failed.append(importSummaryValue)
+                    }
                 }
             }
         }
