e12fb652c6a0f8414953003bcb24a0c3534c67e0
Sabrina Tardio <44158575+SabrinaTardio@users.noreply.github.com>
2023-08-16 14:54:03 +0200

    revise www dropping (#1426)
    Task/Issue URL:
    https://app.asana.com/0/1177771139624306/1201939328153696/f
    
    **Description**: Revise the use of dropping www. Will remove the ww
    dropping from the logic. It also allow autocomplete to work when the
    user types www

--- DuckDuckGo/Favicons/Model/FaviconReferenceCache.swift
+++ DuckDuckGo/Favicons/Model/FaviconReferenceCache.swift
@@ -117,8 +117,7 @@ final class FaviconReferenceCache {
             default: return urlCacheEntry.mediumFaviconUrl
             }
         } else if let host = documentURL.host,
-                    let hostCacheEntry = hostReferences[host] ?? (host.hasPrefix("www") ?
-                                                                  hostReferences[host.droppingWwwPrefix()] : hostReferences["www.\(host)"]) {
+                    let hostCacheEntry = hostReferences[host] {
             switch sizeCategory {
             case .small: return hostCacheEntry.smallFaviconUrl ?? hostCacheEntry.mediumFaviconUrl
             default: return hostCacheEntry.mediumFaviconUrl
@@ -133,7 +132,7 @@ final class FaviconReferenceCache {
             return nil
         }
 
-        let hostCacheEntry = hostReferences[host] ?? (host.hasPrefix("www") ? hostReferences[host.droppingWwwPrefix()] : hostReferences["www.\(host)"])
+        let hostCacheEntry = hostReferences[host]
 
         switch sizeCategory {
         case .small: return hostCacheEntry?.smallFaviconUrl ?? hostCacheEntry?.mediumFaviconUrl
