20db8c1167d766848355500e4afb5dbd5f21de88
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-08-20 22:14:40 +0200

    Create DMG within archive.sh

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -307,7 +307,6 @@ create_dmg() {
 export_app_version_to_environment() {
 	if [[ -n "${GITHUB_ENV}" ]]; then
 		echo "app-version=${app_version}" >> "${GITHUB_ENV}"
-		echo "app-name=${app_name}" >> "${GITHUB_ENV}"
 	fi
 }
 
