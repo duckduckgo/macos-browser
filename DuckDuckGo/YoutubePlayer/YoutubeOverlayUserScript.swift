f779a1cdba1d08122914328657a55a222274fa41
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-08-21 22:28:01 +0200

    Require Duck Player scheme URL to be passed from YouTube Overlay User Script (#1519)
    Task/Issue URL: https://app.asana.com/0/0/1205318681239304/f
    
    Description:
    Validate the message and return early if the passed URL is different than duck://....

--- DuckDuckGo/YoutubePlayer/YoutubeOverlayUserScript.swift
+++ DuckDuckGo/YoutubePlayer/YoutubeOverlayUserScript.swift
@@ -86,9 +86,10 @@ final class YoutubeOverlayUserScript: NSObject, Subfeature {
         guard let dict = params as? [String: Any],
               let href = dict["href"] as? String,
               let url = href.url,
+              url.isDuckPlayerScheme,
               let webView = message.messageWebView
         else {
-            assertionFailure("YoutubeOverlayUserScript: expected URL")
+            assertionFailure("YoutubeOverlayUserScript: expected duck:// URL")
             return nil
         }
         self.delegate?.youtubeOverlayUserScriptDidRequestDuckPlayer(with: url, in: webView)
