0b44adb1438300549014426ac671f1e7720042f7
Diego Rey Mendez <diegoreymendez@users.noreply.github.com>
2023-08-23 16:16:13 +0200

    Cleans up some code (#1517)
    Task/Issue URL: https://app.asana.com/0/0/1205318856585879/f
    
    ## Description
    
    Small code cleanup PR.  There should be no logical changes.
    
    ## Scope
    
    This task is not meant to be a perfect cleanup, just moving some code to
    a separate file to make the app delegate less cluttered.

--- DuckDuckGo/AppDelegate/AppDelegate.swift
+++ DuckDuckGo/AppDelegate/AppDelegate.swift
@@ -211,7 +211,9 @@ final class AppDelegate: NSObject, NSApplicationDelegate, FileDownloadManagerDel
         UserDefaultsWrapper<Any>.clearRemovedKeys()
 
 #if NETWORK_PROTECTION
-        startupNetworkProtection()
+        if #available(macOS 11.4, *) {
+            NetworkProtectionAppEvents().applicationDidFinishLaunching()
+        }
 #endif
     }
 
@@ -310,82 +312,6 @@ final class AppDelegate: NSObject, NSApplicationDelegate, FileDownloadManagerDel
             self?.syncService?.authState == .active
         }
     }
-
-    // MARK: - Network Protection
-
-#if NETWORK_PROTECTION
-
-    private func startupNetworkProtection() {
-        guard #available(macOS 11.4, *) else { return }
-
-        let loginItemsManager = NetworkProtectionLoginItemsManager()
-        let networkProtectionFeatureVisibility = NetworkProtectionKeychainTokenStore()
-
-        guard networkProtectionFeatureVisibility.isFeatureActivated else {
-            loginItemsManager.disableLoginItems()
-            LocalPinningManager.shared.unpin(.networkProtection)
-            return
-        }
-
-        restartNetworkProtectionIfVersionChanged(using: loginItemsManager)
-        refreshNetworkProtectionServers()
-    }
-
-    @available(macOS 11.4, *)
-    private func restartNetworkProtectionIfVersionChanged(using loginItemsManager: NetworkProtectionLoginItemsManager) {
-        let currentVersion = AppVersion.shared.versionNumber
-        let versionStore = NetworkProtectionLastVersionRunStore()
-        defer {
-            versionStore.lastVersionRun = currentVersion
-        }
-
-        // should‘ve been run at least once with NetP enabled
-        guard let lastVersionRun = versionStore.lastVersionRun else {
-            os_log(.info, log: .networkProtection, "No last version found for the NetP login items, skipping update")
-            return
-        }
-
-        if lastVersionRun != currentVersion {
-            os_log(.info, log: .networkProtection, "App updated from %{public}s to %{public}s: updating login items", lastVersionRun, currentVersion)
-            restartNetworkProtectionTunnelAndMenu(using: loginItemsManager)
-        } else {
-            // If login items failed to launch (e.g. because of the App bundle rename), launch using NSWorkspace
-            loginItemsManager.ensureLoginItemsAreRunning(.ifLoginItemsAreEnabled, after: 1)
-        }
-    }
-
-    @available(macOS 11.4, *)
-    private func restartNetworkProtectionTunnelAndMenu(using loginItemsManager: NetworkProtectionLoginItemsManager) {
-        loginItemsManager.restartLoginItems()
-
-        Task {
-            let provider = NetworkProtectionTunnelController()
-
-            // Restart NetP SysEx on app update
-            if await provider.isConnected {
-                await provider.stop()
-                await provider.start()
-            }
-        }
-    }
-
-    /// Fetches a new list of Network Protection servers, and updates the existing set.
-    @available(macOS 11.4, *)
-    private func refreshNetworkProtectionServers() {
-        Task {
-            let serverCount: Int
-            do {
-                serverCount = try await NetworkProtectionDeviceManager.create().refreshServerList().count
-            } catch {
-                os_log("Failed to update Network Protection servers", log: .networkProtection, type: .error)
-                return
-            }
-
-            os_log("Successfully updated Network Protection servers; total server count = %{public}d", log: .networkProtection, serverCount)
-        }
-    }
-
-#endif
 
     private func subscribeToEmailProtectionStatusNotifications() {
         NotificationCenter.default.addObserver(self,
