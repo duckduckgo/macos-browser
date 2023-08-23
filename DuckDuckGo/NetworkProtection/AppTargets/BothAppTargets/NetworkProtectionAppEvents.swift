a5f22709879ff6d7d2acc1a9d5d82c70c6dadbd5
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-08-23 15:41:00 +0200

    Renames a method to better follow our standard naming

--- DuckDuckGo/NetworkProtection/AppTargets/BothAppTargets/NetworkProtectionAppEvents.swift
+++ DuckDuckGo/NetworkProtection/AppTargets/BothAppTargets/NetworkProtectionAppEvents.swift
@@ -29,7 +29,7 @@ final class NetworkProtectionAppEvents {
 
     /// Call this method when the app finishes launching, to run the startup logic for NetP.
     ///
-    func appLaunchComplete() {
+    func applicationDidFinishLaunching() {
         let loginItemsManager = NetworkProtectionLoginItemsManager()
         let keychainStore = NetworkProtectionKeychainTokenStore()
 
