703f63eb66d242b27fe0b29b7d78c881b8835154
Fernando Bunn <bunn@idevzilla.com>
2023-08-24 18:46:14 +0100

    Update DBP BSK version

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "75.0.4"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "75.1.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
