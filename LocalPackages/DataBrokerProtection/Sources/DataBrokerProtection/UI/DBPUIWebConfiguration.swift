2a040d4c4fa9f1f757a3a2a8e1256806005a0952
Brad Slayter <SlayterDev@users.noreply.github.com>
2023-09-01 09:29:14 -0500

    DBP UI Middleware  (#1553)
    Task/Issue URL:
    https://app.asana.com/0/72649045549333/1205205203094796/f
    Tech Design URL:
    https://app.asana.com/0/481882893211075/1205250208033190/f
    CC: @Bunn @jotaemepereira 
    
    **Description**:
    This PR adds a middleware layer to communicate with the DBP UI
    
    Web UI is in progress. For the moment I have set up a debug page to test
    this PR.
    
    **Steps to test this PR**:
    1. Open Data Broker Protection
    2. You should be presented with a white page with a few buttons
    3. Attach a debugger to the webview either through Safari or by directly
    opening dev tools
    4. Clicking either of the top two buttons on the page should display a
    log in the Xcode console
    5. Clicking the button below these should show a log for `missing
    'setState'` in the web console (this is correct)
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)
    
    ---------
    
    Co-authored-by: Fernando Bunn <bunn@idevzilla.com>

---
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/UI/DBPUIWebConfiguration.swift
@@ -0,0 +1,92 @@
+//
+//  DBPUIWebConfiguration.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import WebKit
+import BrowserServicesKit
+import UserScript
+
+@MainActor
+final class DBPUIUserContentController: WKUserContentController {
+
+    let dbpUIUserScripts: DBPUIUserScript
+
+    init(with privacyConfigurationManager: PrivacyConfigurationManaging, prefs: ContentScopeProperties, delegate: DBPUICommunicationDelegate) {
+        dbpUIUserScripts = DBPUIUserScript(privacyConfig: privacyConfigurationManager, prefs: prefs, delegate: delegate)
+
+        super.init()
+
+        dbpUIUserScripts.userScripts.forEach {
+            let userScript = $0.makeWKUserScriptSync()
+            self.installUserScripts([userScript], handlers: [$0])
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func installUserScripts(_ wkUserScripts: [WKUserScript], handlers: [UserScript]) {
+        handlers.forEach { self.addHandler($0) }
+        wkUserScripts.forEach(self.addUserScript)
+    }
+}
+
+@MainActor
+final class DBPUIUserScript: UserScriptsProvider {
+    lazy var userScripts: [UserScript] = [contentScopeUserScriptIsolated]
+
+    let contentScopeUserScriptIsolated: ContentScopeUserScript
+    var dbpUICommunicationLayer: DBPUICommunicationLayer
+
+    init(privacyConfig: PrivacyConfigurationManaging, prefs: ContentScopeProperties, delegate: DBPUICommunicationDelegate) {
+        contentScopeUserScriptIsolated = ContentScopeUserScript(privacyConfig, properties: prefs, isIsolated: false)
+        contentScopeUserScriptIsolated.messageNames = ["dbpui"]
+        dbpUICommunicationLayer = DBPUICommunicationLayer()
+        dbpUICommunicationLayer.delegate = delegate
+        dbpUICommunicationLayer.broker = contentScopeUserScriptIsolated.broker
+        contentScopeUserScriptIsolated.registerSubfeature(delegate: dbpUICommunicationLayer)
+    }
+
+    @MainActor
+    func loadWKUserScripts() async -> [WKUserScript] {
+        return await withTaskGroup(of: WKUserScriptBox.self) { @MainActor group in
+            var wkUserScripts = [WKUserScript]()
+            userScripts.forEach { userScript in
+                group.addTask { @MainActor in
+                    await userScript.makeWKUserScript()
+                }
+            }
+            for await result in group {
+                wkUserScripts.append(result.wkUserScript)
+            }
+
+            return wkUserScripts
+        }
+    }
+}
+
+extension WKWebViewConfiguration {
+
+    @MainActor
+    func applyDBPUIConfiguration(privacyConfig: PrivacyConfigurationManaging, prefs: ContentScopeProperties, delegate: DBPUICommunicationDelegate) {
+        preferences.isFraudulentWebsiteWarningEnabled = false
+        let userContentController = DBPUIUserContentController(with: privacyConfig, prefs: prefs, delegate: delegate)
+        self.userContentController = userContentController
+     }
+}
