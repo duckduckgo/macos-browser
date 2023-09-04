99f1ab3ca16f589a17708fad097d07429d62bd04
Sam Symons <sam@samsymons.com>
2023-09-03 19:16:06 -0700

    Set BSK version to 76.0.0.

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "75.3.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "76.0.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
