2d98a52333954be46e8bdd23255d4abffa743d14
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-09-04 11:32:01 +0200

    Point to BSK Release

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "75.2.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "76.1.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
