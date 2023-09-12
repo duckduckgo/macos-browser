f94bb423f5141b3e81e98c2e575325b68dc82793
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-09-12 18:50:04 +0200

    Updates to BSK 77.3.1

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "77.2.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "77.3.1"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
