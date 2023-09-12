7f866e65d41e2c6134ed209bb935a4027d42a600
Alexey Martemyanov <mallexxx@gmail.com>
2023-09-12 12:52:43 +0600

    set BSK to 77.3.0

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "77.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "77.3.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
--- LocalPackages/NetworkProtectionUI/Package.swift
+++ LocalPackages/NetworkProtectionUI/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["NetworkProtectionUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "77.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "77.3.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
