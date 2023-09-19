83c1ba5de46f27c587255944bedad9f089d98141
Daniel Bernal <1156669+afterxleep@users.noreply.github.com>
2023-09-19 18:25:14 +0200

    Open Home Page on New Windows Only

--- DuckDuckGo/Tab/Model/Tab.swift
+++ DuckDuckGo/Tab/Model/Tab.swift
@@ -387,11 +387,6 @@ protocol NewWindowPolicyDecisionMaker {
             handleFavicon()
         }
 
-        // Navigate to custom Home page if required
-        if content == .homePage {
-            openHomePage()
-        }
-
         emailDidSignOutCancellable = NotificationCenter.default.publisher(for: .emailDidSignOut)
             .receive(on: DispatchQueue.main)
             .sink { [weak self] notification in
