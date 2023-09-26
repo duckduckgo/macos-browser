b69767851b7dadc9339506e38926c03d14b4ce18
Chris Brind <brindy@duckduckgo.com>
2023-09-26 17:54:02 +0100

    force update embedded files

--- DuckDuckGo/ContentBlocker/AppPrivacyConfigurationDataProvider.swift
+++ DuckDuckGo/ContentBlocker/AppPrivacyConfigurationDataProvider.swift
@@ -22,8 +22,8 @@ import BrowserServicesKit
 final class AppPrivacyConfigurationDataProvider: EmbeddedDataProvider {
 
     public struct Constants {
-        public static let embeddedDataETag = "\"0eee50c39678bb8acd8e9336b1e395a8\""
-        public static let embeddedDataSHA = "38ea8996855aab006a6ff03b00f658e4d038ffcf7a2268acfecd43a0fcf44729"
+        public static let embeddedDataETag = "\"9b3b456b8a5b1c6176e390f33fc37e61\""
+        public static let embeddedDataSHA = "43195da72b1cbcbe8c515b18e896385e633902cf577f670425980e80053c6a7c"
     }
 
     var embeddedDataEtag: String {
--- DuckDuckGo/ContentBlocker/AppTrackerDataSetProvider.swift
+++ DuckDuckGo/ContentBlocker/AppTrackerDataSetProvider.swift
@@ -23,7 +23,7 @@ final class AppTrackerDataSetProvider: EmbeddedDataProvider {
 
     public struct Constants {
         public static let embeddedDataETag = "\"c9a18e593a4f5f8d32a18db294497226\""
-        public static let embeddedDataSHA = "68238637d6f5961fdb75c2ce64e69c2ff582db5d4dfe9b895b6d8419d408a11f"
+        public static let embeddedDataSHA = "6264eb92240fee69aeebb14aa333bdc8d76b2818fb7f44f0acb1fb0373d50af4"
     }
 
     var embeddedDataEtag: String {
