a70eb0e0e4fe92f65204d93f78d59b409dc8a8d2
Chris Brind <brindy@duckduckgo.com>
2023-09-27 17:42:51 +0100

    update to bsk which updates to autofill 8.4.1

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "80.1.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "hotfix/80.1.0-1"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
