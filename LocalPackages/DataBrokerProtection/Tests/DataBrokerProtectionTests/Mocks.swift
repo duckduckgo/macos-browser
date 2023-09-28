2b02d23eea383650dfe94541a1fb29562a4a452a
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-09-28 13:31:56 -0300

    Opt-out pixels improvements

--- LocalPackages/DataBrokerProtection/Tests/DataBrokerProtectionTests/Mocks.swift
+++ LocalPackages/DataBrokerProtection/Tests/DataBrokerProtectionTests/Mocks.swift
@@ -576,6 +576,8 @@ public class MockDataBrokerProtectionPixelsHandler: EventMapping<DataBrokerProte
                 print("PIXEL: optOutValidate")
             case .optOutFinish:
                 print("PIXEL: optOutFinish")
+            case .optOutSubmitSuccess:
+                print("PIXEL: optOutSubmitSuccess")
             case .optOutSuccess:
                 print("PIXEL: optOutSuccess")
             case .optOutFailure:
