9e011ce000830efa9c74e0c75f43850d76502898
Tom Strba <57389842+tomasstrba@users.noreply.github.com>
2023-10-06 15:27:22 +0200

    Address bar parsing issue in new SDK resolved (#1724)
    Task/Issue URL: https://app.asana.com/0/0/1205638643093186/f
    Tech Design URL:
    CC:
    
    **Description**:
    Taking care of new logic in `URL(string:)` constructor. See
    [documentation](https://developer.apple.com/documentation/foundation/url/3126806-init)
    for more info

--- LocalPackages/Account/Package.swift
+++ LocalPackages/Account/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Account"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "80.3.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "80.3.0-1"),
         .package(path: "../Purchase")
     ],
     targets: [
