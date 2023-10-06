ff9eda305e8b8b6f05be58a74aefaf0213ef2664
Sam Symons <sam@samsymons.com>
2023-10-06 16:00:22 -0700

    TEMPORARY: Fire a test pixel when the agent starts.

--- DuckDuckGoVPN/DuckDuckGoVPNAppDelegate.swift
+++ DuckDuckGoVPN/DuckDuckGoVPNAppDelegate.swift
@@ -137,6 +137,20 @@ final class DuckDuckGoVPNAppDelegate: NSObject, NSApplicationDelegate {
 
         // Initialize the IPC server
         _ = tunnelControllerIPCServer
+
+        PixelKit.setUp(dryRun: false, appVersion: AppVersion.shared.versionNumber, defaultHeaders: [:], log: .networkProtectionPixel) { (pixelName: String, headers: [String: String], parameters: [String: String], _, _, onComplete: @escaping (Error?) -> Void) in
+
+            let url = URL.pixelUrl(forPixelNamed: pixelName)
+            let apiHeaders = APIRequest.Headers(additionalHeaders: headers) // workaround - Pixel class should really handle APIRequest.Headers by itself
+            let configuration = APIRequest.Configuration(url: url, method: .get, queryParameters: parameters, headers: apiHeaders)
+            let request = APIRequest(configuration: configuration)
+
+            request.fetch { _, error in
+                onComplete(error)
+            }
+        }
+
+        PixelKit.fire(NetworkProtectionPixelEvent.networkProtectionTestPixel, frequency: .standard, withHeaders: [:])
     }
 }
 
