14429918500186ec8283c3abbcdc80170a5d8933
Tom Strba <57389842+tomasstrba@users.noreply.github.com>
2023-10-11 15:58:30 +0200

    BSK hotfix 80.3.0-1 merged to the main branch (#1736)
    Task/Issue URL: https://app.asana.com/0/0/1205641398750303/f
    
    **Description**:
    See https://github.com/duckduckgo/BrowserServicesKit/pull/528

--- LocalPackages/NetworkProtectionMac/Package.swift
+++ LocalPackages/NetworkProtectionMac/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .library(name: "NetworkProtectionUI", targets: ["NetworkProtectionUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "81.1.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "81.1.1"),
         .package(path: "../XPC"),
         .package(path: "../SwiftUIExtensions")
     ],
