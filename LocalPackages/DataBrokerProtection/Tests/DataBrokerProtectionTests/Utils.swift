b4554b8bf1b8fea3cb081d1bce3ef81cf28b123e
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2023-10-17 10:29:35 -0300

    Add Initial Scan and Maintenance endpoints for UI communication (#1718)

--- LocalPackages/DataBrokerProtection/Tests/DataBrokerProtectionTests/Utils.swift
+++ LocalPackages/DataBrokerProtection/Tests/DataBrokerProtectionTests/Utils.swift
@@ -37,6 +37,25 @@ func areDatesEqualIgnoringSeconds(date1: Date?, date2: Date?) -> Bool {
     return normalizedDate1 == normalizedDate2
 }
 
+func areDatesEqualsOnDayMonthAndYear(date1: Date?, date2: Date?) -> Bool {
+    if date1 == date2 {
+        return true
+    }
+    guard let date1 = date1, let date2 = date2 else {
+        return false
+    }
+    let calendar = Calendar.current
+    let components: Set<Calendar.Component> = [.year, .month, .day]
+
+    let date1Components = calendar.dateComponents(components, from: date1)
+    let date2Components = calendar.dateComponents(components, from: date2)
+
+    let normalizedDate1 = calendar.date(from: date1Components)
+    let normalizedDate2 = calendar.date(from: date2Components)
+
+    return normalizedDate1 == normalizedDate2
+}
+
 extension HTTPURLResponse {
     static let ok = HTTPURLResponse(url: URL(string: "www.example.com")!, statusCode: 200, httpVersion: nil, headerFields: [String: String]())!
 }
