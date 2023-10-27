c54f4ac55e4f682c4c01b6602c3bb1c7c50dab19
Dax Mobile <44842493+daxmobile@users.noreply.github.com>
2023-10-27 23:43:11 +1100

    Update BSK with autofill 9.0.0 (#1763)
    Task/Issue URL:
    https://app.asana.com/0/1205747234047385/1205747234047385
    Autofill Release:
    https://github.com/duckduckgo/duckduckgo-autofill/releases/tag/9.0.0
    BSK PR: https://github.com/duckduckgo/BrowserServicesKit/pull/537
    
    ## Description
    Updates Autofill to version
    [9.0.0](https://github.com/duckduckgo/duckduckgo-autofill/releases/tag/9.0.0).

--- LocalPackages/Account/Package.swift
+++ LocalPackages/Account/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Account"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.0.1"),
         .package(path: "../Purchase")
     ],
     targets: [
