b07a1fc5c32f86a36db87b52f6f2983fceb72e85
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2023-10-31 17:18:22 +0100

    Point to 82.1.0

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", revision: "f2a5d102da34842b3ef02c876a1a539648bd5930"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.1.0"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
