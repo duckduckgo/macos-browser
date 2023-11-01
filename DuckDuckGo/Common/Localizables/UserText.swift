99ab869cf4f5a7bb0d217d65b62a5c89f2b23544
Alexey Martemyanov <mallexxx@gmail.com>
2023-11-01 19:14:41 +0600

    update final copy after Ship Review

--- DuckDuckGo/Common/Localizables/UserText.swift
+++ DuckDuckGo/Common/Localizables/UserText.swift
@@ -554,7 +554,11 @@ struct UserText {
     static let onboardingSetDefaultButton = NSLocalizedString("onboarding.setdefault.button", value: "Let's Do It!", comment: "Launch the set default UI")
     static let onboardingNotNowButton = NSLocalizedString("onboarding.notnow.button", value: "Maybe Later", comment: "Skip a step of the onboarding flow")
 
-    static let importFromChromiumMoreInfo = NSLocalizedString("import.from.chromium.info", value: "You will be asked for your computer password.\n\nDuckDuckGo will not see your password.\n\nImported passwords are stored securely using encryption.", comment: "More info when importing from Chromium")
+    static let importFromChromiumMoreInfo = NSLocalizedString("import.from.chromium.info", value: """
+    If your computer prompts you to enter a password prior to import, DuckDuckGo will not see that password.
+
+    Imported passwords are stored securely using encryption.
+    """, comment: "More info when importing from Chromium")
 
     static let importFromFirefoxMoreInfo = NSLocalizedString("import.from.firefox.info", value: "You'll be asked to enter your Primary Password for Firefox.\n\nImported passwords are encrypted and only stored on this computer.", comment: "More info when importing from Firefox")
 
