805108899d0354532dc0ec29edf620594fc3b885
Sabrina Tardio <stardio@duckduckgo.com>
2023-11-06 13:56:25 +0100

    fix unwanted changes

--- DuckDuckGo/Common/Extensions/NSAlertExtension.swift
+++ DuckDuckGo/Common/Extensions/NSAlertExtension.swift
@@ -214,7 +214,7 @@ extension NSAlert {
     static func syncCredentialsPaused() -> NSAlert {
         let alert = NSAlert()
         alert.messageText = UserText.syncCredentialsPausedAlertTitle
-        alert.informativeText = UserText.syncBookmarkPausedAlertDescription
+        alert.informativeText = UserText.syncCredentialsPausedAlertDescription
         alert.alertStyle = .warning
         alert.addButton(withTitle: UserText.ok)
         alert.addButton(withTitle: UserText.learnMore)
