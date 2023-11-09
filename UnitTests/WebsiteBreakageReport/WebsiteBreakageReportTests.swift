018ca74e87104d9be3d49be10b7afc56f2460fc1
Shane Osbourne <sosbourne@duckduckgo.com>
2023-11-09 12:55:21 +0000

    switch to true|false for protectionsState param

--- UnitTests/WebsiteBreakageReport/WebsiteBreakageReportTests.swift
+++ UnitTests/WebsiteBreakageReport/WebsiteBreakageReportTests.swift
@@ -58,7 +58,7 @@ class WebsiteBreakageReportTests: XCTestCase {
         XCTAssertEqual(queryItems[valueFor: "tds"], "abc123")
         XCTAssertEqual(queryItems[valueFor: "blockedTrackers"], "bad.tracker.test,tracking.test")
         XCTAssertEqual(queryItems[valueFor: "surrogates"], "surrogate.domain.test")
-        XCTAssertEqual(queryItems[valueFor: "protectionsState"], "1")
+        XCTAssertEqual(queryItems[valueFor: "protectionsState"], "true")
     }
 
     func testThatNativeAppSpecificFieldsAreReported() throws {
@@ -98,7 +98,7 @@ class WebsiteBreakageReportTests: XCTestCase {
         XCTAssertEqual(queryItems[valueFor: "tds"], "abc123")
         XCTAssertEqual(queryItems[valueFor: "blockedTrackers"], "bad.tracker.test,tracking.test")
         XCTAssertEqual(queryItems[valueFor: "surrogates"], "surrogate.domain.test")
-        XCTAssertEqual(queryItems[valueFor: "protectionsState"], "1")
+        XCTAssertEqual(queryItems[valueFor: "protectionsState"], "true")
         XCTAssertEqual(queryItems[valueFor: "manufacturer"], "IBM")
         XCTAssertEqual(queryItems[valueFor: "os"], "12")
         XCTAssertEqual(queryItems[valueFor: "gpc"], "true")
