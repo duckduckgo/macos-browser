ea8f4436376112aeac59b57ae4c8440dd5bb43f2
Sam Symons <sam@samsymons.com>
2023-11-08 19:35:31 -0800

    Change BSK to 82.2.2 after the 82.2.1-1 hotfix.
    The main branch accepted the hotfix BSK version without conflict for some reason, so I’m fixing it by hand.

--- LocalPackages/Account/Package.swift
+++ LocalPackages/Account/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Account"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.2.1-1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.2.2"),
         .package(path: "../Purchase")
     ],
     targets: [
--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.2.1-1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "82.2.2"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
