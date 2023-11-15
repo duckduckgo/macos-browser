d339f8c0f3fd99ec94f64e41e3ba433ae38625e4
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-11-15 10:48:59 +0100

    Updates BSK to 83.0.0-1

--- LocalPackages/Account/Package.swift
+++ LocalPackages/Account/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Account"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "83.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "83.0.0-1"),
         .package(path: "../Purchase")
     ],
     targets: [
