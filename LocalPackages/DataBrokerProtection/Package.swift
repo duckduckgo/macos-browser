70fbdec1e0a90d2ab1e3a0ab89057c2fa1252509
amddg44 <amallon@duckduckgo.com>
2023-11-16 17:37:14 +0100

    Autofill "Never Save for this Site" support for iOS (#1827)
    Task/Issue URL: https://app.asana.com/0/0/1205783642285427/f
    Tech Design URL:
    CC:
    
    Description:
    BSK update to support "Never Save for this Site" on iOS including support for getRuntimeConfiguration

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "84.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "84.1.0"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
