f5fd7b1b9e3a2d3f0533d484bd7b7427c51e1a23
Sabrina Tardio <44158575+SabrinaTardio@users.noreply.github.com>
2023-11-17 15:43:52 +0100

    new tab experiment (#1856)
    Task/Issue URL:
    https://app.asana.com/0/1177771139624306/1205944325086210/f
    
    **Description**: Create a pixel experiment that tests current new tab
    cards vs no card + set default prompt.

--- DuckDuckGo/HomePage/Model/HomePageContinueSetUpModel.swift
+++ DuckDuckGo/HomePage/Model/HomePageContinueSetUpModel.swift
@@ -364,34 +364,40 @@ extension HomePage.Models {
         }
 
         private var shouldMakeDefaultCardBeVisible: Bool {
+            !PixelExperiment.isNoCardsExperimentOn &&
             shouldShowMakeDefaultSetting &&
             !defaultBrowserProvider.isDefault
         }
 
         private var shouldImportCardBeVisible: Bool {
+            !PixelExperiment.isNoCardsExperimentOn &&
             shouldShowImportSetting &&
             !dataImportProvider.didImport
         }
 
         private var shouldDuckPlayerCardBeVisible: Bool {
+            !PixelExperiment.isNoCardsExperimentOn &&
             shouldShowDuckPlayerSetting &&
             duckPlayerPreferences.duckPlayerModeBool == nil &&
             !duckPlayerPreferences.youtubeOverlayAnyButtonPressed
         }
 
         private var shouldEmailProtectionCardBeVisible: Bool {
+            !PixelExperiment.isNoCardsExperimentOn &&
             shouldShowEmailProtectionSetting &&
             !emailManager.isSignedIn
         }
 
         private var shouldCookieCardBeVisible: Bool {
+            !PixelExperiment.isNoCardsExperimentOn &&
             shouldShowCookieSetting &&
             privacyPreferences.autoconsentEnabled != true
         }
 
         private var shouldSurveyDay0BeVisible: Bool {
             let oneDayAgo = Calendar.current.date(byAdding: .weekday, value: -1, to: Date())!
-            return isDay0SurveyEnabled &&
+            return !PixelExperiment.isNoCardsExperimentOn &&
+            isDay0SurveyEnabled &&
             shouldShowSurveyDay0 &&
             !userInteractedWithSurveyDay0 &&
             firstLaunchDate > oneDayAgo
@@ -399,7 +405,8 @@ extension HomePage.Models {
 
         private var shouldSurveyDay7BeVisible: Bool {
             let oneWeekAgo = Calendar.current.date(byAdding: .weekOfYear, value: -1, to: Date())!
-            return isDay7SurveyEnabled &&
+            return !PixelExperiment.isNoCardsExperimentOn &&
+            isDay7SurveyEnabled &&
             shouldShowSurveyDay0 &&
             shouldShowSurveyDay7 &&
             !userInteractedWithSurveyDay0 &&
