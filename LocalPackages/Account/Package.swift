ae38cab0af70b7560d504d339bafcd06918c2431
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-11-23 15:25:06 +0100

    Fix migration to form-factor-specific favorites (#1884)
    Task/Issue URL: https://app.asana.com/0/414709148257752/1206016824928730/f
    
    Description:
    Fix Core Data bug in migrating one-to-many to many-to-many relationship by retrieving favorites
    order manually from Bookmarks v3 model and applying that order during migration
    to form-factor-specific favorites.

--- LocalPackages/Account/Package.swift
+++ LocalPackages/Account/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Account"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "83.0.0-2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "83.0.0-3"),
         .package(path: "../Purchase")
     ],
     targets: [
