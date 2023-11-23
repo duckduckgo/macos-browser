bec2d9a4041c45da91211844fa111b98a0ae9993
Federico Cappelli <fcappelli@duckduckgo.com>
2023-11-23 14:02:45 +0000

    Revert "Breakage report improvements" (#1885)
    Reverts duckduckgo/macos-browser#1864

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "85.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "84.2.0"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
