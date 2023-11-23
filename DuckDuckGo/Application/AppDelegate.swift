057e68e5321cc0aa7f380d88499e56d88c3364d8
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-11-23 14:26:46 +0100

    Fix migration of favorites

--- DuckDuckGo/Application/AppDelegate.swift
+++ DuckDuckGo/Application/AppDelegate.swift
@@ -19,6 +19,7 @@
 import Cocoa
 import Combine
 import Common
+import CoreData
 import BrowserServicesKit
 import Persistence
 import Configuration
@@ -112,6 +113,24 @@ final class AppDelegate: NSObject, NSApplicationDelegate, FileDownloadManagerDel
                 Thread.sleep(forTimeInterval: 1)
                 fatalError("Could not load DB: \(error.localizedDescription)")
             }
+
+            let preMigrationErrorHandling = EventMapping<BookmarkFormFactorFavoritesMigration.MigrationErrors> { _, error, _, _ in
+                if let error = error {
+                    Pixel.fire(.debug(event: .bookmarksCouldNotLoadDatabase, error: error))
+                } else {
+                    Pixel.fire(.debug(event: .bookmarksCouldNotLoadDatabase))
+                }
+
+                Thread.sleep(forTimeInterval: 1)
+                fatalError("Could not create Bookmarks database stack: \(error?.localizedDescription ?? "err")")
+            }
+
+            BookmarkDatabase.shared.preFormFactorSpecificFavoritesFolderOrder = BookmarkFormFactorFavoritesMigration
+                .getFavoritesOrderFromPreV4Model(
+                    dbContainerLocation: BookmarkDatabase.defaultDBLocation,
+                    dbFileURL: BookmarkDatabase.defaultDBFileURL,
+                    errorEvents: preMigrationErrorHandling
+                )
 
             BookmarkDatabase.shared.db.loadStore { context, error in
                 guard let context = context else {
