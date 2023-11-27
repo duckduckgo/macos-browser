970c406dc59f58ecebba1cd8c2a4b6cd39b2a368
Graeme Arthur <graeme@graemearthur.com>
2023-11-27 14:46:06 +0100

    Fix bug where VPN location isn't respected (#1887)
    Task/Issue URL: https://app.asana.com/0/0/1206025077480506/f
    BSK: https://github.com/duckduckgo/BrowserServicesKit/pull/576
    
    Description:
    There was a bug with location switching where the user’s choice wasn’t
    always being respected. I noticed that there was a place where we
    weren’t passing it. I also noticed that we’re sometimes not using the
    selected environment because of the default function arguments, so I
    also updated them as it’s a somewhat related issue.
    
    Steps to test this PR:
    - Just make sure NetP / env selection still works
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- LocalPackages/Account/Package.swift
+++ LocalPackages/Account/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Account"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "85.0.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "85.1.0"),
         .package(path: "../Purchase")
     ],
     targets: [
