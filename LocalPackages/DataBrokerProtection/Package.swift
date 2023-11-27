f51964cc57e60b5ef5e99cd65c4a9a3a900c1ff8
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-11-27 18:52:07 +0100

    Update BSK to 83.0.0-4

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "83.0.0-3"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "83.0.0-4"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
