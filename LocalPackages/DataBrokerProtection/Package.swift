239f4781c76696d56b07cf306f8ae1ffa0e81fcd
Anh Do <ado@duckduckgo.com>
2023-11-28 22:32:03 -0500

    Update BSK

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "86.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "86.1.0"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
