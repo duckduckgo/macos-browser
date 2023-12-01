fe1ad8df38038660f0410924cd5a8dcc102ab6d2
Thom Espach <thomespach@me.com>
2023-12-01 09:59:36 +0000

    Fix Address Bar Bug - Incorrectly Updated (#1911)
    Task/Issue URL:
    https://app.asana.com/0/1177771139624306/1206063910171914/f
    
    **Description**: 
    This PR fixes two issues:
    1. The address bar appeared slow to the user when entering text directly
    into it since we waited until content is finished loading before
    updating the address bar.
    2. The address bar is _not_ updated for every navigation that it should
    be, including on GitHub.com
    
    The solution:
    1. Always update address bar strings as soon as the user enters text -
    there is no risk of address bar spoofing when it's user entered text so
    not a security concern.
    2. Subscribe to tab.$content -> tab.$loadingProgress to ensure address
    bar strings are securely updated (once content has actually started
    loading) instead of as soon as the URL content is received or after
    isLoading completes. This fires for all tested navigation events.
    
    **Steps to test this PR**:
    1. Ensure address bar isn't spoofed here:
    https://privacy-test-pages.site/security/address-bar-spoofing/spoof-js-page-rewrite.html
    2. Click on [this
    link](https://github.com/duckduckgo/BrowserServicesKit/pull/575) and
    ensure the address bar is updated properly
    3. Go to https://jsfiddle.net/
    4. Make an edit and click save
    5. Check the address bar string is updated to the unique sharable URL
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- DuckDuckGo/Tab/ViewModel/TabViewModel.swift
+++ DuckDuckGo/Tab/ViewModel/TabViewModel.swift
@@ -68,6 +68,7 @@ final class TabViewModel {
     @Published private(set) var addressBarString: String = ""
     @Published private(set) var passiveAddressBarString: String = ""
     var lastAddressBarTextFieldValue: AddressBarTextField.Value?
+    private(set) var addressBarHasUpdated: Bool = false
 
     @Published private(set) var title: String = UserText.tabHomeTitle
     @Published private(set) var favicon: NSImage?
@@ -105,17 +106,26 @@ final class TabViewModel {
     }
 
     private func subscribeToUrl() {
-        tab.$isLoading
-            .dropFirst()
+        tab.$content
             .receive(on: DispatchQueue.main)
-            .sink { [weak self] isLoading in
-                self?.isLoading = isLoading
-                if !isLoading {
-                    // Update the address bar only after the tab has finished
-                    // to prevent Address Bar Spoofing
+            .sink { [weak self] content in
+                self?.waitToUpdateAddressBar(content)
+            }
+            .store(in: &cancellables)
+    }
+
+    private func waitToUpdateAddressBar(_ content: Published<Tab.TabContent>.Publisher.Output) {
+        self.addressBarHasUpdated = false
+        tab.$loadingProgress
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] progress in
+                // Update the address bar only after the tab has reached 10% loading
+                // to prevent Address Bar Spoofing
+                if progress > 0.1 && !(self?.addressBarHasUpdated ?? true) {
                     self?.updateAddressBarStrings()
                     self?.updateCanBeBookmarked()
                     self?.updateFavicon()
+                    self?.addressBarHasUpdated = true
                 }
             }
             .store(in: &cancellables)
