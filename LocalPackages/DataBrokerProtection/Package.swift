11cb5bcd4a68bcaeea4f211e588a45d8d188c2ce
Sam Symons <sam@samsymons.com>
2023-12-03 22:12:21 -0800

    Set BSK version to 88.0.0.

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "87.3.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "88.0.0"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
