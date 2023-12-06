4467da14f519efd4f10cbb622a093824bfcfc522
Federico Cappelli <fcappelli@duckduckgo.com>
2023-12-06 11:36:27 +0000

    BSK 91.0.1

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "91.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "91.0.1"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper")
