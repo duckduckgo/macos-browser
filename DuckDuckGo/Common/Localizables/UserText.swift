457842e14e1b9ce93d6086237538f714402cf449
Emanuele Feliziani <feliziani.emanuele@gmail.com>
2023-12-06 13:40:00 +0100

    Copy tweak
    Signed-off-by: Emanuele Feliziani <feliziani.emanuele@gmail.com>

--- DuckDuckGo/Common/Localizables/UserText.swift
+++ DuckDuckGo/Common/Localizables/UserText.swift
@@ -374,7 +374,7 @@ struct UserText {
     static let autofillAutoLock = NSLocalizedString("autofill.auto-lock", value: "Auto-lock", comment: "Autofill settings section title")
     static let autofillLockWhenIdle = NSLocalizedString("autofill.lock-when-idle", value: "Lock Autofill after computer is idle for", comment: "Autofill auto-lock setting")
     static let autofillNeverLock = NSLocalizedString("autofill.never-lock", value: "Never lock Autofill", comment: "Autofill auto-lock setting")
-    static let autofillNeverLockWarning = NSLocalizedString("autofill.never-lock-warning", value: "Anyone with access to your device will be able to use and modify your Autofill data if not locked. For your safety, filling in Credit Cards is always locked.", comment: "Autofill disabled auto-lock warning")
+    static let autofillNeverLockWarning = NSLocalizedString("autofill.never-lock-warning", value: "If not locked, anyone with access to your device will be able to use and modify your Autofill data. For your safety, filling in Credit Cards is always locked.", comment: "Autofill disabled auto-lock warning")
     static let autolockLocksFormFill = NSLocalizedString("autofill.autolock-locks-form-filling", value: "Also lock filling in login forms.", comment: "Lock form filling when auto-lock is active text")
 
     
