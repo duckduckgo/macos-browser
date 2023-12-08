f4b7bee331c59f5f670fae1293fc470d420b4786
Anh Do <ado@duckduckgo.com>
2023-12-08 17:10:30 -0500

    Update BSK

--- LocalPackages/NetworkProtectionMac/Package.swift
+++ LocalPackages/NetworkProtectionMac/Package.swift
@@ -30,7 +30,7 @@ let package = Package(
         .library(name: "NetworkProtectionUI", targets: ["NetworkProtectionUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "92.0.4"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "92.1.0"),
         .package(path: "../XPCHelper"),
         .package(path: "../SwiftUIExtensions")
     ],
