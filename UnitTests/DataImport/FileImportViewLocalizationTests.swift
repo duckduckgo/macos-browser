e005c99334a8cc53b2635749b9fd307d39c4481f
Alexey Martemyanov <mallexxx@gmail.com>
2023-12-13 19:03:44 +0600

    FileImportView instructions localization tests

--- UnitTests/DataImport/FileImportViewLocalizationTests.swift
+++ UnitTests/DataImport/FileImportViewLocalizationTests.swift
@@ -21,32 +21,84 @@ import XCTest
 import SwiftUI
 @testable import DuckDuckGo_Privacy_Browser
 
+@available(macOS 13.0, *)
 class YourLocalizationTests: XCTestCase {
 
     override func tearDown() {
         Bundle.resetSwizzling()
+        customAssert = nil
+        customAssertionFailure = nil
     }
 
-    func testLocalizedStringForEnglish() {
-        var referenceValues = [DataImport.Source: [DataImport.DataType: [InstructionsView.InstructionsItem]]]()
+    func testLocalizedStringForEnglish() throws {
+        // collect reference "Base" localization escaped format arguments
+        var referenceValues = [DataImport.Source: [DataImport.DataType: [String]]]()
         for locale in [.base] + Bundle.main.availableLocalizations().filter({ $0 != .base }) {
+            // swizzle "locale".lproj bundle
             setLocale(locale)
 
             for source in DataImport.Source.allCases {
                 for dataType in source.supportedDataTypes {
-                    let e = expectation(description: "button factory called")
+                    let e = expectation(description: "Button item should not be missing")
+                    // build instructions
                     let items = fileImportInstructionsBuilder(source: source, dataType: dataType) { title in
+                        // button title should not be empty
                         XCTAssertFalse(title.isEmpty)
+                        // button should be present in items
                         e.fulfill()
                         return AnyView(EmptyView())
                     }
                     waitForExpectations(timeout: 0)
 
-                    if locale == .base {
-                        referenceValues[source, default: [:]][dataType, default: []] = items
-                    } else {
-                        XCTAssertEqual(Set(referenceValues[source]![dataType]!), Set(items), "\(locale).\(source.rawValue).\(dataType.rawValue)")
+                    // find first .string (format) item
+                    let formatItem = items.first
+                    guard case .string(let format) = formatItem else {
+                        XCTFail("First item should be Format String – \(source.rawValue).\(dataType.rawValue)")
+                        continue
                     }
+
+                    // find all %-escaped sequences in the format
+                    let formatItemsRegex = /%(?:\d+\$)?(?:lld|ld|d|s|@)/
+                    let formatArgs = format.matches(of: formatItemsRegex).enumerated().map { (idx, match) in
+                        var value = String(match.output)
+                        if !value.contains("\(idx + 1)") {
+                            // if escape sequence has no index - insert it (convert %d to %4$d)
+                            value.insert(contentsOf: "\(idx + 1)$", at: value.index(after: value.startIndex))
+                        }
+                        return value
+                    }
+
+                    switch locale {
+                    case .base:
+                        // write Base reference value
+                        referenceValues[source, default: [:]][dataType] = formatArgs
+                    default:
+                        // format args should match but their positions may differ
+                        XCTAssertEqual(referenceValues[source]![dataType]!.sorted(), formatArgs.sorted(),
+                                       "\(locale).\(source.rawValue).\(dataType.rawValue).formatArgs")
+                    }
+
+                    // number of %@ arguments should match number of builder .image and .button arguments
+                    XCTAssertEqual(referenceValues[source]![dataType]!.filter { $0.hasSuffix("@") }.count,
+                                   items.filter { if case .image = $0 { true } else if case .view = $0 { true } else { false } }.count,
+                                   "\(locale).\(source.rawValue).\(dataType.rawValue).imagesAndButtons")
+
+                    // number of %s arguments should match number of builder .string arguments
+                    XCTAssertEqual(referenceValues[source]![dataType]!.filter { $0.hasSuffix("s") }.count,
+                                   items[1.../* 0 == format */].filter { if case .string = $0 { true } else { false } }.count,
+                                   "\(locale).\(source.rawValue).\(dataType.rawValue).strings")
+
+#if CI
+                    customAssert = { _, _, _, _ in
+                        XCTFail("\(locale).\(source.rawValue).\(dataType.rawValue).InstructionsView.assert")
+                    }
+                    customAssertionFailure = { _, _, _ in
+                        XCTFail("\(locale).\(source.rawValue).\(dataType.rawValue).InstructionsView.assertionFailure")
+                    }
+#endif
+                    _=InstructionsView {
+                        items
+                    } // should not assert
                 }
             }
         }
@@ -72,7 +124,7 @@ extension InstructionsView.InstructionsItem: Hashable, CustomStringConvertible {
 
     public static func == (lhs: DuckDuckGo_Privacy_Browser.InstructionsView.InstructionsItem, rhs: DuckDuckGo_Privacy_Browser.InstructionsView.InstructionsItem) -> Bool {
         switch lhs {
-        case .string: if case .string = rhs { return true }
+        case .string(let value): if case .string(value) = rhs { return true }
         case .image(let value1): if case .image(let value2) = rhs { return value1.tiffRepresentation! == value2.tiffRepresentation }
         case .view: if case .view = rhs { return true }
         }
