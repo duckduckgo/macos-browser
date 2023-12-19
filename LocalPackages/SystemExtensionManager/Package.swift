d52d24a1dbb33141a8fed547aacd46925640addf
Sam Symons <sam@samsymons.com>
2023-12-19 02:40:07 -0800

    Prevent VPN server list persistence failures (#1985)
    Task/Issue URL: https://app.asana.com/0/0/1206201299599597/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR makes two changes:
    
    1. The WireGuard invalid state error has been given a reason field, to allow insight into what state is invalid exactly
    2. The server list store no longer prevents the file from being stored if the old one can't be removed; this appears to be a major source of unhandled errors

--- LocalPackages/SystemExtensionManager/Package.swift
+++ LocalPackages/SystemExtensionManager/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "94.0.3"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "95.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
