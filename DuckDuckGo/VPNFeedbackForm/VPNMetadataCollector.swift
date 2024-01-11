a476ca2ab3c7d88fdc864cd493ac86aafc80573c
Sam Symons <sam@samsymons.com>
2024-01-11 15:47:28 -0800

    Remove unused architectures.

--- DuckDuckGo/VPNFeedbackForm/VPNMetadataCollector.swift
+++ DuckDuckGo/VPNFeedbackForm/VPNMetadataCollector.swift
@@ -192,14 +192,6 @@ final class DefaultVPNMetadataCollector: VPNMetadataCollector {
             return "arm64"
         #elseif arch(i386)
             return "i386"
-        #elseif arch(powerpc64)
-            return "powerpc64"
-        #elseif arch(powerpc64le)
-            return "powerpc64le"
-        #elseif arch(s390x)
-            return "s390x"
-        #elseif arch(wasm32)
-            return "wasm32"
         #elseif arch(x86_64)
             return "x86_64"
         #else
