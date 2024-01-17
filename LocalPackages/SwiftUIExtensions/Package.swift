b0bc72bfa95504b0a2d09d40efceb2def287da73
Brian Hall <brian@hack.design>
2024-01-17 14:52:44 -0600

    Bump BrowserServicesKit Version (#2063)
    Task/Issue URL: https://app.asana.com/0/0/1206352789801869/f
    Tech Design URL:
    CC:
    
    **Description**:
    This is a minor fix to C-S-S that only affects Personal Information
    Removal. Also includes the new JSON files originally created in #2060.
    
    **Steps to test this PR**:
    1.
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)
    
    ---------
    
    Co-authored-by: Fernando Bunn <bunn@idevzilla.com>

--- LocalPackages/SwiftUIExtensions/Package.swift
+++ LocalPackages/SwiftUIExtensions/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "PreferencesViews", targets: ["PreferencesViews"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.3"),
     ],
     targets: [
         .target(
