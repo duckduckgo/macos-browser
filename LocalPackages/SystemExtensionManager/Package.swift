21568ffc05f75f2b76d89aa4639bd982598a82a9
Brian Hall <brian@hack.design>
2024-01-17 14:10:48 -0600

    Revert "Update BSK branch reference"
    This reverts commit 7ce75fbc786af4142a3ab796f49c2c1889da55e2.

--- LocalPackages/SystemExtensionManager/Package.swift
+++ LocalPackages/SystemExtensionManager/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "bhall/bump-content-scope-scripts")
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.3"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
