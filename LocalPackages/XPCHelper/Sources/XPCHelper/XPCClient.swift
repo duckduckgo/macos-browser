f3df1b2a5a0e40d861607ff360e97a28577af7f7
Daniel Bernal <danielbernal@hey.com>
2024-01-22 11:12:16 +0100

    Fix conflicts

--- LocalPackages/XPCHelper/Sources/XPCHelper/XPCClient.swift
+++ LocalPackages/XPCHelper/Sources/XPCHelper/XPCClient.swift
@@ -17,7 +17,6 @@
 //
 
 import Foundation
-@_implementationOnly import XPCDelegateProxy
 
 /// This actor is meant to support synchronized access to the XPC connection
 ///
@@ -30,7 +29,7 @@ private struct XPCConnectionActor {
 
 /// An XPC client
 ///
-public final class XPCClient<ClientInterface: NSObjectProtocol, ServerInterface: AnyObject> {
+public final class XPCClient<ClientInterface: AnyObject, ServerInterface: AnyObject> {
 
     public enum ConnectionError: Error {
         case noRemoteObjectProxy
@@ -63,7 +62,7 @@ public final class XPCClient<ClientInterface: NSObjectProtocol, ServerInterface:
     public weak var delegate: ClientInterface? {
         didSet {
             Task { @XPCConnectionActor in
-                connection.exportedObject = XPCDelegateProxy(delegate: delegate)
+                connection.exportedObject = delegate
             }
         }
     }
@@ -88,7 +87,7 @@ public final class XPCClient<ClientInterface: NSObjectProtocol, ServerInterface:
     private func makeConnection() -> NSXPCConnection {
         let connection = NSXPCConnection(machServiceName: machServiceName)
         connection.exportedInterface = clientInterface
-        connection.exportedObject = XPCDelegateProxy(delegate: delegate)
+        connection.exportedObject = delegate
         connection.remoteObjectInterface = serverInterface
 
         let closeConnection = { [weak self] in
