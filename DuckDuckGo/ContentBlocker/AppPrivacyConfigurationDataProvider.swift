a4800f94e107fd9922d36d9f6c930ff85c9ab85c
Dax the Duck <dax@duckduckgo.com>
2024-01-22 11:00:44 +0000

    Update embedded files

--- DuckDuckGo/ContentBlocker/AppPrivacyConfigurationDataProvider.swift
+++ DuckDuckGo/ContentBlocker/AppPrivacyConfigurationDataProvider.swift
@@ -22,8 +22,8 @@ import BrowserServicesKit
 final class AppPrivacyConfigurationDataProvider: EmbeddedDataProvider {
 
     public struct Constants {
-        public static let embeddedDataETag = "\"f554a2ba0d10bb67736bfd05e231bd46\""
-        public static let embeddedDataSHA = "eb8ef392f3bf3aec4c8784982808cf623d7fb65f606099a532647ed83347b0a5"
+        public static let embeddedDataETag = "\"ae39a312c38f90023f7424e01f1d6684\""
+        public static let embeddedDataSHA = "83149b858dc9ee3aceaa0913e5b1795cd8b89f33c9bf4c966867513b812d77cc"
     }
 
     var embeddedDataEtag: String {
