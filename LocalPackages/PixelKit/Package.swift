e93fda79cd4309fbbb373201c8cb4498c37253e1
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-01-25 17:42:39 +0100

    Updates BSK to 102.0.1

--- LocalPackages/PixelKit/Package.swift
+++ LocalPackages/PixelKit/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "102.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "102.0.1"),
     ],
     targets: [
         .target(
