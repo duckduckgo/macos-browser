26d04367cf999c6fa576402c7fa58fb1f16f4fbd
Sabrina Tardio <stardio@duckduckgo.com>
2024-01-25 12:30:07 +0100

    update BSK version

--- LocalPackages/LoginItems/Package.swift
+++ LocalPackages/LoginItems/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.2.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.2.3"),
     ],
     targets: [
         .target(
