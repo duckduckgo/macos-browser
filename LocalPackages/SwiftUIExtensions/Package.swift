c38b4f8660734ff7cbc236039818c6fa40570398
Sam Symons <sam@samsymons.com>
2024-01-25 21:52:41 -0800

    Use hotfix BSK tag.

--- LocalPackages/SwiftUIExtensions/Package.swift
+++ LocalPackages/SwiftUIExtensions/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(name: "PreferencesViews", targets: ["PreferencesViews"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.5"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.5-1"),
     ],
     targets: [
         .target(
