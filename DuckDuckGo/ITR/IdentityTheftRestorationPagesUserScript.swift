06d95fcb46c80a52f9fc3466bccb549c4d7a2e6d
Michal Smaga <miasma13@gmail.com>
2024-01-26 13:10:12 +0100

    Add first draft of the ITR user script

---
+++ DuckDuckGo/ITR/IdentityTheftRestorationPagesUserScript.swift
@@ -0,0 +1,131 @@
+//
+//  IdentityTheftRestorationPagesUserScript.swift
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import BrowserServicesKit
+import Common
+import Combine
+import Foundation
+import WebKit
+import Subscription
+import UserScript
+
+
+///
+/// The user script that will be the broker for all subscription features
+///
+public final class IdentityTheftRestorationPagesUserScript: NSObject, UserScript, UserScriptMessaging {
+    public var source: String = ""
+
+    public static let context = "identityTheftRestorationPages"
+
+    // special pages messaging cannot be isolated as we'll want regular page-scripts to be able to communicate
+    public let broker = UserScriptMessageBroker(context: IdentityTheftRestorationPagesUserScript.context, requiresRunInPageContentWorld: true )
+
+    public let messageNames: [String] = [
+        IdentityTheftRestorationPagesUserScript.context
+    ]
+
+    public let injectionTime: WKUserScriptInjectionTime = .atDocumentStart
+    public let forMainFrameOnly = true
+    public let requiresRunInPageContentWorld = true
+}
+
+extension IdentityTheftRestorationPagesUserScript: WKScriptMessageHandlerWithReply {
+    @MainActor
+    public func userContentController(_ userContentController: WKUserContentController,
+                                      didReceive message: WKScriptMessage) async -> (Any?, String?) {
+        let action = broker.messageHandlerFor(message)
+        do {
+            let json = try await broker.execute(action: action, original: message)
+            return (json, nil)
+        } catch {
+            // forward uncaught errors to the client
+            return (nil, error.localizedDescription)
+        }
+    }
+}
+
+// MARK: - Fallback for macOS 10.15
+extension IdentityTheftRestorationPagesUserScript: WKScriptMessageHandler {
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        // unsupported
+    }
+}
+
+///
+/// Use Subscription sub-feature
+///
+final class IdentityTheftRestorationPagesFeature: Subfeature {
+    var broker: UserScriptMessageBroker?
+
+    var featureName = "useIdentityTheftRestoration"
+
+    var messageOriginPolicy: MessageOriginPolicy = .only(rules: [
+        .exact(hostname: "duckduckgo.com"),
+        .exact(hostname: "abrown.duckduckgo.com")
+    ])
+
+    func with(broker: UserScriptMessageBroker) {
+        self.broker = broker
+    }
+
+    func handler(forMethodNamed methodName: String) -> Subfeature.Handler? {
+        switch methodName {
+        case "getAccessToken": return getAccessToken
+        default:
+            return nil
+        }
+    }
+
+//    struct Subscription: Encodable {
+//        let token: String
+//    }
+//
+//    /// Values that the Frontend can use to determine the current state.
+//    struct SubscriptionValues: Codable {
+//        enum CodingKeys: String, CodingKey {
+//            case token
+//        }
+//        let token: String
+//    }
+
+    func getAccessToken(params: Any, original: WKScriptMessage) async throws -> Encodable? {
+        if let accessToken = AccountManager().accessToken {
+//            return Subscription(token: accessToken)
+            return ["token" : accessToken]
+        } else {
+            return [String: String]()
+        }
+    }
+
+//    // MARK: Push actions
+//
+//    enum SubscribeActionName: String {
+//        case onPurchaseUpdate
+//    }
+//
+//    @MainActor
+//    func pushPurchaseUpdate(originalMessage: WKScriptMessage, purchaseUpdate: PurchaseUpdate) async {
+//        pushAction(method: .onPurchaseUpdate, webView: originalMessage.webView!, params: purchaseUpdate)
+//    }
+//
+//    func pushAction(method: SubscribeActionName, webView: WKWebView, params: Encodable) {
+//        let broker = UserScriptMessageBroker(context: SubscriptionPagesUserScript.context, requiresRunInPageContentWorld: true )
+//        broker.push(method: method.rawValue, params: params, for: self, into: webView)
+//    }
+}
