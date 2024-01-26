038170f9b579e6b668c711228902be9285d30518
Sam Symons <sam@samsymons.com>
2024-01-25 22:26:00 -0800

    Release Branch: VPN fixes (#2113)
    Task/Issue URL: https://app.asana.com/0/1199230911884351/1206425609275905/f
    Tech Design URL:
    CC:
    
    Description:
    
    Steps to test this PR:
    
    The changes in this PR have already been reviewed

--- LocalPackages/Subscription/Package.swift
+++ LocalPackages/Subscription/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Subscription"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.5"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.5-1"),
     ],
     targets: [
         .target(
