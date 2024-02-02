5151cc57f7565642aa12a574a7512b4d87760b44
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-02-02 14:38:23 +0100

    Revert "Remove delayed webView attaching to main window (#2116)"
    This reverts commit 43628d1f769491ac89a9c54adf69ce1014699ea1.

--- DuckDuckGo/Tab/View/BrowserTabViewController.swift
+++ DuckDuckGo/Tab/View/BrowserTabViewController.swift
@@ -345,7 +345,21 @@ final class BrowserTabViewController: NSViewController {
                 }
                 return old == new
             })
-            .asVoid()
+            .map { [weak tabViewModel] tabContent -> AnyPublisher<Void, Never> in
+                guard let tabViewModel, tabContent.isUrl else {
+                    return Just(()).eraseToAnyPublisher()
+                }
+
+                return Publishers.Merge4(
+                    tabViewModel.tab.webViewDidReceiveRedirectPublisher,
+                    tabViewModel.tab.webViewDidCommitNavigationPublisher,
+                    tabViewModel.tab.webViewDidFailNavigationPublisher,
+                    tabViewModel.tab.webViewDidReceiveUserInteractiveChallengePublisher
+                )
+                .prefix(1)
+                .eraseToAnyPublisher()
+            }
+            .switchToLatest()
             .receive(on: DispatchQueue.main)
             .sink { [weak self, weak tabViewModel] in
                 guard let tabViewModel else { return }
