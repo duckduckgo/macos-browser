b7a9dcfb66b17baf00153a84f1291a4d98dbdd2c
Sabrina Tardio <stardio@duckduckgo.com>
2024-02-07 13:54:07 +0100

    extract strings from crash report storyboard

--- DuckDuckGo/Common/Localizables/UserText.swift
+++ DuckDuckGo/Common/Localizables/UserText.swift
@@ -1061,6 +1061,13 @@ struct UserText {
     static let fireproofingRemoveFireproofing = NSLocalizedString("fireproofing.remove-button", value: "Remove Fireproofing", comment: "Button to allow the user the remove the fireproofing from a website")
     static let fireproofingDescription = NSLocalizedString("fireproofing.description", value: "Websites rely on cookies to keep you signed in. When you fireproof a site, cookies won’t be erased, and you’ll stay signed in, even after using the Fire button. We still block third-party trackers found on Fireproof websites.", comment: "Description of dialog when the user can decide to fireproof or not a website")
 
+    // MARK: Crash Report
+    static let crashReportTitle = NSLocalizedString("crash-report.title", value: "DuckDuckGo Privacy Browser quit unexpectedly.", comment: "Title of the dialog where the user can send a crash report")
+    static let crashReportDescription = NSLocalizedString("crash-report.description", value: "Click \"Send to DuckDuckGo\" to submit report to DuckDuckGo. Crash reports help DuckDuckGo diagnose issues and improve our products. No personal information is sent with this report.", comment: "Description of the dialog where the user can send a crash report (please leave escaping caracters)")
+    static let crashReportTextFieldTitle = NSLocalizedString("crash-report.textfield.title", value: "Problem Details", comment: "Title of the text field where the problems that caused the crashed are detailed")
+    static let crashReportSendButton = NSLocalizedString("crash-report.send-button", value: "Send to DuckDuckGo", comment: "Button the user can press to send the crash report to DuckDuckGo")
+    static let crashReportDontSendButton = NSLocalizedString("crash-report.dont-send-button", value: "Don’t Send", comment: "Button the user can press to not send the crash report")
+
 
 #if SUBSCRIPTION
     static let subscriptionOptionsMenuItem = NSLocalizedString("subscription.menu.item", value: "Privacy Pro", comment: "Title for Subscription item in the options menu")
