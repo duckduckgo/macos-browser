7440b1b58f6dc2cf5e48c8229d9fd03c475de0d1
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-02-07 11:41:47 +0100

    Preparing the base app for tvOS support

---
+++ DuckDuckGoVPN_tvOS/DuckDuckGoVPN_tvOS/LocalPackages/VPNtvOS/Package.swift
@@ -0,0 +1,61 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+//
+//  Package.swift
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "VPNtvOS",
+    platforms: [
+        .tvOS(.v14)
+    ],
+    products: [
+        .library(name: "VPNtvOS", targets: ["VPNtvOS"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "104.1.1"),
+        //.package(path: "../SwiftUIExtensions")
+    ],
+    targets: [
+        // MARK: - VPNUI
+
+        .target(
+            name: "VPNtvOS",
+            dependencies: [
+                .product(name: "NetworkProtection", package: "BrowserServicesKit"),
+                //.product(name: "SwiftUIExtensions", package: "SwiftUIExtensions")
+            ],
+            resources: [
+                .copy("Resources/Assets.xcassets")
+            ],
+            swiftSettings: [
+                .define("DEBUG", .when(configuration: .debug))
+            ],
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "BrowserServicesKit")]
+        ),
+        .testTarget(
+            name: "VPNtvOSTests",
+            dependencies: [
+                "VPNtvOS",
+                .product(name: "NetworkProtectionTestUtils", package: "BrowserServicesKit")
+            ],
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "BrowserServicesKit")]
+        )
+    ]
+)
---
+++ DuckDuckGoVPN_tvOS/DuckDuckGoVPN_tvOS/LocalPackages/VPNtvOS/README.md
@@ -0,0 +1,6 @@
+# NetworkProtectionMac
+
+This package contains several components of Network Protection that are specific to macOS.
+
+- NetworkProtectionUI: UI elements used in Network Protection that are reusable across build targets.
+- NetworkProtectionIPC: code to support IPC communication between NetworkProtection targets.
