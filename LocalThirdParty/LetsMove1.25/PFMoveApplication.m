b0c9ff510484e23265b55d91974de649031d54c1
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2024-02-09 14:45:06 +0100

    Remove accidentally commited comment

--- LocalThirdParty/LetsMove1.25/PFMoveApplication.m
+++ LocalThirdParty/LetsMove1.25/PFMoveApplication.m
@@ -251,25 +251,6 @@ static BOOL IsInApplicationsFolder(NSString *path) {
 	return NO;
 }
 
-/*
-
- var isInApplicationDirectory: Bool {
- guard let appPath = resourceURL?.deletingLastPathComponent() else { return false }
- let dirPaths = NSSearchPathForDirectoriesInDomains(.applicationDirectory, .localDomainMask, true)
- var applicationIsAtCorrectPath = false
- for path in dirPaths {
-    let filePath: URL
-    if #available(macOS 13.0, *) {
-        filePath = URL(filePath: path)
-    } else {
-        filePath = URL(fileURLWithPath: path)
-    }
-    applicationIsAtCorrectPath = appPath.absoluteString.hasPrefix(filePath.absoluteString)
- }
- return applicationIsAtCorrectPath
-}
- */
-
 static BOOL IsInDownloadsFolder(NSString *path) {
 	NSArray *downloadDirs = NSSearchPathForDirectoriesInDomains(NSDownloadsDirectory, NSAllDomainsMask, YES);
 	for (NSString *downloadsDirPath in downloadDirs) {
