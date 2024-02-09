b61de4a5a4a8c3e76a9d28681fa5272b27e228a6
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2024-02-09 13:59:35 +0100

    Remove incorrect comment

--- LocalThirdParty/LetsMove1.25/PFMoveApplication.m
+++ LocalThirdParty/LetsMove1.25/PFMoveApplication.m
@@ -239,10 +239,6 @@ BOOL PFMoveIsInProgress(void) {
 #pragma mark Helper Functions
 
 static NSString *PreferredInstallLocation(void) {
-	// Return the preferred install location.
-	// Assume that if the user has a ~/Applications folder, they'd prefer their
-	// applications to go there.
-
     return [[NSSearchPathForDirectoriesInDomains(NSApplicationDirectory, NSLocalDomainMask, YES) lastObject] stringByResolvingSymlinksInPath];
 }
 
