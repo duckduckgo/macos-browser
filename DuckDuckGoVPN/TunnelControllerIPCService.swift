d0ff6db204094d7de478aa83d446c8a33e2f17c1
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-02-15 21:25:28 +0100

    Removes an unnecessary line of code

--- DuckDuckGoVPN/TunnelControllerIPCService.swift
+++ DuckDuckGoVPN/TunnelControllerIPCService.swift
@@ -112,8 +112,6 @@ extension TunnelControllerIPCService: IPCServerInterface {
     }
 
     func debugCommand(_ command: DebugCommand) async throws {
-        _ = try await ConnectionSessionUtilities.activeSession(networkExtensionBundleID: Bundle.tunnelExtensionBundleID)
-
         switch command {
         case .removeSystemExtension:
 #if NETP_SYSTEM_EXTENSION
