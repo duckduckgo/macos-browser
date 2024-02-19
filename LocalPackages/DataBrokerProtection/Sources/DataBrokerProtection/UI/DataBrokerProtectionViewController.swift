97acc0ab44092ae83d744c7bd252904d26741f02
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2024-02-19 10:52:19 -0300

    Add initial loading indicator when loading web UI

--- LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/UI/DataBrokerProtectionViewController.swift
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/UI/DataBrokerProtectionViewController.swift
@@ -26,11 +26,16 @@ final public class DataBrokerProtectionViewController: NSViewController {
     private let dataManager: DataBrokerProtectionDataManaging
     private let scheduler: DataBrokerProtectionScheduler
     private var webView: WKWebView?
+    private var loader: NSProgressIndicator!
     private let webUISettings: DataBrokerProtectionWebUIURLSettingsRepresentable
     private let webUIViewModel: DBPUIViewModel
 
     private let openURLHandler: (URL?) -> Void
     private var reloadObserver: NSObjectProtocol?
+
+    private enum Consts {
+        static let loaderDimension: CGFloat = 50.0
+    }
 
     public init(scheduler: DataBrokerProtectionScheduler,
                 dataManager: DataBrokerProtectionDataManaging,
@@ -63,9 +68,10 @@ final public class DataBrokerProtectionViewController: NSViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
 
+        addLoadingIndicator()
         reloadObserver = NotificationCenter.default.addObserver(forName: DataBrokerProtectionNotifications.shouldReloadUI,
-                                               object: nil,
-                                               queue: .main) { [weak self] _ in
+                                                                object: nil,
+                                                                queue: .main) { [weak self] _ in
             self?.webView?.reload()
         }
     }
@@ -75,14 +81,38 @@ final public class DataBrokerProtectionViewController: NSViewController {
 
         webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 1024, height: 768), configuration: configuration)
         webView?.uiDelegate = self
+        webView?.navigationDelegate = self
         view = webView!
 
         if let url = URL(string: webUISettings.selectedURL) {
             webView?.load(url)
         } else {
+            removeLoadingIndicator()
             assertionFailure("Selected URL is not valid \(webUISettings.selectedURL)")
         }
 
+    }
+
+    private func addLoadingIndicator() {
+        loader = NSProgressIndicator()
+        loader.wantsLayer = true
+        loader.style = .spinning
+        loader.controlSize = .regular
+        loader.sizeToFit()
+        loader.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loader)
+
+        NSLayoutConstraint.activate([
+            loader.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loader.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loader.widthAnchor.constraint(equalToConstant: Consts.loaderDimension),
+            loader.heightAnchor.constraint(equalToConstant: Consts.loaderDimension)
+        ])
+    }
+
+    private func removeLoadingIndicator() {
+        loader.stopAnimation(nil)
+        loader.removeFromSuperview()
     }
 
     deinit {
@@ -96,5 +126,16 @@ extension DataBrokerProtectionViewController: WKUIDelegate {
     public func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
         openURLHandler(navigationAction.request.url)
         return nil
+    }
+}
+
+extension DataBrokerProtectionViewController: WKNavigationDelegate {
+
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        loader.startAnimation(nil)
+    }
+
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        removeLoadingIndicator()
     }
 }
