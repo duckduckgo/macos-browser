258cdc11524563a035a8a0bef2695fa7ec2e63e3
Fernando Bunn <bunn@idevzilla.com>
2024-02-22 18:07:43 +0000

    Update BSK

--- LocalPackages/LoginItems/Package.swift
+++ LocalPackages/LoginItems/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "109.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "109.0.2"),
     ],
     targets: [
         .target(
