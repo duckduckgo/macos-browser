8cbf3a419cc647665ea953584830200a478f4609
Alessandro Boron <alessandro.boron@gmail.com>
2024-02-23 12:00:07 +1100

    Bookmarks folder add/edit logic (#2221)
    Task/Issue URL:
    https://app.asana.com/0/72649045549333/1206590790808721/f
    
    **Description**:
    1. Add logic and test for `AddEditBookmarkFolderViewModel`.
    2. Make a common view to re-use by `AddBookmarkPopoverView` &
    `AddEditBookmarkDialogView`
    3. Enhance `BookmarkManager` & `BookmarkStore` to update Folder title
    and location in one save.

---
+++ DuckDuckGo/Bookmarks/ViewModel/AddEditBookmarkFolderDialogViewModel.swift
@@ -0,0 +1,179 @@
+//
+//  AddEditBookmarkFolderDialogViewModel.swift
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+protocol BookmarkFolderDialogViewModel: ObservableObject {
+    var title: String { get }
+    var folderName: String { get }
+    var folders: [FolderViewModel] { get }
+    var selectedFolder: BookmarkFolder? { get }
+
+    func cancel()
+    func addOrSave()
+}
+
+@MainActor
+final class AddEditBookmarkFolderDialogViewModel: ObservableObject {
+
+    /// The type of operation to perform on a folder
+    enum Mode {
+        /// Add a new folder. Folders can have a parent folder but not necessarily.
+        /// If the users add a folder to a folder whose parent is the root `Bookmarks` folder, then the parent folder is `nil`.
+        /// If the users add a folder to a folder whose parent is not the root `Bookmarks` folder, then the parent folder is not `nil`.
+        case add(parentFolder: BookmarkFolder? = nil)
+        /// Edit an existing folder. Existing folder can have a parent folder but not necessarily.
+        /// If the users edit a  folder whose parent is the root `Bookmarks` folder, then the parent folder is `nil`
+        /// If the users edit a folder whose parent is not the root `Bookmarks` folder, then the parent folder is not `nil`.
+        case edit(folder: BookmarkFolder, parentFolder: BookmarkFolder?)
+    }
+
+    @Published var folderName: String
+    @Published var selectedFolder: BookmarkFolder?
+
+    let folders: [FolderViewModel]
+
+    var title: String {
+        mode.title
+    }
+
+    var cancelActionTitle: String {
+        mode.cancelActionTitle
+    }
+
+    var defaultActionTitle: String {
+        mode.defaultActionTitle
+    }
+
+    let isCancelActionDisabled = false
+
+    var isDefaultActionButtonDisabled: Bool {
+        folderName.trimmingWhitespace().isEmpty
+    }
+
+    private let mode: Mode
+    private let bookmarkManager: BookmarkManager
+
+    init(mode: Mode, bookmarkManager: BookmarkManager = LocalBookmarkManager.shared) {
+        self.mode = mode
+        self.bookmarkManager = bookmarkManager
+        folderName = mode.folderName
+        folders = .init(bookmarkManager.list)
+        selectedFolder = mode.parentFolder
+    }
+
+    func cancel(dismiss: () -> Void) {
+        dismiss()
+    }
+
+    func addOrSave(dismiss: () -> Void) {
+        defer { dismiss() }
+
+        guard !folderName.isEmpty else {
+            assertionFailure("folderName is empty, button should be disabled")
+            return
+        }
+
+        let folderName = folderName.trimmingWhitespace()
+
+        switch mode {
+        case let .edit(folder, originalParent):
+            // If there are no pending changes dismiss
+            guard folder.title != folderName || selectedFolder?.id != originalParent?.id else { return }
+            // Otherwise update Folder.
+            update(folder: folder, originalParent: originalParent, newParent: selectedFolder)
+        case .add:
+            add(folderWithName: folderName, to: selectedFolder)
+        }
+    }
+
+}
+
+// MARK: - Private
+
+private extension AddEditBookmarkFolderDialogViewModel {
+
+    func update(folder: BookmarkFolder, originalParent: BookmarkFolder?, newParent: BookmarkFolder?) {
+        // If the original location of the folder changed move it to the new folder.
+        if selectedFolder?.id != originalParent?.id {
+            // Update the title anyway.
+            folder.title = folderName
+            let parentFolderType: ParentFolderType = newParent.flatMap { ParentFolderType.parent(uuid: $0.id) } ?? .root
+            bookmarkManager.update(folder: folder, andMoveToParent: parentFolderType)
+        } else if folder.title != folderName { // If only title changed just update the folder title without updating its parent.
+            folder.title = folderName
+            bookmarkManager.update(folder: folder)
+        }
+    }
+
+    func add(folderWithName name: String, to parent: BookmarkFolder?) {
+        bookmarkManager.makeFolder(for: name, parent: parent, completion: { _ in })
+    }
+
+}
+
+// MARK: - AddEditBookmarkFolderDialogViewModel.Mode
+
+private extension AddEditBookmarkFolderDialogViewModel.Mode {
+
+    var title: String {
+        switch self {
+        case .add:
+            return UserText.Bookmarks.Dialog.Title.addFolder
+        case .edit:
+            return UserText.Bookmarks.Dialog.Title.editFolder
+        }
+    }
+
+    var cancelActionTitle: String {
+        switch self {
+        case .add, .edit:
+            return UserText.cancel
+        }
+    }
+
+    var defaultActionTitle: String {
+        switch self {
+        case .add:
+            return UserText.Bookmarks.Dialog.Action.addFolder
+        case .edit:
+            return UserText.save
+        }
+    }
+
+    var folderName: String {
+        switch self {
+        case .add:
+            return ""
+        case let .edit(folder, _):
+            return folder.title
+        }
+    }
+
+    var parentFolder: BookmarkFolder? {
+        switch self {
+        case let .add(parentFolder):
+            return parentFolder
+        case let .edit(_, parentFolder):
+            return parentFolder
+        }
+    }
+
+}
