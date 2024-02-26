69496d61089cc3c7c463e9279a68e6d504ec26e3
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-02-26 13:14:33 +0100

    Update to BSK 111.0.2

--- LocalPackages/LoginItems/Package.swift
+++ LocalPackages/LoginItems/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "111.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "111.0.2"),
     ],
     targets: [
         .target(
