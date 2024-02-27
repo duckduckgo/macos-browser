406e77cbf057046a6587ceb7087653b2db441e3e
Michal Smaga <miasma13@gmail.com>
2024-02-27 18:18:13 +0100

    Bump BSK

--- LocalPackages/SyncUI/Package.swift
+++ LocalPackages/SyncUI/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../SwiftUIExtensions"),
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "111.1.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "111.1.1"),
     ],
     targets: [
         .target(
