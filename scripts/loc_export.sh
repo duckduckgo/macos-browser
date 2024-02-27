0dbb82d1cd473686104b06a2ff65f514e18da37d
Sabrina Tardio <stardio@duckduckgo.com>
2024-02-27 12:54:25 +0100

    cleann up

--- scripts/loc_export.sh
+++ scripts/loc_export.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Get the directory where the script is stored
+# Get the directory where the script is stored and define paths
 script_dir=$(dirname "$(readlink -f "$0")")
-base_dir="${script_dir}/.."
-
-# Define the base directory for localization export
 export_path="${script_dir}/TempLocalizationExport"
-
-# Define the final directory for the xliff file
 final_xliff_path="${script_dir}/assets/loc"
-
-# Check for an input argument to rename the .xliff file, if provided
-new_xliff_name="$1"
+new_xliff_name="$1" # Optional argument for renaming the .xliff file
 
 # Ensure the final xliff directory exists
 mkdir -p "$final_xliff_path"
@@ -19,26 +12,21 @@ mkdir -p "$final_xliff_path"
 # Export localizations
 xcodebuild -exportLocalizations -localizationPath "$export_path" -derivedDataPath "${script_dir}/DerivedData" -scheme "DuckDuckGo Privacy Browser" APP_STORE_PRODUCT_MODULE_NAME_OVERRIDE=DuckDuckGo_Privacy_Browser_App_Store PRIVACY_PRO_PRODUCT_MODULE_NAME_OVERRIDE=DuckDuckGo_Privacy_Browser_Privacy_Pro
 
-# Navigate to the exported .xloc file
-cd "${export_path}" || exit
+# Attempt to find the .xcloc package
+xcloc_package=$(find "$export_path" -type d -name "*.xcloc")
 
-xcloc_package=$(find . -type d -name "*.xcloc")
-
-if [ -n "$xcloc_package" ]; then
-    echo "Extracting .xliff from $xcloc_package"
-    # Determine the target .xliff file path
-    xliff_file="en.xliff"
-    if [ -n "$new_xliff_name" ]; then
-        xliff_file="${new_xliff_name}.xliff"
-    fi
-
-    # Extract the .xliff file to the final path
-    cp "${xcloc_package}/Localized Contents/en.xliff" "${final_xliff_path}/${xliff_file}"
-    echo "Extraction complete. .xliff file is now in ${final_xliff_path} as ${xliff_file}"
-else
+# Check if .xcloc package was found and proceed
+if [ -z "$xcloc_package" ]; then
     echo "No .xcloc package found. Exiting."
     exit 1
 fi
+
+echo "Extracting .xliff from $xcloc_package"
+xliff_file="${new_xliff_name:-en}.xliff" # Use provided name or default to "en.xliff"
+
+# Extract the .xliff file to the final path
+cp "${xcloc_package}/Localized Contents/en.xliff" "${final_xliff_path}/${xliff_file}"
+echo "Extraction complete. .xliff file is now in ${final_xliff_path} as ${xliff_file}"
 
 # Cleanup
 echo "Cleaning up temporary files..."
@@ -47,10 +35,8 @@ echo "Cleanup complete."
 
 # Open the directory containing the xliff file
 echo "Opening directory containing the xliff file..."
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    xdg-open "$final_xliff_path"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    open "$final_xliff_path"
-else
-    echo "Cannot open directory automatically on this OS. Please navigate manually to: $final_xliff_path"
-fi
+case "$OSTYPE" in
+  linux-gnu*) xdg-open "$final_xliff_path";;
+  darwin*) open "$final_xliff_path";;
+  *) echo "Cannot open directory automatically on this OS. Please navigate manually to: $final_xliff_path";;
+esac
