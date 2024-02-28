81ed93f1400e558de8a658a03c7e44ba4b5b96b7
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-02-28 16:46:48 +0100

    Updates BSK

--- LocalPackages/LoginItems/Package.swift
+++ LocalPackages/LoginItems/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "111.1.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "111.1.1-1"),
     ],
     targets: [
         .target(
