50f14111a9302d9cd9583e65bf60d16036ab7130
ladamski@duckduckgo.com <ladamski@users.noreply.github.com>
2024-03-02 14:52:49 -0800

    update CTL rule management to use main TDS

---
+++ DuckDuckGo/ContentBlocker/ClickToLoad/ClickToLoadRulesSplitter.swift
@@ -0,0 +1,122 @@
+//
+//  ClickToLoadRulesSplitter.swift
+//  DuckDuckGo
+//
+//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import TrackerRadarKit
+import BrowserServicesKit
+
+struct ClickToLoadRulesSplitter {
+
+    public enum Constant {
+
+        public static let clickToLoadRuleListPrefix = "CTL_"
+
+    }
+
+    private let rulesList: ContentBlockerRulesList
+
+    init(rulesList: ContentBlockerRulesList) {
+        self.rulesList = rulesList
+    }
+
+    func split() -> (withoutBlockCTL: ContentBlockerRulesList, withBlockCTL: ContentBlockerRulesList)? {
+        let splitTDS = rulesList.trackerData != nil ? split(trackerData: rulesList.trackerData!) : nil
+        let splitFallbackTDS = split(trackerData: rulesList.fallbackTrackerData)
+
+        if splitTDS != nil || splitFallbackTDS != nil {
+            return (
+                ContentBlockerRulesList(name: rulesList.name,
+                                        trackerData: splitTDS?.withoutBlockCTL ?? rulesList.trackerData,
+                                        fallbackTrackerData: splitFallbackTDS?.withoutBlockCTL ?? rulesList.fallbackTrackerData),
+                ContentBlockerRulesList(name: "XD",
+                                        trackerData: splitTDS?.withBlockCTL ?? rulesList.trackerData,
+                                        fallbackTrackerData: splitFallbackTDS?.withBlockCTL ?? rulesList.fallbackTrackerData)
+            )
+        }
+        return nil
+    }
+
+    private func split(trackerData: TrackerDataManager.DataSet) -> (withoutBlockCTL: TrackerDataManager.DataSet, withBlockCTL: TrackerDataManager.DataSet)? {
+        let trackersWithBlockCTL = filterTrackersByBlockCTLAction(trackerData.tds.trackers, hasBlockCTL: true)
+
+        if !trackersWithBlockCTL.isEmpty {
+            let trackersWithoutBlockCTL = filterTrackersByBlockCTLAction(trackerData.tds.trackers, hasBlockCTL: false)
+            let trackerDataWithoutBlockCTL = makeTrackerData(using: trackersWithoutBlockCTL, originalTDS: trackerData.tds)
+            let trackerDataWithBlockCTL = makeTrackerData(using: trackersWithBlockCTL, originalTDS: trackerData.tds)
+
+            return (
+                (tds: trackerDataWithoutBlockCTL, etag: Constant.clickToLoadRuleListPrefix + trackerData.etag),
+                (tds: trackerDataWithBlockCTL, etag: Constant.clickToLoadRuleListPrefix + trackerData.etag)
+            )
+        }
+        return nil
+    }
+
+    private func makeTrackerData(using trackers: [String: KnownTracker], originalTDS: TrackerData) -> TrackerData {
+        let entities = originalTDS.extractEntities(for: trackers)
+        let domains = extractDomains(from: entities)
+        return TrackerData(trackers: trackers,
+                           entities: entities,
+                           domains: domains,
+                           cnames: originalTDS.cnames)
+    }
+
+    private func filterTrackersByBlockCTLAction(_ trackers: [String: KnownTracker], hasBlockCTL: Bool) -> [String: KnownTracker] {
+        trackers.filter { (_, tracker) in tracker.containsCTLActions == hasBlockCTL }
+    }
+
+    private func extractDomains(from entities: [String: Entity]) -> [String: String] {
+        var domains = [String: String]()
+        for entity in entities {
+            for domain in entity.value.domains ?? [] {
+                domains[domain] = entity.key
+            }
+        }
+        return domains
+    }
+
+}
+
+private extension TrackerData {
+
+    func extractEntities(for trackers: [String: KnownTracker]) -> [String: Entity] {
+        let trackerOwners = Set(trackers.values.compactMap { $0.owner?.name })
+        let entities = entities.filter { trackerOwners.contains($0.key) }
+        return entities
+    }
+
+}
+
+private extension KnownTracker {
+
+    var containsCTLActions: Bool {
+        if let defaultAction = defaultAction, defaultAction == .blockCtlFB {
+            return true
+        }
+
+        if let rules = rules {
+            for rule in rules {
+                if let action = rule.action, action == .blockCtlFB {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+}
--- DuckDuckGo/ContentBlocker/ContentBlockerRulesLists.swift
+++ DuckDuckGo/ContentBlocker/ContentBlockerRulesLists.swift
@@ -66,21 +66,24 @@ final class ContentBlockerRulesLists: DefaultContentBlockerRulesListsSource {
            let tdsRulesIndex = result.firstIndex(where: { $0.name == DefaultContentBlockerRulesListsSource.Constants.trackerDataSetRulesListName }) {
             let tdsRules = result[tdsRulesIndex]
             let allowlistedTrackerNames = adClickAttribution.allowlist.map { $0.entity }
-            let splitter = AdClickAttributionRulesSplitter(rulesList: tdsRules, allowlistedTrackerNames: allowlistedTrackerNames)
-            if let splitRules = splitter.split() {
+            let adSplitter = AdClickAttributionRulesSplitter(rulesList: tdsRules, allowlistedTrackerNames: allowlistedTrackerNames)
+            if let splitRules = adSplitter.split() {
                 result.remove(at: tdsRulesIndex)
                 result.append(splitRules.0)
                 result.append(splitRules.1)
             }
         }
 
-        // Add new ones
-        let etag = MD5(data: Self.fbTrackerDataFile)
-        let dataSet: TrackerDataManager.DataSet = TrackerDataManager.DataSet(Self.fbTrackerDataSet, etag)
-        let CTLRulesList = ContentBlockerRulesList(name: Constants.clickToLoadRulesListName,
-                                                   trackerData: nil,
-                                                   fallbackTrackerData: dataSet)
-            result.append(CTLRulesList)
+        // split CTL rules so they can be managed separately from the main list when user clicks through a CTL dialog
+        if let tdsRulesIndex = result.firstIndex(where: { $0.name == DefaultContentBlockerRulesListsSource.Constants.trackerDataSetRulesListName }) {
+            let tdsRules = result[tdsRulesIndex]
+            let ctlSplitter = ClickToLoadRulesSplitter(rulesList: tdsRules)
+            if let splitRules = ctlSplitter.split() {
+                result.remove(at: tdsRulesIndex)
+                result.append(splitRules.withoutBlockCTL)
+                result.append(splitRules.withBlockCTL)
+            }
+        }
 
         return result
     }
