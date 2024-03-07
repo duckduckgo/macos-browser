e233c18eb21a929686c96928edf4e2bc2581ff4d
Anh Do <ado@duckduckgo.com>
2024-03-06 22:56:34 -0500

    Update BSK

--- LocalPackages/LoginItems/Package.swift
+++ LocalPackages/LoginItems/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/duckduckgo/apple-toolbox.git", exact: "1.0.0"),
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "116.0.2")
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "116.1.0")
     ],
     targets: [
         .target(
