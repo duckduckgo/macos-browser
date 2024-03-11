1006f74261cb2c0eab98e83e62cfee4dada8c5e7
Michal Smaga <msmaga@duckduckgo.com>
2024-03-11 12:16:06 +0100

    Properly open purchase page tab from alerts

--- DuckDuckGo/Tab/UserScripts/SubscriptionPagesUserScript.swift
+++ DuckDuckGo/Tab/UserScripts/SubscriptionPagesUserScript.swift
@@ -397,7 +397,7 @@ extension MainWindowController {
         guard let window else { return }
 
         window.show(.subscriptionNotFoundAlert(), firstButtonAction: {
-            WindowControllersManager.shared.show(url: .subscriptionPurchase, source: .ui, newTab: true)
+            WindowControllersManager.shared.showTab(with: .subscription(.subscriptionPurchase))
         })
     }
 
@@ -406,7 +406,7 @@ extension MainWindowController {
         guard let window else { return }
 
         window.show(.subscriptionInactiveAlert(), firstButtonAction: {
-            WindowControllersManager.shared.show(url: .subscriptionPurchase, source: .ui, newTab: true)
+            WindowControllersManager.shared.showTab(with: .subscription(.subscriptionPurchase))
         })
     }
 
