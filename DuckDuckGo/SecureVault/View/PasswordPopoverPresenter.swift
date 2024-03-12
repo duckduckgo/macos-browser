789f06c301f10a3c3f5eb1b50d5791dec642705a
Pete <peadar81@gmail.com>
2024-03-12 16:52:30 +0000

    Revert "Add NavigationBarPopoverTests, mocks"
    This reverts commit c315ed98bdf3e11716bb110d6fdc297f9073a1b6.

--- DuckDuckGo/SecureVault/View/PasswordPopoverPresenter.swift
+++ DuckDuckGo/SecureVault/View/PasswordPopoverPresenter.swift
@@ -19,7 +19,6 @@
 import BrowserServicesKit
 import Foundation
 
-/// Conforming types provide methods to show and dismiss a `PasswordManagementPopover`
 protocol PasswordPopoverPresenter {
     var passwordDomain: String? { get set }
     var popoverIsDirty: Bool { get }
@@ -49,12 +48,10 @@ final class DefaultPasswordPopoverPresenter: NSObject, PasswordPopoverPresenter,
         popover?.isShown ?? false
     }
 
-    /// Note: Dismisses any previously displayed popover before showing a new one
     func show(under view: NSView, withDomain domain: String?, selectedCategory category: SecureVaultSorting.Category?) {
         show(under: view, withDomain: domain).select(category: category)
     }
 
-    /// Note: Dismisses any previously displayed popover before showing a new one
     func show(under view: NSView, withSelectedAccount account: SecureVaultModels.WebsiteAccount) {
         show(under: view, withDomain: nil).select(websiteAccount: account)
     }
