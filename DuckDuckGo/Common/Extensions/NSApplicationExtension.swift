9aab41c2af1fe6f6487fa57bf2907eb74b6ab8b2
kshann <kshannon@duckduckgo.com>
2024-03-13 16:00:19 +1100

    Set up a CI workflow for running UI tests for the macOS browser (#2387)
    Task/Issue URL:
    https://app.asana.com/0/72649045549333/1206610209649715/f
    Tech Design URL:
    CC: @ayoy 
    
    **Description**:
    PR that enables running of sync tests via CI on macos11/12/13/14. Tests
    currently not passing.
    Known issues:
    - xcpretty doesn't work for macos11
    - Asana is not notified for failures (yet)
    
    **Steps to test this PR**:
    1. Run the workflows
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)
    
    ---------
    
    Co-authored-by: Dominik Kapusta <dkapusta@duckduckgo.com>

--- DuckDuckGo/Common/Extensions/NSApplicationExtension.swift
+++ DuckDuckGo/Common/Extensions/NSApplicationExtension.swift
@@ -54,9 +54,16 @@ extension NSApplication {
             }
         } else if ProcessInfo().environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
             return .xcPreviews
+        } else if ProcessInfo.processInfo.environment["UITEST_MODE"] == "1" {
+            return .uiTests
         } else {
             return .normal
         }
+#elseif REVIEW
+        if ProcessInfo.processInfo.environment["UITEST_MODE"] == "1" {
+            return .uiTests
+        }
+        return .normal
 #else
         return .normal
 #endif
