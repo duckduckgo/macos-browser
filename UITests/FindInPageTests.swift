d1e2190255bcc8e45907c384f253349a6d307546
Halle <378795+Halle@users.noreply.github.com>
2024-03-14 17:38:14 +0100

    Find in page tests

--- UITests/FindInPageTests.swift
+++ UITests/FindInPageTests.swift
@@ -28,7 +28,7 @@ extension FindInPageTests {
 
     func saveLocalHTML() {
         let loremIpsumHTML = """
-        <html><head><title>Lorem Ipsum</title></head><body><table><tr><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac sem nisi. Cras fermentum mi vitae turpis efficitur malesuada. Donec eget maximus ligula, et tincidunt sapien. Suspendisse posuere diam maximus, dignissim ex at, fringilla elit. Maecenas enim tellus, ornare non pretium a, sodales nec lectus. Vestibulum quis augue orci. Donec eget mi sed magna consequat auctor a a nulla. Etiam condimentum, neque at congue semper, arcu sem commodo tellus, venenatis finibus ex magna vitae erat. Nunc non enim sit amet mi posuere egestas. Donec nibh nisl, pretium sit amet molestie aliquet, porta id nibh. Pellentesque ullamcorper mauris quam, semper hendrerit mi dictum non. Nullam pulvinar, nulla a maximus egestas, velit mi volutpat neque, vitae placerat eros sapien vitae tellus. Pellentesque malesuada accumsan dolor, ut feugiat enim. Curabitur nunc quam, maximus venenatis augue vel, molestie accumsan eros.</p>
+        <html><head><title>Lorem Ipsum</title></head><body><table><tr><td><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac sem nisi. Cras fermentum mi vitae turpis efficitur malesuada. Donec eget maximus ligula, et tincidunt sapien. Suspendisse posuere diam maximus, dignissim ex at, fringilla elit. Maecenas enim tellus, ornare non pretium a, sodales nec lectus. Vestibulum quis augue orci. Donec eget mi sed magna consequat auctor a a nulla. Etiam condimentum, neque at congue semper, arcu sem commodo tellus, venenatis finibus ex magna vitae erat. Nunc non enim sit amet mi posuere egestas. Donec nibh nisl, pretium sit amet aliquet, porta id nibh. Pellentesque ullamcorper mauris quam, semper hendrerit mi dictum non. Nullam pulvinar, nulla a maximus egestas, velit mi volutpat neque, vitae placerat eros sapien vitae tellus. Pellentesque malesuada accumsan dolor, ut feugiat enim. Curabitur nunc quam, maximus venenatis augue vel, accumsan eros.</p>
 
                                 <p>Donec consequat ultrices ante non maximus. Quisque eu semper diam. Nunc ullamcorper eget ex id luctus. Duis metus ex, dapibus sit amet vehicula eget, rhoncus eget lacus. Nulla maximus quis turpis vel pulvinar. Duis neque ligula, tristique et diam ut, fringilla sagittis arcu. Vestibulum suscipit semper lectus, quis placerat ex euismod eu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;</p>
 
@@ -55,85 +55,113 @@ extension FindInPageTests {
 
 class FindInPageTests: XCTestCase {
     let app = XCUIApplication()
-    override class func setUp() {
-        // This is the setUp() class method.
-        // XCTest calls it before calling the first test method.
-        // Set up any overall initial state here.
-    }
-
-    override func setUp() async throws {
-        // This is the setUp() async instance method.
-        // XCTest calls it before each test method.
-        // Perform any asynchronous setup in this method.
-    }
+    let timeout = 0.3
+    let searchOrEnterAddressTextField = XCUIApplication().windows.textFields["AddressBarViewController.addressBarTextField"]
+    let loremIpsumWebView = XCUIApplication().windows.webViews["Lorem Ipsum"]
+    let findInPageCloseButton = XCUIApplication().windows.buttons["FindInPageController.closeButton"]
 
     override func setUpWithError() throws {
         continueAfterFailure = false
-
+        saveLocalHTML()
         app.launch()
-        // This is the setUpWithError() instance method.
-        // XCTest calls it before each test method.
-        // Set up any synchronous per-test state that might throw errors here.
-    }
-
-    override func setUp() {
-        // This is the setUp() instance method.
-        // XCTest calls it before each test method.
-        // Set up any synchronous per-test state here.
-    }
-
-    override class func tearDown() {
-        // This is the tearDown() class method.
-        // XCTest calls it after the last test method completes.
-        // Perform any overall cleanup here.
-    }
-
-    override func tearDown() {
-        // This is the tearDown() instance method.
-        // XCTest calls it after each test method.
-        // Perform any synchronous per-test cleanup here.
     }
 
     override func tearDownWithError() throws {
-        // This is the tearDownWithError() instance method.
-        // XCTest calls it after each test method.
-        // Perform any synchronous per-test cleanup that might throw errors here.
-    }
-
-    override func tearDown() async throws {
-        // This is the tearDown() async instance method.
-        // XCTest calls it after each test method.
-        // Perform any asynchronous per-test cleanup here.
+        removeLocalHTML()
     }
 
     func test_findInPage_canBeOpenedWithKeyCommand() throws {
-        saveLocalHTML()
-        let searchOrEnterAddressTextField = app.windows.textFields["AddressBarViewController.addressBarTextField"]
-        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: 1), "Address bar text field does not exist when it is expected to exist")
+        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: timeout), "Address bar text field does not exist when it is expected to exist")
         searchOrEnterAddressTextField.typeText("\(loremIpsumFileURL().absoluteString)\r")
-        let loremIpsumWebView = XCUIApplication().windows.webViews["Lorem Ipsum"]
-        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: 1), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
-        
+        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: timeout), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
+
         app.typeKey("f", modifierFlags: .command)
-        let findInPageCloseButton = XCUIApplication().windows.buttons["FindInPageController.closeButton"]
-        
-        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: 1), "After invoking find in page with command-f, the elements of the find in page interface should exist.")
-        removeLocalHTML()
+
+        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: timeout), "After invoking find in page with command-f, the elements of the find in page interface should exist.")
     }
 
-    func test_findInPage_canBeOpenedWithMenuItem() throws {
-        saveLocalHTML()
-        let searchOrEnterAddressTextField = app.windows.textFields["AddressBarViewController.addressBarTextField"]
-        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: 1), "Address bar text field does not exist when it is expected to exist")
+    func test_findInPage_canBeOpenedWithMenuBarItem() throws {
+        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: timeout), "Address bar text field does not exist when it is expected to exist")
         searchOrEnterAddressTextField.typeText("\(loremIpsumFileURL().absoluteString)\r")
-        let loremIpsumWebView = XCUIApplication().windows.webViews["Lorem Ipsum"]
-        let findInPageMenuBarItem = app.menuItems["MainViewController.findInPage"]
-        XCTAssertTrue(findInPageMenuBarItem.waitForExistence(timeout: 1), "Couldn't find Find in Page menu bar item in a reasonable timeframe.")
-        
+        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: timeout), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
+        let findInPageMenuBarItem = app.menuItems["MainMenu.findInPage"]
+        XCTAssertTrue(findInPageMenuBarItem.waitForExistence(timeout: timeout), "Couldn't find Find in Page menu bar item in a reasonable timeframe.")
+
         findInPageMenuBarItem.click()
-        let findInPageCloseButton = XCUIApplication().windows.buttons["FindInPageController.closeButton"]
-        
-        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: 1), "After invoking find in page via the menu items Edit->Find->Find in Page, the elements of the find in page interface should exist.")
-        removeLocalHTML()
+
+        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: timeout), "After invoking find in page via the menu items Edit->Find->Find in Page, the elements of the find in page interface should exist.")
+    }
+
+    func test_findInPage_canBeOpenedWithMoreOptionsMenuItem() throws {
+        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: timeout), "Address bar text field does not exist when it is expected to exist")
+        searchOrEnterAddressTextField.typeText("\(loremIpsumFileURL().absoluteString)\r")
+        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: timeout), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
+        let optionsButton = XCUIApplication().windows.buttons["NavigationBarViewController.optionsButton"]
+        XCTAssertTrue(optionsButton.waitForExistence(timeout: timeout), "Couldn't find options item in a reasonable timeframe.")
+        optionsButton.click()
+
+        let findInPageMoreOptionsMenuBarItem = app.menuItems["MoreOptionsMenu.findInPage"]
+        XCTAssertTrue(findInPageMoreOptionsMenuBarItem.waitForExistence(timeout: timeout), "Couldn't find more options find in page menu item in a reasonable timeframe.")
+        findInPageMoreOptionsMenuBarItem.click()
+
+        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: timeout), "After invoking find in page via the more options find in Page menu item, the elements of the find in page interface should exist.")
+    }
+
+    func test_findInPage_canBeClosedWithEscape() throws {
+        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: timeout), "Address bar text field does not exist when it is expected to exist")
+        searchOrEnterAddressTextField.typeText("\(loremIpsumFileURL().absoluteString)\r")
+        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: timeout), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
+        app.typeKey("f", modifierFlags: .command)
+        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: timeout), "After invoking find in page with command-f, the elements of the find in page interface should exist.")
+
+        app.typeKey(.escape, modifierFlags: [])
+
+        XCTAssertTrue(findInPageCloseButton.waitForNonExistence(timeout: timeout), "After closing find in page with escape, the elements of the find in page interface should no longer exist.")
+    }
+
+    func test_findInPage_canBeClosedWithShiftCommandF() throws {
+        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: timeout), "Address bar text field does not exist when it is expected to exist")
+        searchOrEnterAddressTextField.typeText("\(loremIpsumFileURL().absoluteString)\r")
+        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: timeout), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
+        app.typeKey("f", modifierFlags: .command)
+        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: timeout), "After invoking find in page with command-f, the elements of the find in page interface should exist.")
+
+        app.typeKey("f", modifierFlags: [.command, .shift])
+
+        XCTAssertTrue(findInPageCloseButton.waitForNonExistence(timeout: timeout), "After closing find in page with escape, the elements of the find in page interface should no longer exist.")
+    }
+
+    func test_findInPage_canBeClosedWithHideFindMenuItem() throws {
+        XCTAssertTrue(searchOrEnterAddressTextField.waitForExistence(timeout: timeout), "Address bar text field does not exist when it is expected to exist")
+        searchOrEnterAddressTextField.typeText("\(loremIpsumFileURL().absoluteString)\r")
+        XCTAssertTrue(loremIpsumWebView.waitForExistence(timeout: timeout), "Local lorem ipsum web page didn't load with the expected title in a reasonable timeframe.")
+        app.typeKey("f", modifierFlags: .command)
+        XCTAssertTrue(findInPageCloseButton.waitForExistence(timeout: timeout), "After invoking find in page with command-f, the elements of the find in page interface should exist.")
+
+        let findInPageDoneMenuBarItem = app.menuItems["MainMenu.findInPageDone"]
+        XCTAssertTrue(findInPageDoneMenuBarItem.waitForExistence(timeout: timeout), "Couldn't find find in page done main menu item in a reasonable timeframe.")
+        findInPageDoneMenuBarItem.click()
+
+        XCTAssertTrue(findInPageCloseButton.waitForNonExistence(timeout: timeout), "After closing find in page with escape, the elements of the find in page interface should no longer exist.")
+    }
+}
+
+extension XCUIElement {
+    // https://stackoverflow.com/a/37447150/119717
+
+    /**
+     * Waits the specified amount of time for the element’s `exists` property to become `false`.
+     *
+     * - Parameter timeout: The amount of time to wait.
+     * - Returns: `false` if the timeout expires without the element coming out of existence.
+     */
+    func waitForNonExistence(timeout: TimeInterval) -> Bool {
+        let timeStart = Date().timeIntervalSince1970
+
+        while Date().timeIntervalSince1970 <= (timeStart + timeout) {
+            if !exists { return true }
+        }
+
+        return false
     }
 }
