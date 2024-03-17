fffc05bfe568a29dd7fc7314107211b98a56017d
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-03-17 07:56:19 +0100

    Improves pixel information and adds tests

--- DuckDuckGo/NetworkProtection/AppAndExtensionAndAgentTargets/NetworkProtectionPixelEvent.swift
+++ DuckDuckGo/NetworkProtection/AppAndExtensionAndAgentTargets/NetworkProtectionPixelEvent.swift
@@ -103,169 +103,169 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
         switch self {
 
         case .networkProtectionActiveUser:
-            return "m_mac_netp_daily_active"
+            return "netp_daily_active"
 
         case .networkProtectionNewUser:
-            return "m_mac_netp_daily_active_u"
+            return "netp_daily_active_u"
 
         case .networkProtectionControllerStartAttempt:
-            return "m_mac_netp_controller_start_attempt"
+            return "netp_controller_start_attempt"
 
         case .networkProtectionControllerStartSuccess:
-            return "m_mac_netp_controller_start_success"
+            return "netp_controller_start_success"
 
         case .networkProtectionControllerStartFailure:
-            return "m_mac_netp_controller_start_failure"
+            return "netp_controller_start_failure"
 
         case .networkProtectionTunnelStartAttempt:
-            return "m_mac_netp_tunnel_start_attempt"
+            return "netp_tunnel_start_attempt"
 
         case .networkProtectionTunnelStartSuccess:
-            return "m_mac_netp_tunnel_start_success"
+            return "netp_tunnel_start_success"
 
         case .networkProtectionTunnelStartFailure:
-            return "m_mac_netp_tunnel_start_failure"
+            return "netp_tunnel_start_failure"
 
         case .networkProtectionTunnelUpdateAttempt:
-            return "m_mac_netp_tunnel_update_attempt"
+            return "netp_tunnel_update_attempt"
 
         case .networkProtectionTunnelUpdateSuccess:
-            return "m_mac_netp_tunnel_update_success"
+            return "netp_tunnel_update_success"
 
         case .networkProtectionTunnelUpdateFailure:
-            return "m_mac_netp_tunnel_update_failure"
+            return "netp_tunnel_update_failure"
 
         case .networkProtectionEnableAttemptConnecting:
-            return "m_mac_netp_ev_enable_attempt"
+            return "netp_ev_enable_attempt"
 
         case .networkProtectionEnableAttemptSuccess:
-            return "m_mac_netp_ev_enable_attempt_success"
+            return "netp_ev_enable_attempt_success"
 
         case .networkProtectionEnableAttemptFailure:
-            return "m_mac_netp_ev_enable_attempt_failure"
+            return "netp_ev_enable_attempt_failure"
 
         case .networkProtectionTunnelFailureDetected:
-            return "m_mac_netp_ev_tunnel_failure"
+            return "netp_ev_tunnel_failure"
 
         case .networkProtectionTunnelFailureRecovered:
-            return "m_mac_netp_ev_tunnel_failure_recovered"
+            return "netp_ev_tunnel_failure_recovered"
 
         case .networkProtectionLatency(let quality):
-            return "m_mac_netp_ev_\(quality.rawValue)_latency"
+            return "netp_ev_\(quality.rawValue)_latency"
 
         case .networkProtectionLatencyError:
-            return "m_mac_netp_ev_latency_error"
+            return "netp_ev_latency_error"
 
         case .networkProtectionTunnelConfigurationNoServerRegistrationInfo:
-            return "m_mac_netp_tunnel_config_error_no_server_registration_info"
+            return "netp_tunnel_config_error_no_server_registration_info"
 
         case .networkProtectionTunnelConfigurationCouldNotSelectClosestServer:
-            return "m_mac_netp_tunnel_config_error_could_not_select_closest_server"
+            return "netp_tunnel_config_error_could_not_select_closest_server"
 
         case .networkProtectionTunnelConfigurationCouldNotGetPeerPublicKey:
-            return "m_mac_netp_tunnel_config_error_could_not_get_peer_public_key"
+            return "netp_tunnel_config_error_could_not_get_peer_public_key"
 
         case .networkProtectionTunnelConfigurationCouldNotGetPeerHostName:
-            return "m_mac_netp_tunnel_config_error_could_not_get_peer_host_name"
+            return "netp_tunnel_config_error_could_not_get_peer_host_name"
 
         case .networkProtectionTunnelConfigurationCouldNotGetInterfaceAddressRange:
-            return "m_mac_netp_tunnel_config_error_could_not_get_interface_address_range"
+            return "netp_tunnel_config_error_could_not_get_interface_address_range"
 
         case .networkProtectionClientFailedToFetchServerList:
-            return "m_mac_netp_backend_api_error_failed_to_fetch_server_list"
+            return "netp_backend_api_error_failed_to_fetch_server_list"
 
         case .networkProtectionClientFailedToParseServerListResponse:
-            return "m_mac_netp_backend_api_error_parsing_server_list_response_failed"
+            return "netp_backend_api_error_parsing_server_list_response_failed"
 
         case .networkProtectionClientFailedToEncodeRegisterKeyRequest:
-            return "m_mac_netp_backend_api_error_encoding_register_request_body_failed"
+            return "netp_backend_api_error_encoding_register_request_body_failed"
 
         case .networkProtectionClientFailedToFetchRegisteredServers:
-            return "m_mac_netp_backend_api_error_failed_to_fetch_registered_servers"
+            return "netp_backend_api_error_failed_to_fetch_registered_servers"
 
         case .networkProtectionClientFailedToParseRegisteredServersResponse:
-            return "m_mac_netp_backend_api_error_parsing_device_registration_response_failed"
+            return "netp_backend_api_error_parsing_device_registration_response_failed"
 
         case .networkProtectionClientFailedToEncodeRedeemRequest:
-            return "m_mac_netp_backend_api_error_encoding_redeem_request_body_failed"
+            return "netp_backend_api_error_encoding_redeem_request_body_failed"
 
         case .networkProtectionClientInvalidInviteCode:
-            return "m_mac_netp_backend_api_error_invalid_invite_code"
+            return "netp_backend_api_error_invalid_invite_code"
 
         case .networkProtectionClientFailedToRedeemInviteCode:
-            return "m_mac_netp_backend_api_error_failed_to_redeem_invite_code"
+            return "netp_backend_api_error_failed_to_redeem_invite_code"
 
         case .networkProtectionClientFailedToParseRedeemResponse:
-            return "m_mac_netp_backend_api_error_parsing_redeem_response_failed"
+            return "netp_backend_api_error_parsing_redeem_response_failed"
 
         case .networkProtectionClientFailedToFetchLocations:
-            return "m_mac_netp_backend_api_error_failed_to_fetch_location_list"
+            return "netp_backend_api_error_failed_to_fetch_location_list"
 
         case .networkProtectionClientFailedToParseLocationsResponse:
-            return "m_mac_netp_backend_api_error_parsing_location_list_response_failed"
+            return "netp_backend_api_error_parsing_location_list_response_failed"
 
         case .networkProtectionClientInvalidAuthToken:
-            return "m_mac_netp_backend_api_error_invalid_auth_token"
+            return "netp_backend_api_error_invalid_auth_token"
 
         case .networkProtectionServerListStoreFailedToEncodeServerList:
-            return "m_mac_netp_storage_error_failed_to_encode_server_list"
+            return "netp_storage_error_failed_to_encode_server_list"
 
         case .networkProtectionServerListStoreFailedToDecodeServerList:
-            return "m_mac_netp_storage_error_failed_to_decode_server_list"
+            return "netp_storage_error_failed_to_decode_server_list"
 
         case .networkProtectionServerListStoreFailedToWriteServerList:
-            return "m_mac_netp_storage_error_server_list_file_system_write_failed"
+            return "netp_storage_error_server_list_file_system_write_failed"
 
         case .networkProtectionServerListStoreFailedToReadServerList:
-            return "m_mac_netp_storage_error_server_list_file_system_read_failed"
+            return "netp_storage_error_server_list_file_system_read_failed"
 
         case .networkProtectionKeychainErrorFailedToCastKeychainValueToData:
-            return "m_mac_netp_keychain_error_failed_to_cast_keychain_value_to_data"
+            return "netp_keychain_error_failed_to_cast_keychain_value_to_data"
 
         case .networkProtectionKeychainReadError:
-            return "m_mac_netp_keychain_error_read_failed"
+            return "netp_keychain_error_read_failed"
 
         case .networkProtectionKeychainWriteError:
-            return "m_mac_netp_keychain_error_write_failed"
+            return "netp_keychain_error_write_failed"
 
         case .networkProtectionKeychainUpdateError:
-            return "m_mac_netp_keychain_error_update_failed"
+            return "netp_keychain_error_update_failed"
 
         case .networkProtectionKeychainDeleteError:
-            return "m_mac_netp_keychain_error_delete_failed"
+            return "netp_keychain_error_delete_failed"
 
         case .networkProtectionWireguardErrorCannotLocateTunnelFileDescriptor:
-            return "m_mac_netp_wireguard_error_cannot_locate_tunnel_file_descriptor"
+            return "netp_wireguard_error_cannot_locate_tunnel_file_descriptor"
 
         case .networkProtectionWireguardErrorInvalidState:
-            return "m_mac_netp_wireguard_error_invalid_state"
+            return "netp_wireguard_error_invalid_state"
 
         case .networkProtectionWireguardErrorFailedDNSResolution:
-            return "m_mac_netp_wireguard_error_failed_dns_resolution"
+            return "netp_wireguard_error_failed_dns_resolution"
 
         case .networkProtectionWireguardErrorCannotSetNetworkSettings:
-            return "m_mac_netp_wireguard_error_cannot_set_network_settings"
+            return "netp_wireguard_error_cannot_set_network_settings"
 
         case .networkProtectionWireguardErrorCannotStartWireguardBackend:
-            return "m_mac_netp_wireguard_error_cannot_start_wireguard_backend"
+            return "netp_wireguard_error_cannot_start_wireguard_backend"
 
         case .networkProtectionNoAuthTokenFoundError:
-            return "m_mac_netp_no_auth_token_found_error"
+            return "netp_no_auth_token_found_error"
 
         case .networkProtectionRekeyAttempt:
-            return "m_mac_netp_rekey_attempt"
+            return "netp_rekey_attempt"
 
         case .networkProtectionRekeyCompleted:
-            return "m_mac_netp_rekey_completed"
+            return "netp_rekey_completed"
 
         case .networkProtectionRekeyFailure:
-            return "m_mac_netp_rekey_failure"
+            return "netp_rekey_failure"
 
         case .networkProtectionSystemExtensionActivationFailure:
-            return "m_mac_netp_system_extension_activation_failure"
+            return "netp_system_extension_activation_failure"
 
         case .networkProtectionUnhandledError:
-            return "m_mac_netp_unhandled_error"
+            return "netp_unhandled_error"
         }
     }
 
