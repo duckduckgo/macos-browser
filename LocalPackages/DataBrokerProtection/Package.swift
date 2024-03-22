11853baef85f1c778cd0cc292b9bd1f2002c966a
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2024-03-22 18:32:32 -0300

    BrowserServicesKit bump to 129.1.4 (#2487)

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "129.1.3"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "129.1.4"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
