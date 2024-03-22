c7cf61dcd5575ea6007ddb07c2d3ffcbe3da4d81
Michal Smaga <miasma13@gmail.com>
2024-03-22 21:05:08 +0100

    Rework the caching logic for subscription and entitlements (#2485)
    Task/Issue URL:
    https://app.asana.com/0/1200019156869587/1206909771441779/f
    
    
    **Description**:
    
    - Change default staleness time interval for subscription and
    entitlement cache to 20min
    - When storing subscription in the cache use the date based on default
    interval or its date of renewalOrExpiry - whatever is earlier
    - Make the VPN code use cache and its logic for doing fetch if data is
    stale
    - On applicationDidBecomeActive refresh subscription details and
    entitlements
    - In the app rely on subscription details and entitlements based on
    cache logic (cached if still valid else fetch)
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "129.1.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "129.1.3"),
         .package(path: "../PixelKit"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
