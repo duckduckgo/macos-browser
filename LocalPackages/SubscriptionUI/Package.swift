8a1a28d43ac36a17c90dba7848298534d99157cb
Brad Slayter <slayterdevelopment@gmail.com>
2024-03-22 10:40:54 -0500

    Pin BSK version

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", revision: "60d5768d975cc71769072a2da01c7227b05e04e0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "129.1.2"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
