2c223b185d8902b57828c51e31eba08d57d4435b
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-03-26 13:20:59 +0100

    Add support for local field validation for synced bookmarks and credentials (#2454)
    Task/Issue URL: https://app.asana.com/0/0/1206637913741251/f
    
    Description:
    This change adds a mechanism that filters out syncable objects that would fail validation on the backend
    before sending Sync patch request. Objects rejected from patch payload are retried on every subsequent
    Sync request, until they're updated to pass validation or deleted.

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "129.2.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "130.0.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
