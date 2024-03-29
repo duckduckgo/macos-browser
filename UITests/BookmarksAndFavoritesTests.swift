15b29fe699d896f266115cc36cd8e86191d7f670
Halle <378795+Halle@users.noreply.github.com>
2024-03-29 16:04:47 +0100

    First three pasing tests

--- UITests/BookmarksAndFavoritesTests.swift
+++ UITests/BookmarksAndFavoritesTests.swift
@@ -29,6 +29,8 @@ class BookmarksAndFavoritesTests: XCTestCase {
     private var bookmarkPageContextMenuItem: XCUIElement!
     private var bookmarksMenu: XCUIElement!
     private var addressBarBookmarkButton: XCUIElement!
+    private var optionsButton: XCUIElement!
+    private var bookmarkDialogBookmarkFolderDropdown: XCUIElement!
 
     override func setUpWithError() throws {
         continueAfterFailure = false
@@ -42,6 +44,8 @@ class BookmarksAndFavoritesTests: XCTestCase {
         bookmarkPageContextMenuItem = app.menuItems["ContextMenuManager.bookmarkPageMenuItem"]
         bookmarksMenu = app.menuBarItems["Bookmarks"]
         addressBarBookmarkButton = app.buttons["AddressBarButtonsViewController.bookmarkButton"]
+        optionsButton = app.buttons["NavigationBarViewController.optionsButton"]
+        bookmarkDialogBookmarkFolderDropdown = app.popUpButtons["bookmark.add.folder.dropdown"]
         app.launch()
     }
 
@@ -51,25 +55,28 @@ class BookmarksAndFavoritesTests: XCTestCase {
             resetBookMarksMenuItem.waitForExistence(timeout: UITests.Timeouts.elementExistence),
             "Reset bookmarks menu item didn't become available in a reasonable timeframe."
         )
-
         resetBookMarksMenuItem.click()
     }
 
     func test_bookmarks_canBeAddedTo_withContextClickBookmarkThisPage() {
         resetBookmarks()
+        XCTAssertTrue(
+            addressBarTextField.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The address bar text field didn't become available in a reasonable timeframe."
+        )
         addressBarTextField.typeText("\(urlForBookmarksBar.absoluteString)\r")
         XCTAssertTrue(
             app.windows.webViews[pageTitle].waitForExistence(timeout: UITests.Timeouts.elementExistence),
             "Visited site didn't load with the expected title in a reasonable timeframe."
         )
         app.windows.webViews[pageTitle].rightClick()
-        XCTAssertTrue(
+        XCTAssertTrue( // Bookmark the loaded page via context menu
             bookmarkPageContextMenuItem.waitForExistence(timeout: UITests.Timeouts.elementExistence),
             "\"Add bookmark\" context menu item didn't appear in a reasonable timeframe."
         )
 
         bookmarkPageContextMenuItem.click()
-        XCTAssertTrue(
+        XCTAssertTrue( // Check Add Bookmark dialog for existence but don't click on it
             defaultBookmarkDialogButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
             "The \"Add bookmark\" dialog option button didn't appear with the expected title in a reasonable timeframe."
         )
@@ -77,11 +84,156 @@ class BookmarksAndFavoritesTests: XCTestCase {
             addressBarBookmarkButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
             "The address bar bookmark button didn't appear with the expected title in a reasonable timeframe."
         )
+        XCTAssertTrue(
+            bookmarkDialogBookmarkFolderDropdown.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Add bookmark\" dialog's bookmark folder dropdown didn't appear with the expected title in a reasonable timeframe."
+        )
+        let bookmarkDialogBookmarkFolderDropdownValue = try? XCTUnwrap( // Bookmark dialog must default to "Bookmarks" folder
+            bookmarkDialogBookmarkFolderDropdown.value as? String,
+            "It wasn't possible to get the value of the \"Add bookmark\" dialog's bookmark folder dropdown as String"
+        )
+        XCTAssertEqual(
+            bookmarkDialogBookmarkFolderDropdownValue,
+            "Bookmarks",
+            "The accessibility value of the \"Add bookmark\" dialog's bookmark folder dropdown must be \"Bookmarks\"."
+        )
         let addressBarBookmarkButtonValue = try? XCTUnwrap(
             addressBarBookmarkButton.value as? String,
             "It wasn't possible to get the value of the address bar bookmark button as String"
         )
 
+        XCTAssertEqual( // The bookmark icon is already in a filled state and it isn't necessary to click the add button
+            addressBarBookmarkButtonValue,
+            "Bookmarked",
+            "The accessibility value of the address bar bookmark button must be \"Bookmarked\", which indicates the icon in the filled state."
+        )
+        XCTAssertTrue(
+            bookmarksMenu.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Bookmarks\" menu bar item didn't appear with the expected title in a reasonable timeframe."
+        )
+        bookmarksMenu.click()
+        XCTAssertTrue( // And the bookmark is found in the Bookmarks menu
+            app.menuItems[pageTitle].waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The bookmark in the \"Bookmarks\" menu with the title of the test page didn't appear with the expected title in a reasonable timeframe."
+        )
+    }
+
+    func test_bookmarks_canBeAddedTo_withSettingsItemBookmarkThisPage() {
+        let openBookmarksMenuItem = app.menuItems["MoreOptionsMenu.openBookmarks"]
+        let bookmarkPageMenuItem = app.menuItems["MoreOptionsMenu.bookmarkPage"]
+        resetBookmarks()
+        XCTAssertTrue(
+            addressBarTextField.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The address bar text field didn't become available in a reasonable timeframe."
+        )
+        addressBarTextField.typeText("\(urlForBookmarksBar.absoluteString)\r")
+        XCTAssertTrue(
+            app.windows.webViews[pageTitle].waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "Visited site didn't load with the expected title in a reasonable timeframe."
+        )
+
+        XCTAssertTrue( // Bookmark the loaded page via settings menu
+            optionsButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The options button didn't appear with the expected title in a reasonable timeframe."
+        )
+        optionsButton.click()
+        XCTAssertTrue(
+            openBookmarksMenuItem.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The settings \"Bookmarks\" menu item didn't appear with the expected title in a reasonable timeframe."
+        )
+        openBookmarksMenuItem.hover()
+        XCTAssertTrue(
+            bookmarkPageMenuItem.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The settings \"Bookmark page\" menu item didn't appear with the expected title in a reasonable timeframe."
+        )
+        bookmarkPageMenuItem.click()
+        XCTAssertTrue( // Check Add Bookmark dialog for existence but don't click on it
+            defaultBookmarkDialogButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Add bookmark\" dialog option button didn't appear with the expected title in a reasonable timeframe."
+        )
+        XCTAssertTrue(
+            addressBarBookmarkButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The address bar bookmark button didn't appear with the expected title in a reasonable timeframe."
+        )
+        XCTAssertTrue(
+            bookmarkDialogBookmarkFolderDropdown.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Add bookmark\" dialog's bookmark folder dropdown didn't appear with the expected title in a reasonable timeframe."
+        )
+        let bookmarkDialogBookmarkFolderDropdownValue = try? XCTUnwrap(
+            bookmarkDialogBookmarkFolderDropdown.value as? String,
+            "It wasn't possible to get the value of the \"Add bookmark\" dialog's bookmark folder dropdown as String"
+        )
+        XCTAssertEqual( // Bookmark dialog must default to "Bookmarks" folder
+            bookmarkDialogBookmarkFolderDropdownValue,
+            "Bookmarks",
+            "The accessibility value of the \"Add bookmark\" dialog's bookmark folder dropdown must be \"Bookmarks\"."
+        )
+        let addressBarBookmarkButtonValue = try? XCTUnwrap(
+            addressBarBookmarkButton.value as? String,
+            "It wasn't possible to get the value of the address bar bookmark button as String"
+        )
+
+        XCTAssertEqual( // The bookmark icon is already in a filled state and it isn't necessary to click the add button
+            addressBarBookmarkButtonValue,
+            "Bookmarked",
+            "The accessibility value of the address bar bookmark button must be \"Bookmarked\", which indicates the icon in the filled state."
+        )
+        XCTAssertTrue(
+            bookmarksMenu.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Bookmarks\" menu bar item didn't appear with the expected title in a reasonable timeframe."
+        )
+        bookmarksMenu.click()
+        XCTAssertTrue( // And the bookmark is found in the Bookmarks menu
+            app.menuItems[pageTitle].waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The bookmark in the \"Bookmarks\" menu with the title of the test page didn't appear with the expected title in a reasonable timeframe."
+        )
+    }
+
+    func test_bookmarks_canBeAddedTo_byClickingBookmarksButtonInAddressBar() {
+        resetBookmarks()
+        XCTAssertTrue(
+            addressBarTextField.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The address bar text field didn't become available in a reasonable timeframe."
+        )
+        addressBarTextField.typeText("\(urlForBookmarksBar.absoluteString)\r")
+        XCTAssertTrue(
+            app.windows.webViews[pageTitle].waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "Visited site didn't load with the expected title in a reasonable timeframe."
+        )
+
+        // In order to directly click the bookmark button in the address bar, we need to hover over something in the bar area
+        XCTAssertTrue( // We already have access to the optionsButton so it is OK to use for this hover
+            optionsButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The options button didn't appear with the expected title in a reasonable timeframe."
+        )
+        optionsButton.hover()
+        XCTAssertTrue( // Now the button will exist so we can check for it and use it
+            addressBarBookmarkButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The address bar bookmark button didn't appear with the expected title in a reasonable timeframe."
+        )
+        addressBarBookmarkButton.click()
+        XCTAssertTrue( // Check Add Bookmark dialog for existence but don't click on it
+            defaultBookmarkDialogButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Add bookmark\" dialog option button didn't appear with the expected title in a reasonable timeframe."
+        )
+        XCTAssertTrue(
+            bookmarkDialogBookmarkFolderDropdown.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The \"Add bookmark\" dialog's bookmark folder dropdown didn't appear with the expected title in a reasonable timeframe."
+        )
+        let bookmarkDialogBookmarkFolderDropdownValue = try? XCTUnwrap(
+            bookmarkDialogBookmarkFolderDropdown.value as? String,
+            "It wasn't possible to get the value of the \"Add bookmark\" dialog's bookmark folder dropdown as String"
+        )
+
+        XCTAssertEqual( // Bookmark dialog must default to "Bookmarks" folder
+            bookmarkDialogBookmarkFolderDropdownValue,
+            "Bookmarks",
+            "The accessibility value of the \"Add bookmark\" dialog's bookmark folder dropdown must be \"Bookmarks\"."
+        )
+        let addressBarBookmarkButtonValue = try? XCTUnwrap(
+            addressBarBookmarkButton.value as? String,
+            "It wasn't possible to get the value of the address bar bookmark button as String"
+        )
         XCTAssertEqual( // The bookmark icon is already in a filled state and it isn't necessary to click the add button
             addressBarBookmarkButtonValue,
             "Bookmarked",
