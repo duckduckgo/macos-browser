cdf15afa6527b83c46f2d783a912a2b25fdec192
Halle <378795+Halle@users.noreply.github.com>
2024-04-10 12:58:25 +0200

    Add a debug method and a pre-test-case method to UITests

--- UITests/Common/UITests.swift
+++ UITests/Common/UITests.swift
@@ -49,4 +49,43 @@ enum UITests {
     static func randomPageTitle(length: Int) -> String {
         return String(UUID().uuidString.prefix(length))
     }
+
+    /// This is intended for setting an autocomplete checkbox state that extends across all test cases and is only run once in the class override
+    /// setup() of the case. Setting the autocomplete checkbox state for an individual test shouldn't start and terminate the app, as this function
+    /// does.
+    /// - Parameter requestedToggleState: How the autocomplete checkbox state should be set
+    static func setAutocompleteToggleBeforeTestcaseRuns(_ requestedToggleState: Bool) {
+        let app = XCUIApplication()
+        app.launch()
+
+        app.typeKey(",", modifierFlags: [.command]) // Open settings
+        let generalPreferencesButton = app.buttons["PreferencesSidebar.generalButton"]
+        let autocompleteToggle = app.checkBoxes["PreferencesGeneralView.showAutocompleteSuggestions"]
+        XCTAssertTrue(
+            generalPreferencesButton.waitForExistence(timeout: UITests.Timeouts.elementExistence),
+            "The user settings appearance section button didn't become available in a reasonable timeframe."
+        )
+        generalPreferencesButton.click(forDuration: 0.5, thenDragTo: generalPreferencesButton)
+
+        let currentToggleState = try? XCTUnwrap(
+            autocompleteToggle.value as? Bool,
+            "It wasn't possible to get the \"Autocomplete\" value as a Bool"
+        )
+
+        switch (requestedToggleState, currentToggleState) { // Click autocomplete toggle if it is different than our request
+        case (false, true), (true, false):
+            autocompleteToggle.click()
+        default:
+            break
+        }
+        app.terminate()
+    }
+
+    /// A debug function that is going to need some other functionality in order to be useful for debugging address bar focus issues
+    static func openVanillaBrowser() {
+        let app = XCUIApplication()
+        let openVanillaBrowser = app.menuItems["MainMenu.openVanillaBrowser"]
+        openVanillaBrowser.clickAfterExistenceTestSucceeds()
+        app.typeKey("w", modifierFlags: [.command, .option])
+    }
 }
