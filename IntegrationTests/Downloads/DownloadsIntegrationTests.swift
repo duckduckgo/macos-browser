6530cb5b6d2a01da659317ec8aa2d7975d912464
Alexey Martemyanov <mallexxx@gmail.com>
2024-04-10 11:34:22 +0600

    add integration tests

--- IntegrationTests/Downloads/DownloadsIntegrationTests.swift
+++ IntegrationTests/Downloads/DownloadsIntegrationTests.swift
@@ -92,6 +92,102 @@ class DownloadsIntegrationTests: XCTestCase {
     }
 
     @MainActor
+    func testWhenUnsupportedMimeType_downloadStarts() async throws {
+        let preferences = DownloadsPreferences.shared
+        preferences.alwaysRequestDownloadLocation = false
+        preferences.selectedDownloadLocation = FileManager.default.temporaryDirectory
+
+        let downloadTaskFuture = FileDownloadManager.shared.downloadsPublisher.timeout(5).first().promise()
+        let suffix = Int.random(in: 0..<Int.max)
+        let url = URL.testsServer
+            .appendingPathComponent("fname_\(suffix).dat")
+            .appendingTestParameters(data: data.html,
+                                     headers: ["Content-Type": "application/unsupported-mime-type"])
+        let tab = tabViewModel.tab
+        _=await tab.setUrl(url, source: .link)?.result
+
+        let fileUrl = try await downloadTaskFuture.get().output
+            .timeout(1, scheduler: DispatchQueue.main) { .init(TimeoutError() as NSError) }.first().promise().get()
+
+        XCTAssertEqual(fileUrl, FileManager.default.temporaryDirectory.appendingPathComponent("fname_\(suffix).dat"))
+        XCTAssertEqual(try? Data(contentsOf: fileUrl), data.html)
+    }
+
+    @MainActor
+    func testWhenLocalFile_downloadStartsAlwaysDisplayingSavePanel() async throws {
+        let tempFileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        let destDirURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        try FileManager.default.createDirectory(at: destDirURL, withIntermediateDirectories: true)
+        try data.html.write(to: tempFileURL)
+
+        let preferences = DownloadsPreferences.shared
+        preferences.alwaysRequestDownloadLocation = false
+        preferences.selectedDownloadLocation = destDirURL
+        let persistor = DownloadsPreferencesUserDefaultsPersistor()
+        persistor.lastUsedCustomDownloadLocation = destDirURL.path
+
+        let downloadTaskFuture = FileDownloadManager.shared.downloadsPublisher.timeout(5).first().promise()
+        let tab = tabViewModel.tab
+        _=await tab.setUrl(tempFileURL, source: .link)?.result
+
+        let eSaveDialogShown = expectation(description: "Save dialog shown")
+        let getSaveDialog = Task { @MainActor in
+            while true {
+                if let sheet = self.window.sheets.first as? NSSavePanel {
+                    eSaveDialogShown.fulfill()
+                    return sheet
+                }
+                try await Task.sleep(interval: 0.01)
+            }
+        }
+
+        if case .timedOut = await XCTWaiter(delegate: self).fulfillment(of: [eSaveDialogShown], timeout: 5) {
+            getSaveDialog.cancel()
+        }
+        let saveDialog = try await getSaveDialog.value
+        window.endSheet(saveDialog, returnCode: .OK)
+
+        let fileUrl = try await downloadTaskFuture.get().output
+            .timeout(1, scheduler: DispatchQueue.main) { .init(TimeoutError() as NSError) }.first().promise().get()
+
+        XCTAssertEqual(fileUrl.resolvingSymlinksInPath(), destDirURL.appendingPathComponent(tempFileURL.lastPathComponent).resolvingSymlinksInPath())
+        XCTAssertEqual(try? Data(contentsOf: fileUrl), data.html)
+    }
+
+    @MainActor
+    func testWhenLocalNonExistentFile_loadingFails() async throws {
+        let preferences = DownloadsPreferences.shared
+        preferences.alwaysRequestDownloadLocation = false
+        preferences.selectedDownloadLocation = FileManager.default.temporaryDirectory
+        let tempFileURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+
+        let downloadTaskFuture = FileDownloadManager.shared.downloadsPublisher.timeout(5).first().promise()
+        let tab = tabViewModel.tab
+        let loadingResult = await tab.setUrl(tempFileURL, source: .link)?.result
+
+        XCTAssertThrowsError(try loadingResult?.get()) { error in
+            XCTAssertEqual((error as? URLError)?.code, .fileDoesNotExist)
+        }
+    }
+
+    @MainActor
+    func testWhenLocalFolder_loadingFails() async throws {
+        let preferences = DownloadsPreferences.shared
+        preferences.alwaysRequestDownloadLocation = false
+        preferences.selectedDownloadLocation = FileManager.default.temporaryDirectory
+        let dirURL = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        try FileManager.default.createDirectory(at: dirURL, withIntermediateDirectories: true)
+
+        let downloadTaskFuture = FileDownloadManager.shared.downloadsPublisher.timeout(5).first().promise()
+        let tab = tabViewModel.tab
+        let loadingResult = await tab.setUrl(dirURL, source: .link)?.result
+
+        XCTAssertThrowsError(try loadingResult?.get()) { error in
+            XCTAssertEqual((error as? URLError)?.code, .fileIsDirectory)
+        }
+    }
+
+    @MainActor
     func testWhenSaveDialogOpenInBackgroundTabAndTabIsClosed_downloadIsCancelled() async throws {
         let persistor = DownloadsPreferencesUserDefaultsPersistor()
         persistor.alwaysRequestDownloadLocation = true
