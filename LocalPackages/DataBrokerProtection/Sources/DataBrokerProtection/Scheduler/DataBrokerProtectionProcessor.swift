570206e73dfc4a5acd7e70f02d9c9cae7d81bf32
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2024-04-24 19:29:26 -0300

    Fix DataBrokerProtectionProcessor.swift lint

--- LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Scheduler/DataBrokerProtectionProcessor.swift
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Scheduler/DataBrokerProtectionProcessor.swift
@@ -56,7 +56,7 @@ final class DataBrokerProtectionProcessor {
 
     // MARK: - Public functions
     func startManualScans(showWebView: Bool = false,
-                              completion: ((DataBrokerProtectionSchedulerErrorCollection?) -> Void)? = nil) {
+                          completion: ((DataBrokerProtectionSchedulerErrorCollection?) -> Void)? = nil) {
         operationQueue.cancelAllOperations()
         runOperations(operationType: .scan,
                       priorityDate: nil,
