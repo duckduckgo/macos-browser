523a7975b895dace051fd8b05a73fff3fe85fd3a
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-04-25 18:03:41 +0200

    Clean up extract_release_notes script

--- scripts/extract_release_notes.sh
+++ scripts/extract_release_notes.sh
@@ -13,6 +13,7 @@ placeholder="add release notes here"
 is_capturing=0
 is_capturing_pp=0
 has_content=0
+notes=
 pp_notes=
 
 output="html"
@@ -52,6 +53,40 @@ lowercase() {
 	tr '[:upper:]' '[:lower:]' <<< "$input"
 }
 
+print_and_exit() {
+	echo -ne "$notes"
+	exit 0
+}
+
+add_to_notes() {
+	notes+="$1"
+	if [[ "$output" != "asana" ]]; then
+		notes+="\\n"
+	fi
+}
+
+add_to_pp_notes() {
+	pp_notes+="$1"
+	if [[ "$output" != "asana" ]]; then
+		pp_notes+="\\n"
+	fi
+}
+
+add_release_note() {
+	local release_note="$1"
+	local processed_release_note=
+	if [[ "$output" == "raw" || "$output" == "tasks" ]]; then
+		processed_release_note="$release_note"
+	else
+		processed_release_note="<li>$(make_links "$(html_escape "$release_note")")</li>"
+	fi
+	if [[ $is_capturing_pp -eq 1 ]]; then
+		add_to_pp_notes "$processed_release_note"
+	else
+		add_to_notes "$processed_release_note"
+	fi
+}
+
 while read -r line
 do
 	# Lowercase each line to compare with markers
@@ -61,68 +96,39 @@ do
 		# Only start capturing here
 		is_capturing=1
 		if [[ "$output" == "asana" ]]; then
-			printf '%s' '<ul>'
+			add_to_notes "<ul>"
 		elif [[ "$output" == "html" ]]; then
 			# Add HTML header and start the list
-			echo "<h3 style=\"font-size:14px\">What's new</h3>"
-			echo "<ul>"
+			add_to_notes "<h3 style=\"font-size:14px\">What's new</h3>"
+			add_to_notes "<ul>"
 		fi
 	elif [[ -n "$pp_marker" && "$lowercase_line" =~ $pp_marker ]]; then
 		is_capturing_pp=1
 		if [[ "$output" == "asana" ]]; then
-			pp_notes="$(printf '%s' '</ul>')"
-			pp_notes+="$(printf '%s' '<h2>For Privacy Pro subscribers</h2>')"
-			pp_notes+="$(printf '%s' '<ul>')"
+			add_to_pp_notes "</ul><h2>For Privacy Pro subscribers</h2><ul>"
 		elif [[ "$output" == "html" ]]; then
-			# If we've reached the PP marker, end the list and start the PP list\
-			# redirect to pp_notes variable
-			pp_notes="</ul>\\n"
-			pp_notes+="<h3 style=\"font-size:14px\">For Privacy Pro subscribers</h3>\\n"
-			pp_notes+="<ul>\\n"
+			# If we've reached the PP marker, end the list and start the PP list
+			add_to_pp_notes "</ul>"
+			add_to_pp_notes "<h3 style=\"font-size:14px\">For Privacy Pro subscribers</h3>"
+			add_to_pp_notes "<ul>"
 		else
-			pp_notes="$line\\n"
+			add_to_pp_notes "$line"
 		fi
 	elif [[ -n "$end_marker" && "$lowercase_line" == "$end_marker" ]]; then
-		# If we've reached the end marker, end the PP list and start the end list
+		# If we've reached the end marker, check if PP notes are present and not a placeholder, and add them verbatim to notes
 		# shellcheck disable=SC2076
 		if [[ -n "$pp_notes" && ! "$(lowercase "$pp_notes")" =~ "$placeholder" ]]; then
-			if [[ "$output" == "asana" ]]; then
-				printf '%s' "$pp_notes"
-			else
-				echo -ne "$pp_notes"
-			fi
+			notes+="$pp_notes" # never add extra newline here (that's why we don't use `add_to_notes`)
 		fi
-		# End the list on end marker
-		if [[ "$output" == "asana" ]]; then
-			printf '%s' '</ul>'
-		elif [[ "$output" == "html" ]]; then
-			echo "</ul>"
+		if [[ "$output" != "raw" ]]; then
+			# End the list on end marker
+			add_to_notes "</ul>"
 		fi
-		exit 0
+		# Print output and exit
+		print_and_exit
 	elif [[ $is_capturing -eq 1 && -n "$line" ]]; then
 		has_content=1
-		if [[ "$output" == "asana" ]]; then
-			escaped_line=$(html_escape "$line")
-			if [[ $is_capturing_pp -eq 1 ]]; then
-				pp_notes+="<li>$(make_links "$escaped_line")</li>"
-			else
-				printf '%s' "<li>$(make_links "$escaped_line")</li>"
-			fi
-		elif [[ "$output" == "html" ]]; then
-			# Add each line as a list item and convert URLs to clickable links
-			escaped_line=$(html_escape "$line")
-			if [[ $is_capturing_pp -eq 1 ]]; then
-				pp_notes+="<li>$(make_links "$escaped_line")</li>\\n"
-			else
-				echo "<li>$(make_links "$escaped_line")</li>"
-			fi
-		else
-			if [[ $is_capturing_pp -eq 1 ]]; then
-				pp_notes+="$line\\n"
-			else
-				echo "$line"
-			fi
-		fi
+		add_release_note "$line"
 	fi
 done
 
@@ -130,4 +136,4 @@ if [[ $has_content -eq 0 ]]; then
 	exit 1
 fi
 
-exit 0
+print_and_exit
