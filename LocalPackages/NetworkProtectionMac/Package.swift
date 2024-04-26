c926d465183e1c382e5919d5c2aa43fcb87310e3
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-04-26 17:39:20 +0200

    Updated some BSK references

--- LocalPackages/NetworkProtectionMac/Package.swift
+++ LocalPackages/NetworkProtectionMac/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
         .library(name: "NetworkProtectionUI", targets: ["NetworkProtectionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "140.0.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "140.0.3"),
         .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.4.1"),
         .package(path: "../XPCHelper"),
         .package(path: "../SwiftUIExtensions"),
