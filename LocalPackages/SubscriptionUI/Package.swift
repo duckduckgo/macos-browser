9e4cf4ef9bc76ce069f47eec19f715a20d08766d
Jacek Łyp <jlyp@duckduckgo.com>
2024-05-08 18:32:20 +0200

    Update BSK

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "144.0.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "144.0.3"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
