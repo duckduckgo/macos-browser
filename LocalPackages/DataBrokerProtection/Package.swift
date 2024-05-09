4ab25c97c48dbbf8bd7f95f1323d063c5122d35b
Christopher Brind <brindy@duckduckgo.com>
2024-05-09 11:45:47 +0100

    Add bookmarks to top hits suggestions on iOS (#2754)
    Task/Issue URL: https://app.asana.com/0/0/1207251449708624/f
    Tech Design URL:
    CC:
    
    **Description**:
    Changes iOS so that bookmarks are included in top hits. No change for
    macOS.
    
    **Steps to test this PR**:
    See https://github.com/duckduckgo/BrowserServicesKit/pull/812

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "144.0.3"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "144.0.4"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
     ],
