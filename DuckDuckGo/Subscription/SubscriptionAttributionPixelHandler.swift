5dcf4a3fc4e6c0488dc87bfe0b09df339db5d06c
Alessandro Boron <aboron@duckduckgo.com>
2024-05-09 16:50:59 +1000

    Add extensions to GenericAttributionPixelHandler to track attribution for successful subscription

---
+++ DuckDuckGo/Subscription/SubscriptionAttributionPixelHandler.swift
@@ -0,0 +1,54 @@
+//
+//  SubscriptionAttributionPixelHandler.swift
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import Subscription
+
+protocol SubscriptionAttributionPixelHandler {
+    func fireSuccessfulSubscriptionAttributionPixel()
+}
+
+// MARK: - SubscriptionAttributionPixelHandler
+
+extension GenericAttributionPixelHandler: SubscriptionAttributionPixelHandler {
+
+    func fireSuccessfulSubscriptionAttributionPixel() {
+        fireAttributionPixel(
+            event: PrivacyProPixel.privacyProSuccessfulSubscriptionAttribution,
+            frequency: .standard,
+            parameters: nil
+        )
+    }
+
+}
+
+// MARK: - GenericAttributionPixelHandler + Subscription
+
+extension GenericAttributionPixelHandler {
+
+    static let subscription = GenericAttributionPixelHandler(
+        originProvider: SubscriptionOriginStore(
+            userDefaults: UserDefaults.subs
+        )
+    )
+
+}
+
+// MARK: SubscriptionOriginStore + AttributionOriginProvider
+
+extension SubscriptionOriginStore: AttributionOriginProvider {}
