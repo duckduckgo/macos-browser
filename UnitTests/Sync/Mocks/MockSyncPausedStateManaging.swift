55ad2d273f64db8349a7a073f8ff9c20efd3dc64
Sabrina Tardio <stardio@duckduckgo.com>
2024-05-09 12:15:16 +0200

    solve linting

--- UnitTests/Sync/Mocks/MockSyncPausedStateManaging.swift
+++ UnitTests/Sync/Mocks/MockSyncPausedStateManaging.swift
@@ -20,7 +20,7 @@ import Foundation
 import Combine
 @testable import DuckDuckGo_Privacy_Browser
 
-class MockSyncPausedStateManaging: SyncPausedStateManaging {    
+class MockSyncPausedStateManaging: SyncPausedStateManaging {
     static var syncBookmarksPausedData = SyncPausedMessageData(title: "Bookmarks Paused", description: "Something with bookmark is wrong", buttonTitle: "Manage Bookmarks", action: {print("something bookmarks")})
     static var syncCredentialsPausedData = SyncPausedMessageData(title: "Credentials Paused", description: "Something with Credentials is wrong", buttonTitle: "Manage Credentials", action: {print("something Credentials")})
     static var syncIsPausedData = SyncPausedMessageData(title: "Paused", description: "Something is wrong", buttonTitle: "", action: nil)
