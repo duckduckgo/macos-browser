2619e1fa6664c6368cbafe8fd2cafaca797427f4
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-05-10 15:12:52 +0200

    Enable gzip compression for Sync PATCH payloads (#2723)
    Task/Issue URL: https://app.asana.com/0/0/1206919211758354/f
    
    Description:
    This change adds gzip compression to all Sync data PATCH requests.

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "144.0.5"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "144.0.6"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
