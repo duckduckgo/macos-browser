0815921366f6e79ff9b839b5aec4d97557d3d01a
Brian Hall <brian@hack.design>
2024-05-15 10:31:00 -0500

    Bump BSK Version (#2766)
    Task/Issue URL: https://app.asana.com/0/0/1207281817761492/f
    Tech Design URL:
    CC:
    
    **Description**:
    
    **Steps to test this PR**:
    1.
    
    <!--
    Tagging instructions
    If this PR isn't ready to be merged for whatever reason it should be
    marked with the `DO NOT MERGE` label (particularly if it's a draft)
    If it's pending Product Review/PFR, please add the `Pending Product
    Review` label.
    
    If at any point it isn't actively being worked on/ready for
    review/otherwise moving forward (besides the above PR/PFR exception)
    strongly consider closing it (or not opening it in the first place). If
    you decide not to close it, make sure it's labelled to make it clear the
    PRs state and comment with more information.
    -->
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "145.0.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "145.1.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
     ],
