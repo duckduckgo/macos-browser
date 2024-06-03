9984e50bd9e371dad1a85b48fa19ac1b46045699
Pete Smith <peadar81@gmail.com>
2024-06-03 12:12:27 +0200

    DBP: Enable DBP Operations to Run When Device is Locked (#2797)
    Task/Issue URL:
    https://app.asana.com/0/1206488453854252/1206891169830209/f
    Tech Design URL:
    https://app.asana.com/0/1206488453854252/1207313481959835/f
    
    **Description**:
    This PR changes the accessibility attribute of DBP Keychain items to
    enable them to be accessed when the device is locked. It also migrates
    Keychain values for existing DBP users.

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "148.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "149.0.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
