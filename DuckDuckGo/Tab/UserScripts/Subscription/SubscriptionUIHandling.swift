6f85a8b6b9caf86af9452cf50e4fbf216993ad0d
Federico Cappelli <fcappelli@duckduckgo.com>
2024-06-11 17:38:51 +0100

    cleanup

--- DuckDuckGo/Tab/UserScripts/Subscription/SubscriptionUIHandling.swift
+++ DuckDuckGo/Tab/UserScripts/Subscription/SubscriptionUIHandling.swift
@@ -21,68 +21,19 @@ import SubscriptionUI
 
 @MainActor
 protocol SubscriptionUIHandling {
-    // MARK: - ProgressViewController
+    
+    // MARK: ProgressViewController
     func presentProgressViewController(withTitle: String)
-    // let mainViewController = await WindowControllersManager.shared.lastKeyMainWindowController?.mainViewController
-    // let progressViewController = await ProgressViewController(title: UserText.purchasingSubscriptionTitle)
-
     func dismissProgressViewController()
-    // await mainViewController?.dismiss(progressViewController)
-
     func updateProgressViewController(title: String)
-    // await progressViewController.updateTitleText(UserText.completingPurchaseTitle)
 
-    // MARK: - SubscriptionAccessViewController
+    // MARK: SubscriptionAccessViewController
     func presentSubscriptionAccessViewController(handler: SubscriptionAccessActionHandling, message: WKScriptMessage)
-/*
- guard let mainViewController = await WindowControllersManager.shared.lastKeyMainWindowController?.mainViewController,
-       let windowControllerManager = await WindowControllersManager.shared.lastKeyMainWindowController else {
-     return nil
- }
- let message = original
-
- let actionHandlers = SubscriptionAccessActionHandlers(restorePurchases: {
-     if #available(macOS 12.0, *) {
-         Task { @MainActor in
-             let subscriptionAppStoreRestorer = SubscriptionAppStoreRestorer(subscriptionManager: self.subscriptionManager)
-             await subscriptionAppStoreRestorer.restoreAppStoreSubscription(mainViewController: mainViewController, windowController: windowControllerManager)
-             message.webView?.reload()
-         }
-     }
- }, openURLHandler: { url in
-     DispatchQueue.main.async {
-         WindowControllersManager.shared.showTab(with: .subscription(url))
-     }
- }, uiActionHandler: { event in
-     switch event {
-     case .activateAddEmailClick:
-         PixelKit.fire(PrivacyProPixel.privacyProRestorePurchaseEmailStart, frequency: .dailyAndCount)
-     default:
-         break
-     }
- })
-
- let subscriptionAccessViewController = await SubscriptionAccessViewController(subscriptionManager: subscriptionManager, actionHandlers: actionHandlers)
- await WindowControllersManager.shared.lastKeyMainWindowController?.mainViewController.presentAsSheet(subscriptionAccessViewController)
-
- */
 
     // MARK: Alerts
     func show(alertType: SubscriptionAlertType)
     func show(alertType: SubscriptionAlertType, firstButtonAction: (() -> Void)?)
     func show(alertType: SubscriptionAlertType, text: String?)
-/*
-    func show(_ alert: NSAlert, firstButtonAction: (() -> Void)? = nil) {
-        alert.beginSheetModal(for: self, completionHandler: { response in
-            if case .alertFirstButtonReturn = response {
-                firstButtonAction?()
-            }
-        })
-    }
-
- //        guard let window else { return }
- //        window.show(.subscriptionFoundAlert(), firstButtonAction: {
- */
 
     // MARK: Tab
     func showTab(with content: Tab.TabContent)
