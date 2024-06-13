e688145cc12d2dd48d526ade8512728915a87a16
Federico Cappelli <fcappelli@duckduckgo.com>
2024-06-13 16:13:27 +0100

    Subscription refactoring - UI handler (#2853)
    Task/Issue URL:
    https://app.asana.com/0/1205842942115003/1206805455884775/f
    
    All UI interactions have been extracted from Subscription business logic and isolated in a mockable UI handler

--- DuckDuckGo/Preferences/View/PreferencesRootView.swift
+++ DuckDuckGo/Preferences/View/PreferencesRootView.swift
@@ -165,13 +165,9 @@ enum Preferences {
             let sheetActionHandler = SubscriptionAccessActionHandlers(restorePurchases: {
                 if #available(macOS 12.0, *) {
                     Task {
-                        guard let mainViewController = WindowControllersManager.shared.lastKeyMainWindowController?.mainViewController,
-                              let windowControllerManager = WindowControllersManager.shared.lastKeyMainWindowController else {
-                            return
-                        }
-
-                        let subscriptionAppStoreRestorer = SubscriptionAppStoreRestorer(subscriptionManager: Application.appDelegate.subscriptionManager)
-                        await subscriptionAppStoreRestorer.restoreAppStoreSubscription(mainViewController: mainViewController, windowController: windowControllerManager)
+                        let subscriptionAppStoreRestorer = SubscriptionAppStoreRestorer(subscriptionManager: Application.appDelegate.subscriptionManager,
+                                                                                        uiHandler: Application.appDelegate.subscriptionUIHandler)
+                        await subscriptionAppStoreRestorer.restoreAppStoreSubscription()
                     }
                 }
             },
