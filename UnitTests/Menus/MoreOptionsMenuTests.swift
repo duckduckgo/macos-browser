3842f5cf4621aeac01592d027909a83a9504dec0
Federico Cappelli <fcappelli@duckduckgo.com>
2024-06-24 15:16:05 +0200

    menu unit tests fixed

--- UnitTests/Menus/MoreOptionsMenuTests.swift
+++ UnitTests/Menus/MoreOptionsMenuTests.swift
@@ -45,6 +45,7 @@ final class MoreOptionsMenuTests: XCTestCase {
         capturingActionDelegate = CapturingOptionsButtonMenuDelegate()
         internalUserDecider = InternalUserDeciderMock()
         accountManager = AccountManagerMock()
+        accountManager.accessToken = "something" // accountManager.isUserAuthenticated becomes true
         networkProtectionVisibilityMock = NetworkProtectionVisibilityMock(isInstalled: false, visible: false)
         moreOptionsMenu = MoreOptionsMenu(tabCollectionViewModel: tabCollectionViewModel,
                                    passwordManagerCoordinator: passwordManagerCoordinator,
@@ -70,7 +71,7 @@ final class MoreOptionsMenuTests: XCTestCase {
         moreOptionsMenu = MoreOptionsMenu(tabCollectionViewModel: tabCollectionViewModel,
                                          passwordManagerCoordinator: passwordManagerCoordinator,
                                          vpnFeatureGatekeeper: NetworkProtectionVisibilityMock(isInstalled: false, visible: true),
-                                          subscriptionFeatureAvailability: SubscriptionFeatureAvailabilityMock(isFeatureAvailable: true, isSubscriptionPurchaseAllowed: true),
+                                          subscriptionFeatureAvailability: SubscriptionFeatureAvailabilityMock(isFeatureAvailable: false, isSubscriptionPurchaseAllowed: false),
                                          sharingMenu: NSMenu(),
                                          internalUserDecider: internalUserDecider,
                                          accountManager: accountManager)
@@ -90,21 +91,22 @@ final class MoreOptionsMenuTests: XCTestCase {
         XCTAssertEqual(moreOptionsMenu.items[12].title, UserText.emailOptionsMenuItem)
 
         XCTAssertTrue(moreOptionsMenu.items[13].isSeparatorItem)
-        XCTAssertTrue(moreOptionsMenu.items[14].title.hasPrefix(UserText.subscriptionOptionsMenuItem))
-        XCTAssertTrue(moreOptionsMenu.items[15].isSeparatorItem)
-        XCTAssertEqual(moreOptionsMenu.items[16].title, UserText.settings)
+        XCTAssertTrue(moreOptionsMenu.items[14].title.hasPrefix(UserText.networkProtection))
+        XCTAssertTrue(moreOptionsMenu.items[15].title.hasPrefix(UserText.identityTheftRestorationOptionsMenuItem))
+        XCTAssertTrue(moreOptionsMenu.items[16].isSeparatorItem)
+        XCTAssertEqual(moreOptionsMenu.items[17].title, UserText.settings)
     }
 
     @MainActor
     func testThatMoreOptionMenuHasTheExpectedItemsNotAuthenticated() {
-
         accountManager = AccountManagerMock()
         moreOptionsMenu = MoreOptionsMenu(tabCollectionViewModel: tabCollectionViewModel,
-                                         passwordManagerCoordinator: passwordManagerCoordinator,
-                                         vpnFeatureGatekeeper: NetworkProtectionVisibilityMock(isInstalled: false, visible: true),
-                                         sharingMenu: NSMenu(),
-                                         internalUserDecider: internalUserDecider,
-                                         accountManager: accountManager)
+                                          passwordManagerCoordinator: passwordManagerCoordinator,
+                                          vpnFeatureGatekeeper: NetworkProtectionVisibilityMock(isInstalled: false, visible: true),
+                                          subscriptionFeatureAvailability: SubscriptionFeatureAvailabilityMock(isFeatureAvailable: false, isSubscriptionPurchaseAllowed: false),
+                                          sharingMenu: NSMenu(),
+                                          internalUserDecider: internalUserDecider,
+                                          accountManager: accountManager)
 
         XCTAssertEqual(moreOptionsMenu.items[0].title, UserText.sendFeedback)
         XCTAssertTrue(moreOptionsMenu.items[1].isSeparatorItem)
