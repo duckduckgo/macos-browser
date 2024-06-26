be24b09e44b9d320d1e104d50d0930a3791b4c3f
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-06-27 00:10:33 +0200

    Put RemoteMessagingConfigMatcherProvider in a separate file

--- DuckDuckGo/RemoteMessaging/RemoteMessagingClient.swift
+++ DuckDuckGo/RemoteMessaging/RemoteMessagingClient.swift
@@ -17,117 +17,11 @@
 //
 
 import Combine
-import Common
 import Foundation
 import BrowserServicesKit
 import Persistence
 import PixelKit
-import Bookmarks
 import RemoteMessaging
-import NetworkProtection
-import Subscription
-
-final class RemoteMessagingConfigMatcherProvider: RemoteMessagingConfigMatcherProviding {
-
-    init(
-        bookmarksDatabase: CoreDataDatabase,
-        appearancePreferences: AppearancePreferences,
-        internalUserDecider: InternalUserDecider
-    ) {
-        self.bookmarksDatabase = bookmarksDatabase
-        self.appearancePreferences = appearancePreferences
-        self.internalUserDecider = internalUserDecider
-    }
-
-    let bookmarksDatabase: CoreDataDatabase
-    let appearancePreferences: AppearancePreferences
-    let internalUserDecider: InternalUserDecider
-
-    // swiftlint:disable:next function_body_length
-    func refreshConfigMatcher(with store: RemoteMessagingStoring) async -> RemoteMessagingConfigMatcher {
-
-        var bookmarksCount = 0
-        var favoritesCount = 0
-        let context = bookmarksDatabase.makeContext(concurrencyType: .privateQueueConcurrencyType)
-        context.performAndWait {
-            bookmarksCount = BookmarkUtils.numberOfBookmarks(in: context)
-            favoritesCount = BookmarkUtils.numberOfFavorites(for: appearancePreferences.favoritesDisplayMode, in: context)
-        }
-
-        let statisticsStore = LocalStatisticsStore()
-        let variantManager = DefaultVariantManager()
-        let subscriptionManager = await Application.appDelegate.subscriptionManager
-
-        let isPrivacyProSubscriber = subscriptionManager.accountManager.isUserAuthenticated
-        let isPrivacyProEligibleUser = subscriptionManager.canPurchase
-
-        let activationDateStore = DefaultWaitlistActivationDateStore(source: .netP)
-        let daysSinceNetworkProtectionEnabled = activationDateStore.daysSinceActivation() ?? -1
-
-        var privacyProDaysSinceSubscribed = -1
-        var privacyProDaysUntilExpiry = -1
-        var isPrivacyProSubscriptionActive = false
-        var isPrivacyProSubscriptionExpiring = false
-        var isPrivacyProSubscriptionExpired = false
-        var privacyProPurchasePlatform: String?
-        let surveyActionMapper: RemoteMessagingSurveyActionMapping
-
-        if let accessToken = subscriptionManager.accountManager.accessToken {
-            let subscriptionResult = await subscriptionManager.subscriptionEndpointService.getSubscription(accessToken: accessToken)
-
-            if case let .success(subscription) = subscriptionResult {
-                privacyProDaysSinceSubscribed = Calendar.current.numberOfDaysBetween(subscription.startedAt, and: Date()) ?? -1
-                privacyProDaysUntilExpiry = Calendar.current.numberOfDaysBetween(Date(), and: subscription.expiresOrRenewsAt) ?? -1
-                privacyProPurchasePlatform = subscription.platform.rawValue
-
-                switch subscription.status {
-                case .autoRenewable, .gracePeriod:
-                    isPrivacyProSubscriptionActive = true
-                case .notAutoRenewable:
-                    isPrivacyProSubscriptionActive = true
-                    isPrivacyProSubscriptionExpiring = true
-                case .expired, .inactive:
-                    isPrivacyProSubscriptionExpired = true
-                case .unknown:
-                    break // Not supported in RMF
-                }
-
-                surveyActionMapper = DefaultRemoteMessagingSurveyURLBuilder(statisticsStore: statisticsStore, subscription: subscription)
-            } else {
-                surveyActionMapper = DefaultRemoteMessagingSurveyURLBuilder(statisticsStore: statisticsStore, subscription: nil)
-            }
-        } else {
-            surveyActionMapper = DefaultRemoteMessagingSurveyURLBuilder(statisticsStore: statisticsStore, subscription: nil)
-        }
-
-        let dismissedMessageIds = store.fetchDismissedRemoteMessageIds()
-
-        return RemoteMessagingConfigMatcher(
-            appAttributeMatcher: AppAttributeMatcher(statisticsStore: statisticsStore,
-                                                     variantManager: variantManager,
-                                                     isInternalUser: internalUserDecider.isInternalUser),
-            userAttributeMatcher: UserAttributeMatcher(statisticsStore: statisticsStore,
-                                                       variantManager: variantManager,
-                                                       bookmarksCount: bookmarksCount,
-                                                       favoritesCount: favoritesCount,
-                                                       appTheme: appearancePreferences.currentThemeName.rawValue,
-                                                       isWidgetInstalled: false,
-                                                       daysSinceNetPEnabled: daysSinceNetworkProtectionEnabled,
-                                                       isPrivacyProEligibleUser: isPrivacyProEligibleUser,
-                                                       isPrivacyProSubscriber: isPrivacyProSubscriber,
-                                                       privacyProDaysSinceSubscribed: privacyProDaysSinceSubscribed,
-                                                       privacyProDaysUntilExpiry: privacyProDaysUntilExpiry,
-                                                       privacyProPurchasePlatform: privacyProPurchasePlatform,
-                                                       isPrivacyProSubscriptionActive: isPrivacyProSubscriptionActive,
-                                                       isPrivacyProSubscriptionExpiring: isPrivacyProSubscriptionExpiring,
-                                                       isPrivacyProSubscriptionExpired: isPrivacyProSubscriptionExpired,
-                                                       dismissedMessageIds: dismissedMessageIds),
-            percentileStore: RemoteMessagingPercentileUserDefaultsStore(userDefaults: .standard),
-            surveyActionMapper: surveyActionMapper,
-            dismissedMessageIds: dismissedMessageIds
-        )
-    }
-}
 
 final class RemoteMessagingClient: RemoteMessagingClientBase {
 
@@ -142,7 +36,6 @@ final class RemoteMessagingClient: RemoteMessagingClientBase {
         internalUserDecider: InternalUserDecider
     ) {
         self.database = database
-        self.appearancePreferences = appearancePreferences
         self.internalUserDecider = internalUserDecider
 
         let provider = RemoteMessagingConfigMatcherProvider(
@@ -209,7 +102,6 @@ final class RemoteMessagingClient: RemoteMessagingClientBase {
     }
 
     private let database: RemoteMessagingDatabase
-    private let appearancePreferences: AppearancePreferences
     private let internalUserDecider: InternalUserDecider
     private(set) var store: RemoteMessagingStore?
     private var isRemoteMessagingDatabaseLoaded = false
