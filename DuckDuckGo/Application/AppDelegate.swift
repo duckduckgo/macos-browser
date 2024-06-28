05abde032d17856ac81b4b876d02a5a9c1ba77e8
Alexey Martemyanov <mallexxx@gmail.com>
2024-06-28 17:13:11 +0600

    Add [NSApplication _crashOnException:] handler; bump BSK

--- DuckDuckGo/Application/AppDelegate.swift
+++ DuckDuckGo/Application/AppDelegate.swift
@@ -149,6 +149,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
 
     // swiftlint:disable:next function_body_length
     override init() {
+        // will not add crash handlers and will fire pixel on applicationDidFinishLaunching if didCrashDuringCrashHandlersSetUp == true
         let didCrashDuringCrashHandlersSetUp = UserDefaultsWrapper(key: .didCrashDuringCrashHandlersSetUp, defaultValue: false)
         _didCrashDuringCrashHandlersSetUp = didCrashDuringCrashHandlersSetUp
         if case .normal = NSApplication.runType,
