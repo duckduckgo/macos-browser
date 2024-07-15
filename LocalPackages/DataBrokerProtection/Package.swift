5c10df2fdbd2d3e1833b6edfc22653c5ede0e574
Fernando Bunn <bunn@idevzilla.com>
2024-07-15 14:13:37 +0100

    Update BSK

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "bunn/duckplayer/css-changes"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "171.2.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
     ],
