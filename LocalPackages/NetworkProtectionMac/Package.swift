46a01241158e1669f6ac345ee4365ddd65e9c2a6
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-07-18 16:54:36 +0200

    Updates BSK to 171.1.1-1

--- LocalPackages/NetworkProtectionMac/Package.swift
+++ LocalPackages/NetworkProtectionMac/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         .library(name: "VPNAppLauncher", targets: ["VPNAppLauncher"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "171.1.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "171.1.1-1"),
         .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.4.3"),
         .package(path: "../AppLauncher"),
         .package(path: "../UDSHelper"),
