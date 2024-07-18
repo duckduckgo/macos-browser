f6f2d9eef12abf3556a317f8a671193e6f63f596
Maxim Tsoy <maks.tsoy@gmail.com>
2024-07-18 13:32:20 +0200

    WIP: update autoconsent JS bundle with perf metrics

--- DuckDuckGo/Autoconsent/userscript.js
+++ DuckDuckGo/Autoconsent/userscript.js
@@ -1,4 +1,5 @@
 import AutoConsent from '@duckduckgo/autoconsent';
+import { collectMetrics } from '@duckduckgo/autoconsent';
 import * as rules from '@duckduckgo/autoconsent/rules/rules.json';
 
 const autoconsent = new AutoConsent(
@@ -16,4 +17,20 @@ const autoconsent = new AutoConsent(
 );
 window.autoconsentMessageCallback = (msg) => {
     autoconsent.receiveMessageCallback(msg);
+}
+
+if (window.top === window) {
+    collectMetrics().then(({ tbt, lcp, longTasks, navDuration, cpmInit }) => {
+        // pass the results to the native code. ddgPerfMetrics is a custom JS interface
+        const resultsJson = JSON.stringify({
+            tbt: Math.round(tbt),
+            lcp: Math.round(lcp),
+            longTasksNum: longTasks.length,
+            navDuration: Math.round(navDuration),
+            cpmInit: Math.round(cpmInit),
+        });
+        window.webkit.messageHandlers['ddgPerfMetrics']?.postMessage(location.href + ' ' + resultsJson);
+        console.log(`PERF METRICS: ` + resultsJson);
+        window.alert(`PERF METRICS: ` + resultsJson);
+    });
 }
