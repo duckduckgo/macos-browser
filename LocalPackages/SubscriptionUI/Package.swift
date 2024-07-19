6c7b9051540509fab4b8d4c13cf494fa90661076
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-07-19 08:32:32 +0200

    Use BSK 172.0.0

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "171.2.3"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "172.0.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
