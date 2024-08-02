63b191b85d5311b8100a8ddc26c8dd6178db1f58
Alexey Martemyanov <mallexxx@gmail.com>
2024-08-02 14:38:30 +0600

    BSK -> 176.1.1-1

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "176.1.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "176.1.1-1"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
     ],
