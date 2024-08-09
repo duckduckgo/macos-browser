e4ed6a197c237c7a22c8123c811f6d7f23cb35ed
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-08-09 23:09:05 +0200

    Update address bar padding on new tab page

--- DuckDuckGo/NavigationBar/View/NavigationBarViewController.swift
+++ DuckDuckGo/NavigationBar/View/NavigationBarViewController.swift
@@ -574,7 +574,7 @@ final class NavigationBarViewController: NSViewController {
 
         fileprivate var topPadding: CGFloat {
             switch self {
-            case .homePage: 16
+            case .homePage: 10
             case .popUpWindow: 0
             case .default: 6
             }
@@ -582,7 +582,7 @@ final class NavigationBarViewController: NSViewController {
 
         fileprivate var bottomPadding: CGFloat {
             switch self {
-            case .homePage: 2
+            case .homePage: 8
             case .popUpWindow: 0
             case .default: 6
             }
