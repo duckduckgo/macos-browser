15f442842715190d2e10992c251919b9faa41633
Juan Manuel Pereira <juanmanuel.pereira1@gmail.com>
2024-08-25 21:27:12 -0300

    Fix address bar queries when doing math expressions (#3130)
    Task/Issue URL:
    https://app.asana.com/0/1204006570077678/1208002970105121/f
    Tech Design URL:
    CC:
    
    
    **Description**:
    This pull request introduces a change on how we check for valid
    hostnames. The current regurlar expression had an issue where some
    mathematical expressions where valid hostnames.
    
    BSK PR: https://github.com/duckduckgo/BrowserServicesKit/pull/952
    
    **Steps to test this PR**:
    1. Open the app
    2. Put 16385-12228.75 in the address bar
    3. This should do a search on our SERP instead of trying to open a
    webpage
    4. Test other usual web sites URLs and check those are working.
    
    **Definition of Done**:
    
    * [x] Does this PR satisfy our [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "185.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "185.1.1"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
