85d47e66bd144f62870ba2dac9e6b6ba6a589ef0
Shane Osbourne <sosbourne@duckduckgo.com>
2024-08-27 13:37:45 +0100

    BSK 186.0.1

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "shane/dash-5-1-1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "186.0.1"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
