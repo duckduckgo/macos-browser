af7f0b6d60b144fe43db062c91238960deb3f2d0
Michal Smaga <msmaga@duckduckgo.com>
2024-08-28 16:06:06 +0200

    Bump BSK

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "185.1.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "188.1.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
