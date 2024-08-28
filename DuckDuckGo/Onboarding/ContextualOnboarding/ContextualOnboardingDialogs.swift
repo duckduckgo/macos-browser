ef93b3dd8295ef62e4bb8f3df9ddff193511ca3b
Sabrina Tardio <stardio@duckduckgo.com>
2024-08-28 12:50:36 +0200

    remove trailing space

--- DuckDuckGo/Onboarding/ContextualOnboarding/ContextualOnboardingDialogs.swift
+++ DuckDuckGo/Onboarding/ContextualOnboarding/ContextualOnboardingDialogs.swift
@@ -225,7 +225,7 @@ struct OnboardingSecondaryCTAButton: View {
 
     let title: String
     let action: () -> Void
-    
+
     var body: some View {
         Button(action: action) {
             Text(title)
