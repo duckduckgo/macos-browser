2f273a416aa07670bedfca89d7d166fb88861858
Fernando Bunn <bunn@idevzilla.com>
2024-08-29 14:30:11 +0100

    Update C-S-S
    https://app.asana.com/0/0/1208179338963311/f

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "188.1.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "bunn/duckplayer/onboarding"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
