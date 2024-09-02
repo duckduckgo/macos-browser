b0f6c744a0cc83dc8b23fb98d52045e85598feef
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2024-09-02 19:08:55 +0200

    Update BSK version

--- LocalPackages/NetworkProtectionMac/Package.swift
+++ LocalPackages/NetworkProtectionMac/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         .library(name: "VPNAppLauncher", targets: ["VPNAppLauncher"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "190.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "191.1.0"),
         .package(url: "https://github.com/airbnb/lottie-spm", exact: "4.4.3"),
         .package(path: "../AppLauncher"),
         .package(path: "../UDSHelper"),
