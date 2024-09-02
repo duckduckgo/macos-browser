3eb48b60664a23c56eb5f4c07dea7fbc0afaac24
Mariusz Śpiewak <dus7@users.noreply.github.com>
2024-09-02 19:45:23 +0200

    Remote feature flag for New Tab Page Improvements (#3176)
    Task/Issue URL:
    https://app.asana.com/0/72649045549333/1207539163549338/f
    Tech Design URL:
    CC:
    
    **Description**:
    
    No functional/code changes in macOS, only BSK reference update.
    
    **Steps to test this PR**:
    1. Make sure the project builds.
    
    **Definition of Done**:
    
    * [ ] Does this PR satisfy our [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "191.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "191.1.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
