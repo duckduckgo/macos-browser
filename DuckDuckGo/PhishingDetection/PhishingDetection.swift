1a7ee1f49d40199873437172dacec445193de453
Thomas Espach <tespach@duckduckgo.com>
2024-09-05 15:31:22 +0100

    Add preferences view

--- DuckDuckGo/PhishingDetection/PhishingDetection.swift
+++ DuckDuckGo/PhishingDetection/PhishingDetection.swift
@@ -135,7 +135,7 @@ public class PhishingDetection: PhishingSiteDetecting {
     }
 
     private func startUpdateTasksIfEnabled() {
-        if featureFlagger.isFeatureOn(.phishingDetection),
+        if featureFlagger.isFeatureOn(.phishingDetectionErrorPage),
            self.detectionPreferences.isEnabled {
             startUpdateTasks()
         }
