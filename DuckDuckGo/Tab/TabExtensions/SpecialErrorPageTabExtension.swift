03b77e79162e039626a6054d562614519fb2c6b7
Thomas Espach <tespach@duckduckgo.com>
2024-09-06 18:39:24 +0100

    Revert .load and return .none instead of .redirect

--- DuckDuckGo/Tab/TabExtensions/SpecialErrorPageTabExtension.swift
+++ DuckDuckGo/Tab/TabExtensions/SpecialErrorPageTabExtension.swift
@@ -128,14 +128,13 @@ extension SpecialErrorPageTabExtension: NavigationResponder {
     private func handleMaliciousURL(for navigationAction: NavigationAction, url: URL) -> NavigationActionPolicy? {
         let domain: String
         errorPageType = .phishing
-        if let mainFrameTarget = navigationAction.mainFrameTarget {
+        if navigationAction.mainFrameTarget != nil {
             failingURL = url
             domain = url.host ?? url.toString(decodePunycode: true, dropScheme: true, dropTrailingSlash: true)
             errorData = SpecialErrorData(kind: .phishing, domain: domain, eTldPlus1: tld.eTLDplus1(failingURL?.host))
             if let errorURL = generateErrorPageURL(url) {
-                return .redirect(mainFrameTarget) { navigator in
-                    navigator.load(URLRequest(url: errorURL))
-                }
+                _ = webView?.load(URLRequest(url: errorURL))
+                return .none
             }
         } else {
             return handleMaliciousIframe(navigationAction: navigationAction)
