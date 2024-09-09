7a07edc442949605df6d3256ab1954e6913b839b
Thomas Espach <tespach@duckduckgo.com>
2024-09-09 12:27:49 +0100

    Remove flaky tests- repeatedRedirectChains
    Too many navigation events in short succession.

--- IntegrationTests/PhishingDetection/PhishingDetectionIntegrationTests.swift
+++ IntegrationTests/PhishingDetection/PhishingDetectionIntegrationTests.swift
@@ -110,39 +110,6 @@ class PhishingDetectionIntegrationTests: XCTestCase {
         XCTAssertEqual(tabErrorCode, PhishingDetectionError.detected.errorCode)
     }
 
-    @MainActor
-    func testPhishingDetectedRepeatedClientRedirectChains_tabIsMarkedPhishing() async throws {
-        let urls = [
-            "http://bad.third-party.site/security/badware/phishing-js-redirector-helper.html",
-            "http://bad.third-party.site/security/badware/phishing-js-redirector.html",
-            "http://bad.third-party.site/security/badware/phishing-meta-redirect.html",
-        ]
-
-        for url in urls {
-            try await loadUrl(url)
-            try await waitForTabToFinishLoading()
-            let tabErrorCode = tabViewModel.tab.error?.errorCode
-            XCTAssertEqual(tabErrorCode, PhishingDetectionError.detected.errorCode)
-        }
-    }
-
-    @MainActor
-    func testPhishingDetectedRepeatedServerRedirectChains_tabIsMarkedPhishing() async throws {
-        let urls = [
-            "http://bad.third-party.site/security/badware/phishing-redirect/302",
-            "http://bad.third-party.site/security/badware/phishing-redirect/js",
-            "http://bad.third-party.site/security/badware/phishing-redirect/meta",
-            "http://bad.third-party.site/security/badware/phishing-redirect/meta2"
-        ]
-
-        for url in urls {
-            try await loadUrl(url)
-            try await waitForTabToFinishLoading()
-            let tabErrorCode = tabViewModel.tab.error?.errorCode
-            XCTAssertEqual(tabErrorCode, PhishingDetectionError.detected.errorCode)
-        }
-    }
-
     // MARK: - Helper Methods
 
     @MainActor
