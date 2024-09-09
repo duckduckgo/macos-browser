1fe50d7eb82911cdb63f804b8762ee7275d0d69c
Michal Smaga <msmaga@duckduckgo.com>
2024-09-09 15:47:22 +0200

    Tests for SubscriptionErrorReporter

--- UnitTests/Subscription/SubscriptionErrorReporterTests.swift
+++ UnitTests/Subscription/SubscriptionErrorReporterTests.swift
@@ -24,42 +24,223 @@ import PixelKitTestingUtilities
 
 final class SubscriptionErrorReporterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private struct Constants {
+        static let userDefaultsSuiteName = "SubscriptionErrorReporterTests"
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    var userDefaults: UserDefaults!
+    var pixelKit: PixelKit!
 
-    let testUserDefault = UserDefaults(suiteName: #function)!
-    let reporter = DefaultSubscriptionErrorReporter()
+    var reporter: SubscriptionErrorReporter! = DefaultSubscriptionErrorReporter()
 
-    func handle(error: SubscriptionError, expectedPixel: PrivacyProPixel) async throws {
-        let pixelExpectation = expectation(description: "Pixel fired")
-        pixelExpectation.expectedFulfillmentCount = 2 // All pixels are .dailyAndCount
-        let pixelKit = PixelKit(dryRun: false,
-                                appVersion: "1.0.0",
-                                defaultHeaders: [:],
-                                defaults: testUserDefault) { pixelName, _, _, _, _, _ in
-            if pixelName.hasPrefix(expectedPixel.name) {
-                pixelExpectation.fulfill()
-            } else {
-                XCTFail("Wrong pixel fired: \(pixelName)")
-            }
+    var pixelsFired = Set<String>()
+
+    override func setUp() async throws {
+        userDefaults = UserDefaults(suiteName: Constants.userDefaultsSuiteName)!
+        userDefaults.removePersistentDomain(forName: Constants.userDefaultsSuiteName)
+
+        pixelKit = PixelKit(dryRun: false,
+                            appVersion: "1.0.0",
+                            defaultHeaders: [:],
+                            defaults: userDefaults) { pixelName, _, _, _, _, _ in
+            self.pixelsFired.insert(pixelName)
         }
+        pixelKit.clearFrequencyHistoryForAllPixels()
         PixelKit.setSharedForTesting(pixelKit: pixelKit)
-        reporter.report(subscriptionActivationError: error)
-        await fulfillment(of: [pixelExpectation], timeout: 1.0)
+
+        reporter = DefaultSubscriptionErrorReporter()
+    }
+
+    override func tearDown() async throws {
+        userDefaults = nil
+
         PixelKit.tearDown()
         pixelKit.clearFrequencyHistoryForAllPixels()
+
+        pixelsFired.removeAll()
+
+        reporter = nil
     }
 
-    func testErrorHandling() async throws {
-        try await handle(error: .purchaseFailed, expectedPixel: .privacyProPurchaseFailureStoreError)
-        try await handle(error: .missingEntitlements, expectedPixel: .privacyProPurchaseFailureBackendError)
-        try await handle(error: .failedToGetSubscriptionOptions, expectedPixel: .privacyProPurchaseFailureStoreError)
-        // ... TBC
+    // MARK: - Tests for various subscription errors
+
+    func testReporterForPurchaseFailedError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .purchaseFailed
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
     }
 
+    func testReporterForMissingEntitlementsError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .missingEntitlements
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForFailedToGetSubscriptionOptionsError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .failedToGetSubscriptionOptions
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForFailedToSetSubscriptionError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .failedToSetSubscription
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForFailedToRestoreFromEmailError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .failedToRestoreFromEmail
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForFailedToRestoreFromEmailSubscriptionInactiveError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .failedToRestoreFromEmailSubscriptionInactive
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForFailedToRestorePastPurchaseError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .failedToRestorePastPurchase
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForSubscriptionNotFoundError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .subscriptionNotFound
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_c",
+                                  PrivacyProPixel.privacyProRestorePurchaseStoreFailureNotFound.name + "_d",
+                                  PrivacyProPixel.privacyProRestorePurchaseStoreFailureNotFound.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForSubscriptionExpiredError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .subscriptionExpired
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForHasActiveSubscriptionError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .hasActiveSubscription
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureStoreError.name + "_c",
+                                  PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureBackendError.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForCancelledByUserError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .cancelledByUser
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        XCTAssertTrue(pixelsFired.isEmpty)
+    }
+
+    func testReporterForAccountCreationFailedError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .accountCreationFailed
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        let expectedPixels = Set([PrivacyProPixel.privacyProPurchaseFailureAccountNotCreated.name + "_d",
+                                  PrivacyProPixel.privacyProPurchaseFailureAccountNotCreated.name + "_c"])
+        XCTAssertEqual(pixelsFired, expectedPixels)
+    }
+
+    func testReporterForActiveSubscriptionAlreadyPresentError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .activeSubscriptionAlreadyPresent
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        XCTAssertTrue(pixelsFired.isEmpty)
+    }
+
+    func testReporterForGeneralError() async throws {
+        // Given
+        let errorToBeHandled: SubscriptionError = .generalError
+
+        // When
+        reporter.report(subscriptionActivationError: errorToBeHandled)
+
+        // Then
+        XCTAssertTrue(pixelsFired.isEmpty)
+    }
 }
