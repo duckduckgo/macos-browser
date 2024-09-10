a0dbbad9a44043bb7a02c9914a26ff1fda7f3beb
Anh Do <ado@duckduckgo.com>
2024-09-09 22:11:20 -0400

    Dot goes away when menu opens

--- DuckDuckGo/NavigationBar/View/MoreOptionsMenu.swift
+++ DuckDuckGo/NavigationBar/View/MoreOptionsMenu.swift
@@ -49,7 +49,7 @@ protocol OptionsButtonMenuDelegate: AnyObject {
     func optionsButtonMenuRequestedIdentityTheftRestoration(_ menu: NSMenu)
 }
 
-final class MoreOptionsMenu: NSMenu {
+final class MoreOptionsMenu: NSMenu, NSMenuDelegate {
 
     weak var actionDelegate: OptionsButtonMenuDelegate?
 
@@ -117,6 +117,8 @@ final class MoreOptionsMenu: NSMenu {
             self.sharingMenu = sharingMenu
         }
         self.emailManager.requestDelegate = self
+        
+        delegate = self
 
         setupMenuItems()
     }
@@ -480,6 +482,12 @@ final class MoreOptionsMenu: NSMenu {
         return networkProtectionItem
     }
 
+    func menuWillOpen(_ menu: NSMenu) {
+        guard let updateController = Application.appDelegate.updateController else { return }
+        if updateController.hasPendingUpdate && updateController.needsNotificationDot {
+            updateController.needsNotificationDot = false
+        }
+    }
 }
 
 final class EmailOptionsButtonSubMenu: NSMenu {
