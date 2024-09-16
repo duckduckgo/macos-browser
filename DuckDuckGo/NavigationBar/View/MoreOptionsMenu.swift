5ef38a2fe8999b1631f818e2e1d84c4e6d3863be
Anh Do <ado@duckduckgo.com>
2024-09-16 11:15:34 -0400

    Add missing SPARKLE checks

--- DuckDuckGo/NavigationBar/View/MoreOptionsMenu.swift
+++ DuckDuckGo/NavigationBar/View/MoreOptionsMenu.swift
@@ -410,10 +410,12 @@ final class MoreOptionsMenu: NSMenu, NSMenuDelegate {
     }
 
     func menuWillOpen(_ menu: NSMenu) {
+#if SPARKLE
         guard let updateController = Application.appDelegate.updateController else { return }
         if updateController.hasPendingUpdate && updateController.needsNotificationDot {
             updateController.needsNotificationDot = false
         }
+#endif
     }
 }
 
