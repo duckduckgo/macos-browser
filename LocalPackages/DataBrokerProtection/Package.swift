6d7ca9894264f86ba18b7bfc04d287e8163e3c7f
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2024-09-20 18:00:35 +0200

    Bump BSK version

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "196.2.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "197.0.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../XPCHelper"),
     ],
