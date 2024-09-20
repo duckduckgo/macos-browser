4eff24fb7d1d432803a2d1adafcb472307c36cd6
Graeme Arthur <graeme@graemearthur.com>
2024-09-20 18:49:14 +0200

    Remove login import de-duplication feature flag (#3313)
    Task/Issue URL:
    https://app.asana.com/0/414235014887631/1207759292115042/f
    
    **Description**:
    
    For [✓ macOS: Deduplicate passwords on
    import](https://app.asana.com/0/0/1207597760316574), a feature flag was
    added to mitigate risk of negative user reaction / unforeseen issues.
    This should be removed once we're confident in the implementation.
    
    The feature flag is named `deduplicateLoginsOnImport`
    
    **Steps to test this PR**:
    1. Check login import still deduplicates correctly
    (https://app.asana.com/0/72649045549333/1207597760316577/f)
    
    **Definition of Done**:
    
    * [ ] Does this PR satisfy our [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "196.3.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "197.0.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
