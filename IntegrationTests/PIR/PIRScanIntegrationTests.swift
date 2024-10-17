907cdf810f8c65b2e78fa6b29c7348b1236fed96
Elle Sullivan <ellesailenesullivan@gmail.com>
2024-10-17 16:30:40 +0100

    Change debugging check to be optional

--- IntegrationTests/PIR/PIRScanIntegrationTests.swift
+++ IntegrationTests/PIR/PIRScanIntegrationTests.swift
@@ -237,7 +237,7 @@ final class PIRScanIntegrationTests: XCTestCase {
         let queries9 = try! database.fetchAllBrokerProfileQueryData()
         let initialBrokers = queries9.compactMap { $0.dataBroker }
         XCTAssertEqual(initialBrokers.count, 1)
-        XCTAssertEqual(initialBrokers.first!.name, "DDG Fake Broker")
+        XCTAssertEqual(initialBrokers.first?.name, "DDG Fake Broker")
         XCTAssertEqual(queries9.count, 1)
 
         XCTAssertTrue(loginItemsManager.isAnyEnabled([.dbpBackgroundAgent]))
