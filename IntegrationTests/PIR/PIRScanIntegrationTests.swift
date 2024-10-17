2b8f3dfddc0868daff685fc8b73ce9c216ffbf57
Elle Sullivan <ellesailenesullivan@gmail.com>
2024-10-17 11:43:21 +0100

    Add additional testing to see which brokers are being used

--- IntegrationTests/PIR/PIRScanIntegrationTests.swift
+++ IntegrationTests/PIR/PIRScanIntegrationTests.swift
@@ -234,6 +234,11 @@ final class PIRScanIntegrationTests: XCTestCase {
         })
         print("Stage 1 passed: We save a profile")
 
+        let queries9 = try! database.fetchAllBrokerProfileQueryData()
+        let initialBrokers = queries9.compactMap { $0.dataBroker }
+        XCTAssertEqual(initialBrokers.count, 1)
+        XCTAssertEqual(initialBrokers.first!.name, "DDG Fake Broker")
+
         /*
         2/ We scan brokers
         */
