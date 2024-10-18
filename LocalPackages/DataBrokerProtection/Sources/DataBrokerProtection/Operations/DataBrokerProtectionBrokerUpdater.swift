d568310cbec842ea1cd6dd81e7a381da3fce53f0
Elle Sullivan <ellesailenesullivan@gmail.com>
2024-10-18 17:11:52 +0100

    Go back to using fake broker

--- LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Operations/DataBrokerProtectionBrokerUpdater.swift
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Operations/DataBrokerProtectionBrokerUpdater.swift
@@ -47,7 +47,7 @@ final class FileResources: ResourcesRepository {
         }
 
         //absolutely must change this back, hardcoding for ci testing at the moment
-        let shouldUseFakeBrokers = false//true //(NSApp.runType == .integrationTests)
+        let shouldUseFakeBrokers = true //(NSApp.runType == .integrationTests)
         let brokersURL = resourceURL.appendingPathComponent("Resources").appendingPathComponent("JSON")
         do {
             let fileURLs = try fileManager.contentsOfDirectory(
