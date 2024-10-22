5218a4fef700ce799deda3947e57dd92ea07eafd
Fernando Bunn <bunn@idevzilla.com>
2024-10-22 16:19:09 +0100

    Update BSK

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "200.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "bunn/chat/features"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
