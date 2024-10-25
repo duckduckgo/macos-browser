f47fdd9a1184d0a5c86daf781587febd055367ba
Chris Brind <brindy@duckduckgo.com>
2024-10-25 17:44:34 +0100

    remove some temp code

--- DuckDuckGo/TabBar/ViewModel/TabCollectionViewModel.swift
+++ DuckDuckGo/TabBar/ViewModel/TabCollectionViewModel.swift
@@ -758,12 +758,6 @@ final class TabCollectionViewModel: NSObject {
 
         if self.selectedTabViewModel !== selectedTabViewModel {
             selectedTabViewModel?.tab.lastSelectedAt = Date()
-
-            if selectedTabViewModel?.tab.url?.isErrorPage == true
-                && selectedTabViewModel?.tab.error?.code == WKError.Code.webContentProcessTerminated {
-                // TODO fire pixel
-            }
-
             self.selectedTabViewModel = selectedTabViewModel
         }
     }
