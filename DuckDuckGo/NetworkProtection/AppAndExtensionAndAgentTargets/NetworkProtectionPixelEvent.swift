50ebfc705a9f75a63341193a62a89bffafdf533b
Sam Symons <sam@samsymons.com>
2024-10-29 19:52:54 -0700

    Clean up VPN code.

--- DuckDuckGo/NetworkProtection/AppAndExtensionAndAgentTargets/NetworkProtectionPixelEvent.swift
+++ DuckDuckGo/NetworkProtection/AppAndExtensionAndAgentTargets/NetworkProtectionPixelEvent.swift
@@ -79,10 +79,6 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
     case networkProtectionClientFailedToEncodeRegisterKeyRequest
     case networkProtectionClientFailedToFetchRegisteredServers(_ error: Error?)
     case networkProtectionClientFailedToParseRegisteredServersResponse
-    case networkProtectionClientFailedToEncodeRedeemRequest
-    case networkProtectionClientInvalidInviteCode
-    case networkProtectionClientFailedToRedeemInviteCode(_ error: Error?)
-    case networkProtectionClientFailedToParseRedeemResponse(_ error: Error)
     case networkProtectionClientFailedToFetchLocations(_ error: Error?)
     case networkProtectionClientFailedToParseLocationsResponse(_ error: Error?)
     case networkProtectionClientFailedToFetchServerStatus(_ error: Error?)
@@ -246,18 +242,6 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
         case .networkProtectionClientFailedToParseRegisteredServersResponse:
             return "netp_backend_api_error_parsing_device_registration_response_failed"
 
-        case .networkProtectionClientFailedToEncodeRedeemRequest:
-            return "netp_backend_api_error_encoding_redeem_request_body_failed"
-
-        case .networkProtectionClientInvalidInviteCode:
-            return "netp_backend_api_error_invalid_invite_code"
-
-        case .networkProtectionClientFailedToRedeemInviteCode:
-            return "netp_backend_api_error_failed_to_redeem_invite_code"
-
-        case .networkProtectionClientFailedToParseRedeemResponse:
-            return "netp_backend_api_error_parsing_redeem_response_failed"
-
         case .networkProtectionClientFailedToFetchLocations:
             return "netp_backend_api_error_failed_to_fetch_location_list"
 
@@ -387,8 +371,6 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
             return error?.pixelParameters
         case .networkProtectionClientFailedToFetchRegisteredServers(let error):
             return error?.pixelParameters
-        case .networkProtectionClientFailedToRedeemInviteCode(let error):
-            return error?.pixelParameters
         case .networkProtectionClientFailedToFetchLocations(let error):
             return error?.pixelParameters
         case .networkProtectionClientFailedToParseLocationsResponse(let error):
@@ -450,9 +432,6 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
                 .networkProtectionClientFailedToParseServerListResponse,
                 .networkProtectionClientFailedToEncodeRegisterKeyRequest,
                 .networkProtectionClientFailedToParseRegisteredServersResponse,
-                .networkProtectionClientFailedToEncodeRedeemRequest,
-                .networkProtectionClientInvalidInviteCode,
-                .networkProtectionClientFailedToParseRedeemResponse,
                 .networkProtectionClientInvalidAuthToken,
                 .networkProtectionWireguardErrorCannotLocateTunnelFileDescriptor,
                 .networkProtectionWireguardErrorFailedDNSResolution,
@@ -473,8 +452,7 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
 
     var error: (any Error)? {
         switch self {
-        case .networkProtectionClientFailedToRedeemInviteCode(let error),
-                .networkProtectionClientFailedToFetchLocations(let error),
+        case .networkProtectionClientFailedToFetchLocations(let error),
                 .networkProtectionClientFailedToParseLocationsResponse(let error),
                 .networkProtectionClientFailedToFetchServerList(let error),
                 .networkProtectionClientFailedToFetchRegisteredServers(let error),
@@ -486,7 +464,6 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
                 .networkProtectionTunnelStopFailure(let error),
                 .networkProtectionTunnelUpdateFailure(let error),
                 .networkProtectionTunnelWakeFailure(let error),
-                .networkProtectionClientFailedToParseRedeemResponse(let error),
                 .networkProtectionWireguardErrorCannotSetNetworkSettings(let error),
                 .networkProtectionWireguardErrorCannotStartWireguardBackend(let error),
                 .networkProtectionWireguardErrorCannotSetWireguardConfig(let error),
@@ -530,8 +507,6 @@ enum NetworkProtectionPixelEvent: PixelKitEventV2 {
                 .networkProtectionClientFailedToParseServerListResponse,
                 .networkProtectionClientFailedToEncodeRegisterKeyRequest,
                 .networkProtectionClientFailedToParseRegisteredServersResponse,
-                .networkProtectionClientFailedToEncodeRedeemRequest,
-                .networkProtectionClientInvalidInviteCode,
                 .networkProtectionClientInvalidAuthToken,
                 .networkProtectionKeychainErrorFailedToCastKeychainValueToData,
                 .networkProtectionKeychainReadError,
