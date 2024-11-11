ad8fc8207275a08cfc03d2b9e32dc1cd3a0e9ab9
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-11-11 21:20:23 +0100

    Fixes a build issue

--- DuckDuckGoVPN/DuckDuckGoVPNAppDelegate.swift
+++ DuckDuckGoVPN/DuckDuckGoVPNAppDelegate.swift
@@ -134,8 +134,6 @@ final class DuckDuckGoVPNAppDelegate: NSObject, NSApplicationDelegate {
     private let configurationStore = ConfigurationStore()
     private let configurationManager: ConfigurationManager
     private var configurationSubscription: AnyCancellable?
-    private let privacyConfigurationManager = VPNPrivacyConfigurationManager()
-
     private let privacyConfigurationManager = VPNPrivacyConfigurationManager(internalUserDecider: DefaultInternalUserDecider(store: UserDefaults.appConfiguration))
     private lazy var featureFlagger = DefaultFeatureFlagger(
         internalUserDecider: privacyConfigurationManager.internalUserDecider,
