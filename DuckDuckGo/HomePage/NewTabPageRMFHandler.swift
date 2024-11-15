c94cfc5b5d7d99021787a604549edff4a176cd12
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-11-15 13:14:53 +0100

    Display actual remote messages on NTP

--- DuckDuckGo/HomePage/NewTabPageRMFHandler.swift
+++ DuckDuckGo/HomePage/NewTabPageRMFHandler.swift
@@ -62,24 +62,135 @@ final class NewTabPageRMFHandler: NewTabPageScriptClient {
 
     @MainActor
     private func rmfGetData(params: Any, original: WKScriptMessage) async throws -> Encodable? {
-        let data = NewTabPageUserScript.RMFData(content: getRemoteMessage())
-        return data
-    }
+        guard let remoteMessage = activeRemoteMessageModel.remoteMessage else {
+            return NewTabPageUserScript.RMFData(content: nil)
+        }
 
-    func getRemoteMessage() -> NewTabPageUserScript.RMFMessage? {
-        .small(.init(descriptionText: "Hello, this is a description", id: "hejka", titleText: "Hello I'm a title"))
+        return NewTabPageUserScript.RMFData(content: .init(remoteMessage))
     }
-
 
     private func notifyRemoteMessageDidChange(_ remoteMessage: RemoteMessageModel?) {
-        let data = NewTabPageUserScript.RMFData(content: .small(.init(descriptionText: "Hello, this is a description", id: "hejka", titleText: "Hello I'm a title")))
+        let data: NewTabPageUserScript.RMFData = {
+            guard let remoteMessage else {
+                return .init(content: nil)
+            }
+            return .init(content: NewTabPageUserScript.RMFMessage(remoteMessage))
+        }()
 
         userScriptsSource?.userScripts.forEach { userScript in
             pushMessage(named: MessageNames.rmfGetData.rawValue, params: data, for: userScript)
         }
     }
+}
 
-    func notifyRemoteMessageDidChange(_ remoteMessageData: NewTabPageUserScript.RMFData?) {
+extension NewTabPageUserScript.RMFMessage {
+    init?(_ remoteMessageModel: RemoteMessageModel) {
+        guard let modelType = remoteMessageModel.content, modelType.isSupported else {
+            return nil
+        }
 
+        switch modelType {
+        case let .small(titleText, descriptionText):
+            self = .small(.init(id: remoteMessageModel.id, titleText: titleText, descriptionText: descriptionText))
+
+        case let .medium(titleText, descriptionText, placeholder):
+            self = .medium(.init(id: remoteMessageModel.id, titleText: titleText, descriptionText: descriptionText, icon: .init(placeholder)))
+
+        case let .bigSingleAction(titleText, descriptionText, placeholder, primaryActionText, primaryAction):
+            self = .bigSingleAction(.init(id: remoteMessageModel.id, titleText: titleText, descriptionText: descriptionText, icon: .init(placeholder), primaryActionText: primaryActionText))
+
+        case let .bigTwoAction(titleText, descriptionText, placeholder, primaryActionText, primaryAction, secondaryActionText, secondaryAction):
+            self = .bigTwoAction(.init(id: remoteMessageModel.id, titleText: titleText, descriptionText: descriptionText, icon: .init(placeholder), primaryActionText: primaryActionText, secondaryActionText: secondaryActionText))
+
+        default:
+            return nil
+        }
+    }
+}
+
+extension NewTabPageUserScript {
+
+    struct RMFData: Encodable {
+        var content: RMFMessage?
+    }
+
+    enum RMFMessage: Encodable {
+        case small(SmallMessage), medium(MediumMessage), bigSingleAction(BigSingleActionMessage), bigTwoAction(BigTwoActionMessage)
+
+        func encode(to encoder: any Encoder) throws {
+            try message.encode(to: encoder)
+        }
+
+        var message: Encodable {
+            switch self {
+            case .small(let message):
+                return message
+            case .medium(let message):
+                return message
+            case .bigSingleAction(let message):
+                return message
+            case .bigTwoAction(let message):
+                return message
+            }
+        }
+    }
+
+    struct SmallMessage: Encodable {
+        let messageType = "small"
+
+        var id: String
+        var titleText: String
+        var descriptionText: String
+    }
+
+    struct MediumMessage: Encodable {
+        let messageType = "medium"
+
+        var id: String
+        var titleText: String
+        var descriptionText: String
+        var icon: RMFIcon
+    }
+
+    struct BigSingleActionMessage: Encodable {
+        let messageType = "big_single_action"
+
+        var id: String
+        var titleText: String
+        var descriptionText: String
+        var icon: RMFIcon
+        var primaryActionText: String
+    }
+
+    struct BigTwoActionMessage: Encodable {
+        let messageType = "big_two_action"
+
+        var id: String
+        var titleText: String
+        var descriptionText: String
+        var icon: RMFIcon
+        var primaryActionText: String
+        var secondaryActionText: String
+    }
+
+    enum RMFIcon: String, Encodable {
+        case announce, ddgAnnounce, criticalUpdate, appUpdate, privacyPro
+
+        init(_ placeholder: RemotePlaceholder) {
+            switch placeholder {
+            case .announce:
+                self = .announce
+            case .ddgAnnounce:
+                self = .ddgAnnounce
+            case .criticalUpdate:
+                self = .criticalUpdate
+            case .appUpdate:
+                self = .appUpdate
+            case .privacyShield:
+                self = .privacyPro
+            default:
+                self = .ddgAnnounce
+            }
+        }
     }
 }
