64f856b717ee442b217d766d71c3c85ea7586152
Shane Osbourne <shane.osbourne8@gmail.com>
2024-11-19 15:58:00 +0000

    [macos] adding support for message bridge (#3558)
    Task/Issue URL: https://app.asana.com/0/0/1208773745506350/f
    Tech Design URL:
    CC:
    
    **Description**:
    
    See the Asana task for more details - but a short version is that a
    recent change in C-S-S requires another UUID - I deliberately DO NOT
    want to re-use `sessionKey`, since that's used in other places already
    and it would be unclear when/where we could change this.
    
    - name: `messageSecret` was chosen to match what android already use
    - where: I placed this on ContentScopePreferences because it gets
    serialized as JSON into our scripts and is already used for such things
      - Again, it also matches Android
    
    **Steps to test this PR**: (👀 VIDEO below)
    1. Use the debug menu to set the AI Chat -> Web Communication -> Set
    Custom URL
        - set it to `https://bridge-example.netlify.app/ai-chat.html`
    3. Now override remote config to use:
        - `https://bridge-example.netlify.app/ai-chat.json`
    4. visit https://bridge-example.netlify.app/ai-chat.html
    5. verify settings are opened
    6. verify the data is fetched
    
    
    ---
    
    AI Chat -> Web Communication -> Set Custom URL
    
    <img width="868" alt="Screenshot 2024-11-15 at 10 42 17 AM"
    src="https://github.com/user-attachments/assets/65565ae3-19a0-40cf-aafc-fb746ddd0786">
    
    
    
    https://github.com/user-attachments/assets/9b197aca-965d-4541-9d4b-70b066fc352e
    
    
    
    
    **Definition of Done**:
    
    * [x] Does this PR satisfy our [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?
    
    ---
    ###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)
    
    3.   -  
    8. change the message or4. change the message or
    
    **Definition of Done**:**Definition of Done**:
    
    * [ ] Does this PR satisfy our [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?* [ ]
    Does this PR satisfy our [Definition of
    Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?
    
    ------
    ###### Internal references:###### Internal references:
    [Pull Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)[Pull
    Request Review
    Checklist](https://app.asana.com/0/1202500774821704/1203764234894239/f)
    [Software Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)[Software
    Engineering
    Expectations](https://app.asana.com/0/59792373528535/199064865822552)
    [Technical Design
    Template](https://app.asana.com/0/59792373528535/184709971311943)[Technical
    Design Template](https://app.asana.com/0/59792373528535/184709971311943)
    [Pull Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)[Pull
    Request
    Documentation](https://app.asana.com/0/1202500774821704/1204012835277482/f)
    
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

--- LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Scheduler/DataBrokerProtectionAgentManager.swift
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Scheduler/DataBrokerProtectionAgentManager.swift
@@ -61,6 +61,7 @@ public class DataBrokerProtectionAgentManagerProvider {
                                                   unknownUsernameCategorization: false)
         let contentScopeProperties = ContentScopeProperties(gpcEnabled: false,
                                                             sessionKey: UUID().uuidString,
+                                                            messageSecret: UUID().uuidString,
                                                             featureToggles: features)
 
         let fakeBroker = DataBrokerDebugFlagFakeBroker()
