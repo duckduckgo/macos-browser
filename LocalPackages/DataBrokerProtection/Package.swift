f6aac2542ed97ec37e235c384daadbd207d1018b
Fernando Bunn <bunn@idevzilla.com>
2024-11-28 16:24:57 -0300

    Update BSK

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "bunn/aichat-flags"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "212.1.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../AppKitExtensions"),
         .package(path: "../XPCHelper"),
--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", branch: "bunn/aichat-flags"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "212.1.0"),
         .package(path: "../SwiftUIExtensions")
     ],
     targets: [
