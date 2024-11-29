caccaff8aa7ca809bdb59780e0488b77e0a25d17
Alexey Martemyanov <mallexxx@gmail.com>
2024-11-29 15:07:54 +0600

    fix linter issue

--- DuckDuckGo/Tab/TabExtensions/SpecialErrorPageTabExtension.swift
+++ DuckDuckGo/Tab/TabExtensions/SpecialErrorPageTabExtension.swift
@@ -90,7 +90,6 @@ extension SpecialErrorPageTabExtension: NavigationResponder {
         //
         // There may be cases where this is a bad idea, for example a malicious site that redirects to a socialnetwork.com -
         // but if a flagged site sends you somewhere, you should still be cautious of that site so we want it to remain flagged.
-        // swiftlint:disable:next todo
         if let threatKind = state.bypassedMaliciousSiteThreatKind, navigationAction.navigationType == .other {
             exemptions[url] = threatKind
         }
