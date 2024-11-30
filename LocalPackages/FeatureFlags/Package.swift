d2c78cd64e30b05cd40371fdd31057fd94b16fd4
Sam Symons <sam@samsymons.com>
2024-11-29 17:53:11 -0800

    Update BSK tag.

--- LocalPackages/FeatureFlags/Package.swift
+++ LocalPackages/FeatureFlags/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             targets: ["FeatureFlags"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "215.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "216.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
