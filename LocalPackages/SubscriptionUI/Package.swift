38e51817a1b04b12586a160722b23c1019dd42d9
Sam Symons <sam@samsymons.com>
2024-11-29 18:18:58 -0800

    Remove malformed error pixel (#3612)
    Task/Issue URL: https://app.asana.com/0/414235014887631/1208683003138954/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR removes the malformed error pixel.

--- LocalPackages/SubscriptionUI/Package.swift
+++ LocalPackages/SubscriptionUI/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["SubscriptionUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "215.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "216.0.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../FeatureFlags")
     ],
