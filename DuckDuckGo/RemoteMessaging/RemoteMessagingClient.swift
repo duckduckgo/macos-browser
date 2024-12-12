aa916e3403a2feee8106049c37d712ac270cab49
Juan Manuel Pereira <jpereira@duckduckgo.com>
2024-12-12 18:39:07 -0300

    Use raw JSON

--- DuckDuckGo/RemoteMessaging/RemoteMessagingClient.swift
+++ DuckDuckGo/RemoteMessaging/RemoteMessagingClient.swift
@@ -47,9 +47,9 @@ final class RemoteMessagingClient: RemoteMessagingProcessing {
         static let minimumConfigurationRefreshInterval: TimeInterval = 60 * 30
         static let endpoint: URL = {
 #if DEBUG
-            URL(string: "https://www.jsonblob.com/api/1316017217598578688")!
+            URL(string: "https://raw.githubusercontent.com/duckduckgo/macos-browser/b5cd437bd1bfbfa014deca7a070dc376f22d3024/tab-bar-remote-json-endpoint.json")!
 #else
-            URL(string: "https://www.jsonblob.com/api/1316017217598578688")!
+            URL(string: "https://raw.githubusercontent.com/duckduckgo/macos-browser/b5cd437bd1bfbfa014deca7a070dc376f22d3024/tab-bar-remote-json-endpoint.json")!
 #endif
         }()
     }
