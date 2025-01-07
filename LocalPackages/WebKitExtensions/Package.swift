451566967142d894436514453a53e557f7b91152
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-01-07 19:01:27 +0100

    Use BSK 224.0.0

--- LocalPackages/WebKitExtensions/Package.swift
+++ LocalPackages/WebKitExtensions/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "223.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "224.0.0"),
         .package(path: "../AppKitExtensions")
     ],
     targets: [
