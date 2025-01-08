b1996b20f9540e333208756f9e1282b36c45005c
Anh Do <ado@duckduckgo.com>
2025-01-08 19:15:54 +0700

    Add abuliga.duckduckgo.com to allowlisted domains

--- DuckDuckGo/Tab/UserScripts/IdentityTheftRestorationPagesUserScript.swift
+++ DuckDuckGo/Tab/UserScripts/IdentityTheftRestorationPagesUserScript.swift
@@ -77,7 +77,8 @@ final class IdentityTheftRestorationPagesFeature: Subfeature {
 
     var messageOriginPolicy: MessageOriginPolicy = .only(rules: [
         .exact(hostname: "duckduckgo.com"),
-        .exact(hostname: "abrown.duckduckgo.com")
+        .exact(hostname: "abrown.duckduckgo.com"),
+        .exact(hostname: "abuliga.duckduckgo.com"),
     ])
 
     init(subscriptionFeatureAvailability: SubscriptionFeatureAvailability = DefaultSubscriptionFeatureAvailability()) {
