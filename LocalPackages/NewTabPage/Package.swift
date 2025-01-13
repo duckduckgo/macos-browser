38b485effa9c9dc3cf793b79d6ec0d3f7ad34725
Sam Symons <sam@samsymons.com>
2025-01-13 11:24:04 -0800

    Add `Persistence` dependency to `Crashes` module (#3727)
    Task/Issue URL: https://app.asana.com/0/1199333091098016/1209143709072898/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR updates the BSK Crashes module to correctly include relevant dependencies.

--- LocalPackages/NewTabPage/Package.swift
+++ LocalPackages/NewTabPage/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             targets: ["NewTabPage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "224.6.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "224.6.1"),
         .package(path: "../WebKitExtensions"),
         .package(path: "../Utilities"),
     ],
