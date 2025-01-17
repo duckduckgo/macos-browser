71460a54b280d0d808e5909628f7bcb2a4c98aeb
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-01-17 17:39:18 +0100

    Update documentation for UserScriptActionsManager

--- LocalPackages/UserScriptActionsManager/Package.swift
+++ LocalPackages/UserScriptActionsManager/Package.swift
@@ -32,9 +32,7 @@ let package = Package(
             targets: ["UserScriptActionsManager"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "224.6.1"),
-        .package(path: "../WebKitExtensions"),
-        .package(path: "../Utilities"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "224.6.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
