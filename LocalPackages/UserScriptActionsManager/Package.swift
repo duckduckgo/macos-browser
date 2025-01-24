113ff089d5a5efc0c5c0b4fa2719715d7eea3d63
Alexey Martemyanov <mallexxx@gmail.com>
2025-01-24 12:14:43 +0600

    BSK -> 229.1.1

--- LocalPackages/UserScriptActionsManager/Package.swift
+++ LocalPackages/UserScriptActionsManager/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             targets: ["UserScriptActionsManager"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "229.0.0")
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "229.1.1")
     ],
     targets: [
         .target(
