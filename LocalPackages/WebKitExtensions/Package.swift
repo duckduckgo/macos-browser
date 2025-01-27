efc396bb628e779324fde4f6b615b18821bfc0f5
Pete <petes44@icloud.com>
2025-01-27 12:18:39 +0000

    Bump to latest BSK

--- LocalPackages/WebKitExtensions/Package.swift
+++ LocalPackages/WebKitExtensions/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "231.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "231.0.1"),
         .package(path: "../AppKitExtensions")
     ],
     targets: [
