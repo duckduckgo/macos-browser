3c1c0f8f34476571263e43bbf76ed7bb65da3844
Graeme Arthur <2030310+graeme@users.noreply.github.com>
2025-01-27 14:13:30 +0100

    Point to the BSK release

--- LocalPackages/NewTabPage/Package.swift
+++ LocalPackages/NewTabPage/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             targets: ["NewTabPage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "231.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "232.0.0"),
         .package(path: "../WebKitExtensions"),
         .package(path: "../UserScriptActionsManager"),
         .package(path: "../Utilities"),
