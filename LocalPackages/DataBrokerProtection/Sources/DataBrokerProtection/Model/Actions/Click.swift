18f3dae69699aa93a3d74c55aaee0982fffa7458
Brian Hall <brian@hack.design>
2025-01-30 14:50:16 +0100

    Add the ability to click actions to fail silently

--- LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Model/Actions/Click.swift
+++ LocalPackages/DataBrokerProtection/Sources/DataBrokerProtection/Model/Actions/Click.swift
@@ -36,6 +36,7 @@ struct ClickAction: Action {
     let dataSource: DataSource?
     let choices: [Choice]?
     let `default`: Default?
+    let failSilently: Bool?
 
     let hasDefault: Bool
 
@@ -43,7 +44,7 @@ struct ClickAction: Action {
         let elements: [PageElement]?
     }
 
-    init(id: String, actionType: ActionType, elements: [PageElement]? = nil, dataSource: DataSource? = nil, choices: [Choice]? = nil, `default`: Default? = nil, hasDefault: Bool = false) {
+    init(id: String, actionType: ActionType, elements: [PageElement]? = nil, dataSource: DataSource? = nil, choices: [Choice]? = nil, `default`: Default? = nil, hasDefault: Bool = false, failSilently: Bool? = nil) {
        self.id = id
        self.actionType = actionType
        self.elements = elements
@@ -51,6 +52,7 @@ struct ClickAction: Action {
        self.choices = choices
        self.default = `default`
        self.hasDefault = `default` != nil
+       self.failSilently = failSilently
    }
 
     init(from decoder: any Decoder) throws {
@@ -62,6 +64,8 @@ struct ClickAction: Action {
         self.choices = try container.decodeIfPresent([Choice].self, forKey: .choices)
         self.default = try container.decodeIfPresent(Default.self, forKey: .default)
         self.hasDefault = container.contains(.default)
+        
+        self.failSilently = try container.decodeIfPresent(Bool.self, forKey: .failSilently)
     }
 
     enum CodingKeys: String, CodingKey {
@@ -71,6 +75,7 @@ struct ClickAction: Action {
         case dataSource
         case choices
         case `default`
+        case failSilently
     }
 
     func encode(to encoder: any Encoder) throws {
@@ -80,6 +85,7 @@ struct ClickAction: Action {
         try container.encodeIfPresent(self.elements, forKey: .elements)
         try container.encodeIfPresent(self.dataSource, forKey: .dataSource)
         try container.encodeIfPresent(self.choices, forKey: .choices)
+        try container.encodeIfPresent(self.failSilently, forKey: .failSilently)
 
         if self.hasDefault {
             try container.encode(self.default, forKey: .default)
