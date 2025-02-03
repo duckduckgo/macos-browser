263857e466b1f907d8a5b050ca156c89ac3aea4b
Diego Rey Mendez <dreymendez@duckduckgo.com>
2025-02-03 10:50:06 +0100

    Removes empty files

--- LocalPackages/AppInfoRetriever/Package.swift
+++ LocalPackages/AppInfoRetriever/Package.swift
@@ -16,9 +16,5 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "AppInfoRetriever"),
-        .testTarget(
-            name: "AppInfoRetrieverTests",
-            dependencies: ["AppInfoRetriever"]
-        ),
     ]
 )
