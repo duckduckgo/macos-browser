9df11fb529b6ac52b7421ad564e18bf89051f5e9
Diego Rey Mendez <diegoreymendez@users.noreply.github.com>
2025-02-04 21:54:23 +0100

    Update VPN Status View submenu to show exclusions (#3818)
    Task/Issue URL: https://app.asana.com/0/0/1209150257377771/f
    
    ## Description
    
    Changes in this PR:
    - Re-organizes the Status View submenu
    - Updates titles to remove the "..." suffix
    - Adds exclusions to the VPN Status View submenu
    
    ## Translations
    
    Translation Job is
    [here](https://dashboard.smartling.com/app/projects/f767cb116/account-jobs/f767cb116:kcvr3syqw1nh).

--- DuckDuckGoVPN/UserText.swift
+++ DuckDuckGoVPN/UserText.swift
@@ -19,11 +19,45 @@
 import Foundation
 
 final class UserText {
-    // MARK: - Status Menu
+    // MARK: - VPN Status View submenu (legacy)
 
     static let networkProtectionStatusMenuVPNSettings = NSLocalizedString("network.protection.status.menu.vpn.settings", value: "VPN Settings…", comment: "The status menu 'VPN Settings' menu item")
     static let networkProtectionStatusMenuFAQ = NSLocalizedString("network.protection.status.menu.faq", value: "FAQs and Support…", comment: "The status menu 'FAQ' menu item")
     static let networkProtectionStatusMenuOpenDuckDuckGo = NSLocalizedString("network.protection.status.menu.vpn.open-duckduckgo", value: "Open DuckDuckGo…", comment: "The status menu 'Open DuckDuckGo' menu item")
-    static let networkProtectionStatusMenuShareFeedback = NSLocalizedString("network.protection.status.menu.share.feedback", value: "Share VPN Feedback…", comment: "The status menu 'Share VPN Feedback' menu item")
     static let networkProtectionStatusMenuSendFeedback = NSLocalizedString("network.protection.status.menu.send.feedback", value: "Send Feedback…", comment: "The status menu 'Send Feedback' menu item")
+
+    // MARK: - VPN Status View submenu
+
+    static let vpnStatusViewVPNSettingsMenuItemTitle = NSLocalizedString(
+        "vpn.status-view.vpn-settings.menu-item.title",
+        value: "VPN Settings",
+        comment: "The VPN status view's 'VPN Settings' menu item for our status menu app. The number shown is how many Apps are excluded.")
+
+    static func vpnStatusViewExcludedAppsMenuItemTitle(_ count: Int) -> String {
+        let message = NSLocalizedString(
+            "vpn.status-view.excluded-apps.menu-item.title",
+            value: "Excluded Apps (%d)",
+            comment: "The VPN status view's 'Excluded Apps' menu item for our status menu app. The number shown is how many Apps are excluded.")
+
+        return String(format: message, count)
+    }
+
+    static func vpnStatusViewExcludedDomainsMenuItemTitle(_ count: Int) -> String {
+        let message = NSLocalizedString(
+            "vpn.status-view.excluded-domains.menu-item.title",
+            value: "Excluded Websites (%d)",
+            comment: "The VPN status view's 'Excluded Websites' menu item for our status menu app. The number shown is how many websites are excluded.")
+
+        return String(format: message, count)
+    }
+
+    static let vpnStatusViewFAQMenuItemTitle = NSLocalizedString(
+        "vpn.status-view.faq.menu-item.title",
+        value: "FAQs and Support",
+        comment: "The VPN status view's 'FAQ' menu item for our status menu app")
+
+    static let vpnStatusViewSendFeedbackMenuItemTitle = NSLocalizedString(
+        "vpn.status-view.send-feedback.menu-item.title",
+        value: "Send Feedback",
+        comment: "The VPN status view's 'Send Feedback' menu item for our status menu app")
 }
