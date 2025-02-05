9ca4faab3e43bab90d604e7cfb33f53fb1c8a0a4
Sabrina Tardio <stardio@duckduckgo.com>
2025-02-05 20:08:20 +0100

    fix integration test

--- IntegrationTests/Configurations/ConfigurationManagerIntegrationTests.swift
+++ IntegrationTests/Configurations/ConfigurationManagerIntegrationTests.swift
@@ -48,7 +48,7 @@ final class ConfigurationManagerIntegrationTests: XCTestCase {
         // THEN
         var newEtag = ContentBlocking.shared.trackerDataManager.fetchedData?.etag
         XCTAssertNotEqual(etag, newEtag)
-        XCTAssertEqual(newEtag, "\"2ce60c57c3d384f986ccbe2c422aac44\"")
+        XCTAssertEqual(newEtag, "\"1f75f20fb9fb258f800eef02be752f48\"")
 
         // RESET
         _ = AppConfigurationURLProvider(customPrivacyConfiguration: AppConfigurationURLProvider.Constants.defaultPrivacyConfigurationURL)
