4cbaa98ebb36587832185eaa073de37d4ccefef6
Alessandro Boron <aboron@duckduckgo.com>
2025-02-06 17:55:04 +1100

    Update BSK ref to exact version

--- LocalPackages/HistoryView/Package.swift
+++ LocalPackages/HistoryView/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             targets: ["HistoryView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "235.3.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "236.0.0"),
         .package(path: "../WebKitExtensions"),
         .package(path: "../UserScriptActionsManager"),
         .package(path: "../Utilities"),
