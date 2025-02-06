9f22e5f4451cc293d7db3302c39313bf1fc01858
Elle Sullivan <ellesailenesullivan@gmail.com>
2025-02-06 11:34:09 +0000

    Fix BSK version

--- LocalPackages/DataBrokerProtectionShared/Package.swift
+++ LocalPackages/DataBrokerProtectionShared/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtectionShared"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "235.3.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "236.0.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../AppKitExtensions"),
         .package(path: "../XPCHelper"),
