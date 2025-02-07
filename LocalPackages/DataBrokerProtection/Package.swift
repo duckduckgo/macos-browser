fc35cc77c489bd54989955b6460feb92a16b61d0
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-02-07 17:05:05 +0100

    Fix displaying long URLs on HTML NTP Recent Activity feed (#3848)
    Task/Issue URL: https://app.asana.com/0/0/1209351911435697/f
    
    Description:
    Update BSK to a version containing C-S-S fix for HTML New Tab Page.

--- LocalPackages/DataBrokerProtection/Package.swift
+++ LocalPackages/DataBrokerProtection/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtection"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "235.2.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "235.2.0-1"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../AppKitExtensions"),
         .package(path: "../XPCHelper"),
--- LocalPackages/HistoryView/Package.swift
+++ LocalPackages/HistoryView/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
             targets: ["HistoryView"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "235.2.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "235.2.0-1"),
         .package(path: "../WebKitExtensions"),
         .package(path: "../UserScriptActionsManager"),
         .package(path: "../Utilities"),
