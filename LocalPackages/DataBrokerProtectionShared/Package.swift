bdc1fe4431fbeaf6b2bdf704015627aa440504bd
Dominik Kapusta <dkapusta@duckduckgo.com>
2025-02-13 19:05:41 +0100

    Update C-S-S to 7.18.0 (#3867)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1209396115816169
    
    Description:
    This change bumps C-S-S to a version that includes new APIs for macOS History View

--- LocalPackages/DataBrokerProtectionShared/Package.swift
+++ LocalPackages/DataBrokerProtectionShared/Package.swift
@@ -29,7 +29,7 @@ let package = Package(
             targets: ["DataBrokerProtectionShared"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "237.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "237.1.0"),
         .package(path: "../SwiftUIExtensions"),
         .package(path: "../AppKitExtensions"),
         .package(path: "../XPCHelper"),
--- LocalPackages/UserScriptActionsManager/Package.swift
+++ LocalPackages/UserScriptActionsManager/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             targets: ["UserScriptActionsManager"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "237.0.0"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "237.1.0"),
     ],
     targets: [
         .target(
